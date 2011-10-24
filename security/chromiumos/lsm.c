67af35d773c3a709d88b67cdf5815d275fa3cf60
Stephan Uphoff <ups@chromium.org>
2011-10-24 19:12:48 +0000

    CHROMIUM: LSM: chromiumos security module
    Initially the chromiumos security module only prevents symlinks
    in mount paths. Future versions will be more restrictive and
    will be configurable using a driver interface.
    
    This is a squash of the following commits:
      CHROMIUM: kernel: Initial chromiumos security module.
      CHROMIUM: LSM: add module loading restrictions
      CHROMIUM: LSM: spinlock the root directory lock
      CHROMIUM: LSM: fix sysfs parameter exposure
      CHROMIUM: LSM: switch to stacked Chromium OS LSM
      CHROMIUM: LSM: handle pinned filesystem umount
      CHROMIUM: LSM: enable firmware pinning
      CHROMIUM: LSM: check vfsmount to avoid disengagement
      CHROMIUM: LSM: Assume read-only root if device is NULL
      CHROMIUM: Don't allow building LSM as a module
      CHROMIUM: LSM: kernel flag to allow mounting to symlinked path
      CHROMIUM: fix a ChromiumOS-specific memory leak in fs/namei.c
      CHROMIUM: LSM: handle errors from getname_flags() in nameidata_set_temporary()
    
    Additionally, some fixups were applied to make the code work on top of
    kernel 4.4 and the registration was updated to use regular LSM hooks.
    
    Additional interfaces were exposed in fs/namei.c and do_mount() in
    fs/namespace.c was updated to set up a temporary current->nameidata
    while resolving the mountpoint path until sb_mount security hooks are
    called, in order to make it possible to still check total_link_count
    from those hooks.
    
    It is still not possible to build LSMs as loadable modules, since
    security_hooks_heads (in security/security.c) is not exported. This
    could be fixed with the following upstream change:
      EXPORT_SYMBOL(security_hook_heads);
    For now, we're happy to keep this as a built-in only.
    
    BUG=chromium-os:21954
    TEST=Use a mount path with a symlink and observe mount failure.
    
    Change-Id: I47ade3b7be684ab9e0533ce5ffd1e81009eaebfe
    Signed-off-by: Stephan Uphoff <ups@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/10581
    Reviewed-by: Kees Cook <keescook@chromium.org>
    Reviewed-by: Mandeep Singh Baines <msb@chromium.org>
    
    [rebase44(filbranden): Resolved trivial conflicts in Kconfig.  Updated
     Makefile to use a separate "chromiumos_lsm" name for the module.
     Introduced nameidata_get_total_link_count() and other APIs to make it
     possible to keep a valid nameidata and to access that field from this
     module.  Updated registration to use the LSM_HOOK_INIT API instead of
     adding explicit hooks to global security/security.c]
    Signed-off-by: Filipe Brandenburger <filbranden@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/334277
    Reviewed-on: https://chromium-review.googlesource.com/334278
    Reviewed-on: https://chromium-review.googlesource.com/334279
    
    Conflicts:
    	security/Makefile
    
    [rebase412(groeck):
    	Resolved conflicts; include file name changes; API changes]
    Signed-off-by: Guenter Roeck <groeck@chromium.org>
    
    [rebase414(groeck): Squashed two more commits:
    	CHROMIUM: fix a ChromiumOS-specific memory leak in fs/namei.c
    	CHROMIUM: LSM: handle errors from getname_flags() in
    	nameidata_set_temporary()]
    
    Signed-off-by: Guenter Roeck <groeck@chromium.org>
    
    [rebase419(groeck):
        kAPI changes:
    	fsnotify_add_mark_locked parameter change
    	FSNOTIFY_OBJ_ALL_TYPES -> FSNOTIFY_OBJ_ALL_TYPES_MASK
    	chromiumos_security_file_open() parameter change
        Squashed commits:
    	CHROMIUM: LSM: make modules FS pinning work with unshared mount namespace
    	CHROMIUM: LSM: fix buffer over-read in printable_cmdline
    	CHROMIUM: LSM: set MODULE_PARAM_PREFIX to "lsm."
    	CHROMIUM: LSM: Add support for symlink traversal policy.
    	CHROMIUM: LSM: Updates for v4.9
    	Block FIFO access on stateful partition.
    	CHROMIUM: LSM: add more details to blocked symlink mount
    	CHROMIUM: LSM: handle rcu-walk when following link
    	]
    Signed-off-by: Guenter Roeck <groeck@chromium.org>

---
+++ security/chromiumos/lsm.c
@@ -0,0 +1,133 @@
+/*
+ * Linux Security Module for Chromium OS
+ *
+ * Copyright 2011 Google Inc. All Rights Reserved
+ *
+ * Authors:
+ *      Stephan Uphoff  <ups@google.com>
+ *      Kees Cook       <keescook@chromium.org>
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#define pr_fmt(fmt) "Chromium OS LSM: " fmt
+
+#include <linux/module.h>
+#include <linux/security.h>
+#include <linux/lsm_hooks.h>
+#include <linux/sched.h>	/* current and other task related stuff */
+#include <linux/namei.h>	/* for nameidata_get_total_link_count */
+#include <linux/fs.h>
+#include <linux/fs_struct.h>
+#include <linux/mount.h>
+#include <linux/path.h>
+
+#include "inode_mark.h"
+#include "utils.h"
+
+static int chromiumos_security_sb_mount(const char *dev_name,
+					const struct path *path,
+					const char *type, unsigned long flags,
+					void *data)
+{
+#ifdef CONFIG_SECURITY_CHROMIUMOS_NO_SYMLINK_MOUNT
+	if (nameidata_get_total_link_count()) {
+		char *cmdline;
+		char *pathbuf;
+		char *pathlog;
+
+		cmdline = printable_cmdline(current);
+		/* We add 11 spaces for ' (deleted)' to be appended */
+		pathbuf = kmalloc(PATH_MAX + 11, GFP_KERNEL);
+		if (pathbuf) {
+			pathlog = d_path(path, pathbuf, PATH_MAX + 11);
+			if (IS_ERR(pathlog))
+				pathlog = "<too_long>";
+		} else
+			pathlog = "<no_memory>";
+		pr_notice("Mount path with symlinks prohibited - "
+			"pid=%d cmdline=%s dev=%s type=%s path=%s\n",
+			task_pid_nr(current), cmdline, dev_name, type, pathlog);
+		kfree(pathbuf);
+		kfree(cmdline);
+		return -ELOOP;
+	}
+#endif
+
+	return 0;
+}
+
+static int chromiumos_security_inode_follow_link(struct dentry *dentry,
+						 struct inode *inode, bool rcu)
+{
+	static char accessed_path[PATH_MAX];
+	enum chromiumos_inode_security_policy policy;
+
+	policy = chromiumos_get_inode_security_policy(
+		dentry, inode,
+		CHROMIUMOS_SYMLINK_TRAVERSAL);
+
+	/*
+	 * Emit a warning in cases of blocked symlink traversal attempts. These
+	 * will show up in kernel warning reports collected by the crash
+	 * reporter, so we have some insight on spurious failures that need
+	 * addressing.
+	 */
+	WARN(policy == CHROMIUMOS_INODE_POLICY_BLOCK,
+	     "Blocked symlink traversal for path %x:%x:%s (see https://goo.gl/8xICW6 for context and rationale)\n",
+	     MAJOR(dentry->d_sb->s_dev), MINOR(dentry->d_sb->s_dev),
+	     dentry_path(dentry, accessed_path, PATH_MAX));
+
+	return policy == CHROMIUMOS_INODE_POLICY_BLOCK ? -EACCES : 0;
+}
+
+static int chromiumos_security_file_open(struct file *file)
+{
+	static char accessed_path[PATH_MAX];
+	enum chromiumos_inode_security_policy policy;
+	struct dentry *dentry = file->f_path.dentry;
+
+	/* Returns 0 if file is not a FIFO */
+	if (!S_ISFIFO(file->f_inode->i_mode))
+		return 0;
+
+	policy = chromiumos_get_inode_security_policy(
+		dentry, dentry->d_inode,
+		CHROMIUMOS_FIFO_ACCESS);
+
+	/*
+	 * Emit a warning in cases of blocked fifo access attempts. These will
+	 * show up in kernel warning reports collected by the crash reporter,
+	 * so we have some insight on spurious failures that need addressing.
+	 */
+	WARN(policy == CHROMIUMOS_INODE_POLICY_BLOCK,
+	     "Blocked fifo access for path %x:%x:%s\n (see https://goo.gl/8xICW6 for context and rationale)\n",
+	     MAJOR(dentry->d_sb->s_dev), MINOR(dentry->d_sb->s_dev),
+	     dentry_path(dentry, accessed_path, PATH_MAX));
+
+	return policy == CHROMIUMOS_INODE_POLICY_BLOCK ? -EACCES : 0;
+}
+
+static struct security_hook_list chromiumos_security_hooks[] = {
+	LSM_HOOK_INIT(sb_mount, chromiumos_security_sb_mount),
+	LSM_HOOK_INIT(inode_follow_link, chromiumos_security_inode_follow_link),
+	LSM_HOOK_INIT(file_open, chromiumos_security_file_open),
+};
+
+static int __init chromiumos_security_init(void)
+{
+	security_add_hooks(chromiumos_security_hooks,
+			   ARRAY_SIZE(chromiumos_security_hooks), "chromiumos");
+
+	pr_info("enabled");
+
+	return 0;
+}
+security_initcall(chromiumos_security_init);
