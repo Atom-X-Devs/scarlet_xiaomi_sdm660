be0d623b9eb2bb8865429d5196103ee6dea04611
Steven Rostedt <rostedt@goodmis.org>
2016-12-06 17:50:30 -0500

    net: Have __napi_schedule_irqoff() disable interrupts on RT
    A customer hit a crash where the napi sd->poll_list became corrupted.
    The customer had the bnx2x driver, which does a
    __napi_schedule_irqoff() in its interrupt handler. Unfortunately, when
    running with CONFIG_PREEMPT_RT_FULL, this interrupt handler is run as a
    thread and is preemptable. The call to ____napi_schedule() must be done
    with interrupts disabled to protect the per cpu softnet_data's
    "poll_list, which is protected by disabling interrupts (disabling
    preemption is enough when all interrupts are threaded and
    local_bh_disable() can't preempt)."
    
    As bnx2x isn't the only driver that does this, the safest thing to do
    is to make __napi_schedule_irqoff() call __napi_schedule() instead when
    CONFIG_PREEMPT_RT_FULL is enabled, which will call local_irq_save()
    before calling ____napi_schedule().
    
    Cc: stable-rt@vger.kernel.org
    Signed-off-by: Steven Rostedt (Red Hat) <rostedt@goodmis.org>
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>

--- net/core/dev.c
+++ net/core/dev.c
@@ -5957,6 +5957,7 @@ bool napi_schedule_prep(struct napi_struct *n)
 }
 EXPORT_SYMBOL(napi_schedule_prep);
 
+#ifndef CONFIG_PREEMPT_RT_FULL
 /**
  * __napi_schedule_irqoff - schedule for receive
  * @n: entry to schedule
@@ -5975,6 +5976,7 @@ void __napi_schedule_irqoff(struct napi_struct *n)
 		__napi_schedule(n);
 }
 EXPORT_SYMBOL(__napi_schedule_irqoff);
+#endif
 
 bool napi_complete_done(struct napi_struct *n, int work_done)
 {
