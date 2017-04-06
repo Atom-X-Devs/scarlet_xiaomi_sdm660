a9dfadc8100fca5144e3524ce83411018446a700
Luigi Semenzato <semenzato@chromium.org>
2017-04-06 14:32:27 -0700

    CHROMIUM: add vmstat counts for different page fault types
    VM event counters are added to keep track of anonymous
    vs. file vs. shmem page faults.  They are: pgmajfault_a,
    pgmajfault_f and pgmajfault_s.  These are useful to
    analyze system performance, particularly when the cost
    of a fault for a file page is very different from that
    of an anonymous page, as would happen, for instance, in
    the presence of zram.
    
    The PGMAJFAULT counter is no longer directly maintained,
    for performance.  Instead the three new counters are added
    whenever the total count is needed.
    
    This adds entries to /proc/vmstat.  I considered adding them at
    the end for better compatibility, but the contents of /proc/vmstat
    are already highly variable depending on config options.  It is
    still worth asking whether the new counters should NOT contain
    "pgmajfault" as a substring of their name.
    
    BUG=chromium:718270
    TEST=checked that new fields are printed
    
    Reviewed-on: https://chromium-review.googlesource.com/470508
    Commit-Ready: Luigi Semenzato <semenzato@chromium.org>
    Tested-by: Luigi Semenzato <semenzato@chromium.org>
    Reviewed-by: Sonny Rao <sonnyrao@chromium.org>
    Reviewed-by: Justin TerAvest <teravest@chromium.org>
    Signed-off-by: Luigi Semenzato <semenzato@chromium.org>
    
    Change-Id: I639cdd543d9bd72996ac2c585f06ae9dd581acae
    Reviewed-on: https://chromium-review.googlesource.com/1583122
    Commit-Ready: Luigi Semenzato <semenzato@chromium.org>
    Tested-by: Luigi Semenzato <semenzato@chromium.org>
    Reviewed-by: Brian Geffon <bgeffon@chromium.org>
    Reviewed-by: Sonny Rao <sonnyrao@chromium.org>

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -3401,6 +3401,9 @@ static const unsigned int memcg1_events[] = {
 	PGPGOUT,
 	PGFAULT,
 	PGMAJFAULT,
+	PGMAJFAULT_S,
+	PGMAJFAULT_A,
+	PGMAJFAULT_F,
 };
 
 static const char *const memcg1_event_names[] = {
@@ -3408,6 +3411,9 @@ static const char *const memcg1_event_names[] = {
 	"pgpgout",
 	"pgfault",
 	"pgmajfault",
+	"pgmajfault_s",
+	"pgmajfault_a",
+	"pgmajfault_f",
 };
 
 static int memcg_stat_show(struct seq_file *m, void *v)
@@ -5635,6 +5641,9 @@ static int memory_stat_show(struct seq_file *m, void *v)
 
 	seq_printf(m, "pgfault %lu\n", acc.events[PGFAULT]);
 	seq_printf(m, "pgmajfault %lu\n", acc.events[PGMAJFAULT]);
+	seq_printf(m, "pgmajfault %lu\n", acc.events[PGMAJFAULT_S]);
+	seq_printf(m, "pgmajfault %lu\n", acc.events[PGMAJFAULT_A]);
+	seq_printf(m, "pgmajfault %lu\n", acc.events[PGMAJFAULT_F]);
 
 	seq_printf(m, "pgrefill %lu\n", acc.events[PGREFILL]);
 	seq_printf(m, "pgscan %lu\n", acc.events[PGSCAN_KSWAPD] +
