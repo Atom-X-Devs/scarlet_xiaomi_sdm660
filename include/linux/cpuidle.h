dac9f7e8f74be13def6f5c7006fca4c2bbb15f90
Derek Basehore <dbasehore@chromium.org>
2017-07-07 17:03:02 -0700

    FROMLIST: BACKPORT: freeze: Add error reporting
    This adds error reporting for cpuidle to freeze so suspend-to-idle can
    report errors when the CPU/SoC is unable to idle properly. Freeze will
    abort when an error is encountered.
    
    BUG=b:78841592
    TEST=apply all patches and verify the S0ix check works (is invoked and
    fails / passes as per the expectations)
    
    Note: Changed s2idle_wake() to pm_system_wakeup() to break the s2idle loop.
    
    (Applied from https://patchwork.kernel.org/patch/9831245/)
    
    Change-Id: I99267750fb5f55c02e0b9835560630121aa11cdc
    Suggested-by: Wenkai Du <wenkai.du@intel.com>
    Signed-off-by: Derek Basehore <dbasehore@chromium.org>
    Signed-off-by: Rajneesh Bhardwaj <rajneesh.bhardwaj@intel.com>
    Reviewed-on: https://chromium-review.googlesource.com/1106583
    Commit-Ready: Wenkai Du <wenkai.du@intel.corp-partner.google.com>
    Tested-by: Wenkai Du <wenkai.du@intel.com>
    Reviewed-by: Furquan Shaikh <furquan@chromium.org>
    
    Conflicts:
    	drivers/cpuidle/cpuidle.c
    
    [rebase419(groeck): Context conflicts;
    	squash: FIXUP: FROMLIST: BACKPORT: freeze: Add error reporting]
    Signed-off-by: Guenter Roeck <groeck@chromium.org>

--- include/linux/cpuidle.h
+++ include/linux/cpuidle.h
@@ -58,9 +58,9 @@ struct cpuidle_state {
 	/*
 	 * CPUs execute ->enter_s2idle with the local tick or entire timekeeping
 	 * suspended, so it must not re-enable interrupts at any point (even
-	 * temporarily) or attempt to change states of clock event devices.
+	 * temporarily). Returns 0 on success and non-zero if an error occurred.
 	 */
-	void (*enter_s2idle) (struct cpuidle_device *dev,
+	int (*enter_s2idle) (struct cpuidle_device *dev,
 			      struct cpuidle_driver *drv,
 			      int index);
 };
@@ -206,6 +206,8 @@ extern int cpuidle_find_deepest_state(struct cpuidle_driver *drv,
 extern int cpuidle_enter_s2idle(struct cpuidle_driver *drv,
 				struct cpuidle_device *dev);
 extern void cpuidle_use_deepest_state(bool enable);
+extern void cpuidle_prepare_freeze(void);
+extern int cpuidle_complete_freeze(void);
 #else
 static inline int cpuidle_find_deepest_state(struct cpuidle_driver *drv,
 					     struct cpuidle_device *dev)
@@ -216,6 +218,8 @@ static inline int cpuidle_enter_s2idle(struct cpuidle_driver *drv,
 static inline void cpuidle_use_deepest_state(bool enable)
 {
 }
+static inline void cpuidle_prepare_freeze(void) { }
+static inline int cpuidle_complete_freeze(void) { return -ENODEV; }
 #endif
 
 /* kernel/sched/idle.c */
