171d050c0d4c563c861be275ebd5483d964f66ef
Thierry Strudel <tstrudel@google.com>
2018-01-11 21:15:41 -0800

    power: qpnp-fg-gen3: increase battery temp reading precision
    Use the available 0.25C precision instead of the currently reported 1C.
    
    Change-Id: Ie38e57c8e996dd4ddb2a607068c27f56087ae43f
    Signed-off-by: Thierry Strudel <tstrudel@google.com>
    Signed-off-by: Rapherion Rollerscaperers <rapherion@raphielgang.org>
    Signed-off-by: Dakkshesh <dakkshesh5@gmail.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/power/supply/qcom/qpnp-fg-gen3.c
+++ drivers/power/supply/qcom/qpnp-fg-gen3.c
@@ -619,10 +619,8 @@ static int fg_get_battery_temp(struct fg_dev *fg, int *val)
 
 	temp = ((buf[1] & BATT_TEMP_MSB_MASK) << 8) |
 		(buf[0] & BATT_TEMP_LSB_MASK);
-	temp = DIV_ROUND_CLOSEST(temp, 4);
-
-	/* Value is in Kelvin; Convert it to deciDegC */
-	temp = (temp - 273) * 10;
+	/* Value is in 0.25Kelvin; Convert it to deciDegC */
+	*val = DIV_ROUND_CLOSEST((temp - 273*4) * 10, 4);
 #ifdef CONFIG_MACH_LONGCHEER
 #ifdef CONFIG_MACH_XIAOMI_WAYNE
 	if (temp < -80) {
@@ -658,7 +656,6 @@ static int fg_get_battery_temp(struct fg_dev *fg, int *val)
 		};
 	}
 #endif
-	*val = temp;
 	return 0;
 }
 
