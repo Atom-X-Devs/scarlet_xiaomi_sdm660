77a753cac93851b686effa5f480814bd34860129
Sayali Lokhande <sayalil@codeaurora.org>
2018-09-24 11:58:36 +0530

    FROMLIST: scsi: ufs: Add configfs support for UFS provisioning
    This patch adds configfs support to provision UFS device at
    runtime. This feature can be primarily useful in factory or
    assembly line as some devices may be required to be configured
    multiple times during initial system development phase.
    Configuration Descriptors can be written multiple times until
    bConfigDescrLock attribute is zero.
    
    Configuration descriptor buffer consists of Device and Unit
    descriptor configurable parameters which are parsed from vendor
    specific provisioning file and then passed via configfs node at
    runtime to provision ufs device.
    CONFIG_CONFIGFS_FS and CONFIG_SCSI_UFS_PROVISION needs to be enabled
    for using this feature.
    
    Usage:
    1) To read current configuration descriptor with index X
       (where index X can be 0/1/2/3) :
       cat /config/<device_name>/ufs_config_desc_X
    
    2) To write configuration descriptor with index X :
       echo <config_desc_buf> > /config/<device_name>/ufs_config_desc_X
    
    Signed-off-by: Sayali Lokhande <sayalil@codeaurora.org>
    (am from https://patchwork.kernel.org/patch/10611921/)
    
    BUG=b:111557905
    TEST=Build kernel for Cheza
    
    Change-Id: Ic2b29a0b9f9d0c41dbd9cb95a3654dd2ef2d8119
    Signed-off-by: Evan Green <evgreen@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/1241355
    Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    Reviewed-by: Douglas Anderson <dianders@chromium.org>
    
    Conflicts:
    	drivers/scsi/ufs/Kconfig
    Signed-off-by: Guenter Roeck <groeck@chromium.org>

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -7988,6 +7988,7 @@ EXPORT_SYMBOL(ufshcd_shutdown);
 void ufshcd_remove(struct ufs_hba *hba)
 {
 	ufs_sysfs_remove_nodes(hba->dev);
+	ufshcd_configfs_exit(hba);
 	scsi_remove_host(hba->host);
 	/* disable interrupts */
 	ufshcd_disable_intr(hba, hba->intr_mask);
@@ -8237,7 +8238,7 @@ int ufshcd_init(struct ufs_hba *hba, void __iomem *mmio_base, unsigned int irq)
 
 	async_schedule(ufshcd_async_scan, hba);
 	ufs_sysfs_add_nodes(hba->dev);
-
+	ufshcd_configfs_init(hba, dev_name(hba->dev));
 	return 0;
 
 out_remove_scsi_host:
