8af1c6cdaa7ed2dcb6283d693e54561cfb5e3ab1
Nick Fan <Nick.Fan@mediatek.com>
2018-11-02 16:40:30 +0800

    FROMLIST: opp: Add API for getting voltage from supplies
    Add API to get voltage for multiple supplies from opp table
    
    (am from https://patchwork.kernel.org/patch/10730753/)
    (also found at https://lkml.org/lkml/2018/12/4/114)
    
    BUG=b:109911488
    Test=build and boot to shell
    
    Change-Id: I6e3f4320429b9267e58bc34cc147b9adc41c88ec
    Signed-off-by: Nick Fan <Nick.Fan@mediatek.com>
    Reviewed-on: https://chromium-review.googlesource.com/1377753
    Commit-Ready: Nicolas Boichat <drinkcat@chromium.org>
    Tested-by: Nicolas Boichat <drinkcat@chromium.org>
    Reviewed-by: Nicolas Boichat <drinkcat@chromium.org>

--- include/linux/pm_opp.h
+++ include/linux/pm_opp.h
@@ -84,6 +84,9 @@ void dev_pm_opp_put_opp_table(struct opp_table *opp_table);
 
 unsigned long dev_pm_opp_get_voltage(struct dev_pm_opp *opp);
 
+unsigned long dev_pm_opp_get_voltage_supply(struct dev_pm_opp *opp,
+					    unsigned int index);
+
 unsigned long dev_pm_opp_get_freq(struct dev_pm_opp *opp);
 
 bool dev_pm_opp_is_turbo(struct dev_pm_opp *opp);
