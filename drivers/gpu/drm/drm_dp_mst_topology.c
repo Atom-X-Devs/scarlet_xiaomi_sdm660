733a3cdee3b850219b67ea2a6b95f816d24113a9
Lyude Paul <lyude@redhat.com>
2019-01-10 19:53:24 -0500

    FROMGIT: drm/dp_mst: Fix some formatting in drm_dp_add_port()
    Reindent some stuff, and split some stuff across multiple lines so we
    aren't going over the text width limit.
    
    Signed-off-by: Lyude Paul <lyude@redhat.com>
    Reviewed-by: Harry Wentland <harry.wentland@amd.com>
    Reviewed-by: Daniel Vetter <daniel@ffwll.ch>
    Cc: David Airlie <airlied@redhat.com>
    Cc: Jerry Zuo <Jerry.Zuo@amd.com>
    Cc: Juston Li <juston.li@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20190111005343.17443-2-lyude@redhat.com
    (cherry picked from commit 3d76df632d7f4ebc72338f56efb26d032ef74fb6)
    Signed-off-by: Lee, Shawn C <shawn.c.lee@intel.com>
    
    BUG=b:130706392
    TEST=Connect DP MST docking with 2 external monitor (1*4k, 1*FHD).
         DUT did not freeze or stuck and work normally.
    
    Change-Id: Ibb35f90835733a6922c9e6f3a083e76647f1f546
    Reviewed-on: https://chromium-review.googlesource.com/1598355
    Commit-Ready: Shawn Lee <shawn.c.lee@intel.corp-partner.google.com>
    Tested-by: Lucien Kao <lucien_kao@compal.corp-partner.google.com>
    Reviewed-by: Stéphane Marchesin <marcheu@chromium.org>
    Reviewed-by: Sean Paul <seanpaul@chromium.org>

--- drivers/gpu/drm/drm_dp_mst_topology.c
+++ drivers/gpu/drm/drm_dp_mst_topology.c
@@ -1183,11 +1183,13 @@ static void drm_dp_add_port(struct drm_dp_mst_branch *mstb,
 
 	if (old_ddps != port->ddps) {
 		if (port->ddps) {
-			if (!port->input)
-				drm_dp_send_enum_path_resources(mstb->mgr, mstb, port);
+			if (!port->input) {
+				drm_dp_send_enum_path_resources(mstb->mgr,
+								mstb, port);
+			}
 		} else {
 			port->available_pbn = 0;
-			}
+		}
 	}
 
 	if (old_pdt != port->pdt && !port->input) {
@@ -1201,8 +1203,11 @@ static void drm_dp_add_port(struct drm_dp_mst_branch *mstb,
 	if (created && !port->input) {
 		char proppath[255];
 
-		build_mst_prop_path(mstb, port->port_num, proppath, sizeof(proppath));
-		port->connector = (*mstb->mgr->cbs->add_connector)(mstb->mgr, port, proppath);
+		build_mst_prop_path(mstb, port->port_num, proppath,
+				    sizeof(proppath));
+		port->connector = (*mstb->mgr->cbs->add_connector)(mstb->mgr,
+								   port,
+								   proppath);
 		if (!port->connector) {
 			/* remove it from the port list */
 			mutex_lock(&mstb->mgr->lock);
@@ -1215,7 +1220,8 @@ static void drm_dp_add_port(struct drm_dp_mst_branch *mstb,
 		if ((port->pdt == DP_PEER_DEVICE_DP_LEGACY_CONV ||
 		     port->pdt == DP_PEER_DEVICE_SST_SINK) &&
 		    port->port_num >= DP_MST_LOGICAL_PORT_0) {
-			port->cached_edid = drm_get_edid(port->connector, &port->aux.ddc);
+			port->cached_edid = drm_get_edid(port->connector,
+							 &port->aux.ddc);
 			drm_connector_set_tile_property(port->connector);
 		}
 		(*mstb->mgr->cbs->register_connector)(port->connector);
