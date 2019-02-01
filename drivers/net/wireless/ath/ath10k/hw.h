2b3d1057bf044e442e9166536247be9185713d4b
Alagu Sankar <alagusankar@silex-india.com>
2019-02-01 10:17:09 +0800

    FROMGIT: ath10k: reduce transmit msdu count
    Reduce the transmit MSDU count for SDIO, to match with the descriptors
    as used by the firmware. This also acts as a high watermark level for
    transmit. Too many packets to the firmware results in transmit overflow
    interrupt.
    
    It only affect SDIO chip, it will not cause functionaly changes to
    other hardware.
    
    Tested with QCA6174 SDIO with firmware
    WLAN.RMH.4.4.1-00005-QCARMSWP-1.
    
    Signed-off-by: Alagu Sankar <alagusankar@silex-india.com>
    Signed-off-by: Wen Gong <wgong@codeaurora.org>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>
    
    (cherry picked from commit a73dbce3a6f41f07672956e44725f20b0b79053a
     https://git.kernel.org/pub/scm/linux/kernel/git/kvalo/ath.git ath-next)
    
    BUG=b:121980879
    TEST=Connect and ping success
    
    Change-Id: I392f4ee3486b6b9e8c7b1e5b716d7f8d6754394b
    Signed-off-by: Wen Gong <wgong@codeaurora.org>
    Signed-off-by: Grant Grundler <grundler@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/1477534
    Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    Tested-by: Wen Gong <wgong@qti-qualcomm.corp-partner.google.com>
    Reviewed-by: Sean Paul <seanpaul@chromium.org>

--- drivers/net/wireless/ath/ath10k/hw.h
+++ drivers/net/wireless/ath/ath10k/hw.h
@@ -722,6 +722,7 @@ ath10k_rx_desc_get_l3_pad_bytes(struct ath10k_hw_params *hw,
 #define TARGET_TLV_NUM_TDLS_VDEVS		1
 #define TARGET_TLV_NUM_TIDS			((TARGET_TLV_NUM_PEERS) * 2)
 #define TARGET_TLV_NUM_MSDU_DESC		(1024 + 32)
+#define TARGET_TLV_NUM_MSDU_DESC_HL		64
 #define TARGET_TLV_NUM_WOW_PATTERNS		22
 #define TARGET_TLV_MGMT_NUM_MSDU_DESC		(50)
 
