34d5b81074dab81a4e18c8b5275c3eef5da7151f
Hans Verkuil <hverkuil-cisco@xs4all.nl>
2019-02-07 06:49:43 -0500

    UPSTREAM: media: vb2: fix epoll() by calling poll_wait first
    The epoll function expects that whenever the poll file op is
    called, the poll_wait function is also called. That didn't
    always happen in vb2_core_poll() and vb2_poll(). Fix this,
    otherwise epoll() would timeout when it shouldn't.
    
    BUG=b:123551776
    TEST=Build Kukui image.
    
    Signed-off-by: Hans Verkuil <hverkuil-cisco@xs4all.nl>
    Reported-by: Yi Qingliang <niqingliang2003@gmail.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+samsung@kernel.org>
    (cherry picked from commit 914c68686441c93f6954267dabef6c936ebb479f)
    Signed-off-by: Alexandre Courbot <acourbot@chromium.org>
    
    Change-Id: I708a8ccf3dbc0351763e6cc7295e4a08499a367e
    Reviewed-on: https://chromium-review.googlesource.com/1619667
    Commit-Ready: Alexandre Courbot <acourbot@chromium.org>
    Tested-by: Tomasz Figa <tfiga@chromium.org>
    Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>
    Reviewed-by: Tomasz Figa <tfiga@chromium.org>
    Reviewed-by: Sean Paul <seanpaul@chromium.org>

--- drivers/media/common/videobuf2/videobuf2-core.c
+++ drivers/media/common/videobuf2/videobuf2-core.c
@@ -2268,6 +2268,8 @@ __poll_t vb2_core_poll(struct vb2_queue *q, struct file *file,
 	if (q->is_output && !(req_events & (EPOLLOUT | EPOLLWRNORM)))
 		return 0;
 
+	poll_wait(file, &q->done_wq, wait);
+
 	/*
 	 * Start file I/O emulator only if streaming API has not been used yet.
 	 */
@@ -2319,8 +2321,6 @@ __poll_t vb2_core_poll(struct vb2_queue *q, struct file *file,
 		 */
 		if (q->last_buffer_dequeued)
 			return EPOLLIN | EPOLLRDNORM;
-
-		poll_wait(file, &q->done_wq, wait);
 	}
 
 	/*
