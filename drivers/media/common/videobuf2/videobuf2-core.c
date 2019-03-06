ad139eba5eb9fdab190548a03d1fa2c228ddd4b6
Hans Verkuil <hverkuil-cisco@xs4all.nl>
2019-03-06 16:13:21 -0500

    UPSTREAM: media: vb2: add requires_requests bit for stateless codecs
    Stateless codecs require the use of the Request API as opposed of it
    being optional.
    
    So add a bit to indicate this and let vb2 check for this.
    
    If an attempt is made to queue a buffer without an associated request,
    then the EBADR error is returned to userspace.
    
    Doing this check in the vb2 core simplifies drivers, since they
    don't have to check for this, they can just set this flag.
    
    BUG=b:123551776
    TEST=Build Kukui image.
    
    Signed-off-by: Hans Verkuil <hverkuil-cisco@xs4all.nl>
    Reviewed-by: Paul Kocialkowski <paul.kocialkowski@bootlin.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+samsung@kernel.org>
    (cherry picked from commit 90675d391e9196e95a9e9e31eaa9693d4a995927)
    Signed-off-by: Alexandre Courbot <acourbot@chromium.org>
    
    Change-Id: Id9cf3789737c93d07eb27452d72ddb95ad0e8c00
    Reviewed-on: https://chromium-review.googlesource.com/1619670
    Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    Tested-by: Tomasz Figa <tfiga@chromium.org>
    Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>
    Reviewed-by: Tomasz Figa <tfiga@chromium.org>
    Reviewed-by: Sean Paul <seanpaul@chromium.org>

--- drivers/media/common/videobuf2/videobuf2-core.c
+++ drivers/media/common/videobuf2/videobuf2-core.c
@@ -1470,6 +1470,12 @@ int vb2_core_qbuf(struct vb2_queue *q, unsigned int index, void *pb,
 
 	vb = q->bufs[index];
 
+	if (!req && vb->state != VB2_BUF_STATE_IN_REQUEST &&
+	    q->requires_requests) {
+		dprintk(1, "qbuf requires a request\n");
+		return -EBADR;
+	}
+
 	if ((req && q->uses_qbuf) ||
 	    (!req && vb->state != VB2_BUF_STATE_IN_REQUEST &&
 	     q->uses_requests)) {
@@ -2213,6 +2219,9 @@ int vb2_core_queue_init(struct vb2_queue *q)
 	    WARN_ON(!q->io_modes)	  ||
 	    WARN_ON(!q->ops->queue_setup) ||
 	    WARN_ON(!q->ops->buf_queue))
+		return -EINVAL;
+
+	if (WARN_ON(q->requires_requests && !q->supports_requests))
 		return -EINVAL;
 
 	INIT_LIST_HEAD(&q->queued_list);
