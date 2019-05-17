cac9000677181e63da9453cbcb8d7e1b4d31a673
Ap, Kamal <kamal.ap@intel.corp-partner.google.com>
2019-05-17 16:20:23 +0530

    Revert "CHROMIUM: ASoC: Intel: Add acpi match cml m/c driver"
    This reverts commit 792e9cf70d9dbed1a76a84a754d8700c8e39d694.
    
    BUG=b:134688240
    TEST=Test Audio use cases for CML with full SOF patch series applied.
    
    Signed-off-by: Ap, Kamal <kamal.ap@intel.corp-partner.google.com>

--- sound/soc/intel/common/soc-acpi-intel-cnl-match.c
+++ sound/soc/intel/common/soc-acpi-intel-cnl-match.c
@@ -14,11 +14,6 @@ static struct skl_machine_pdata cnl_pdata = {
 	.use_tplg_pcm = true,
 };
 
-static struct snd_soc_acpi_codecs cml_codecs = {
-	.num_codecs = 1,
-	.codecs = {"MX98357A"}
-};
-
 struct snd_soc_acpi_mach snd_soc_acpi_intel_cnl_machines[] = {
 	{
 		.id = "INT34C2",
@@ -33,13 +28,6 @@ struct snd_soc_acpi_mach snd_soc_acpi_intel_cnl_machines[] = {
 		.drv_name = "cnl_max98373",
 		.fw_filename = "intel/dsp_fw_cnl.bin",
 		.pdata = &cnl_pdata,
-	},
-	{
-		.id = "10EC5682",
-		.drv_name = "cml_rt5682_max98357a",
-		.quirk_data = &cml_codecs,
-		.sof_fw_filename = "sof-cnl.ri",
-		.sof_tplg_filename = "sof-cml-rt5682_max98357a.tplg",
 	},
 	{},
 };
