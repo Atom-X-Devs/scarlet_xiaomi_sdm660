ea201dbf69c6a12461aff4b438a39921825ab8ae
Nick Crews <ncrews@chromium.org>
2019-05-28 16:57:09 -0600

    FIXUP: CHROMIUM: platform/chrome: wilco_ec: Add charge scheduling sysfs
    There were several mistakes in the initial implementation:
    
    The EC property IDs were all messed up in the initial commit.
    This corrects them to follow the specification at
    https://docs.google.com/document/d/1lNgipPdQJySZ8VIPtM0UO0SGeupBR7Vbdu-obFmZ8mI
    
    The EC interface specification was incorrect in the format for the
    Advanced Charging arguments: http://issuetracker.google.com/133749406
    This fixes the format.
    
    Also, this has a couple of improvements:
    - Refactor ALL of the getters and setters into charge_schedule.h,
      so that charge_schedule_sysfs doesn't need to know about property IDs
    - Split up the Advanced Charging and Peak Shift attributes into
      separate directories with the attribute_group struct.
    - Naming tweaks and some other cleanup.
    
    BUG=b:132781159, b:133763518, b:133749406
    TEST=Using a USB-C power meter, playing with the
    policies has an effect on the AC power draw
    
    Change-Id: I253c3516e8beaa050fb842fad0c267735d4174d8
    Signed-off-by: Nick Crews <ncrews@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/1633254
    Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>
    Reviewed-by: Simon Glass <sjg@chromium.org>

--- drivers/platform/chrome/wilco_ec/charge_schedule_sysfs.c
+++ drivers/platform/chrome/wilco_ec/charge_schedule_sysfs.c
@@ -18,66 +18,87 @@
 
 #define DRV_NAME "wilco-charge-schedule"
 
-struct wilco_property_attribute {
-	struct device_attribute dev_attr;
-	u32 property_id;
-};
-
-static ssize_t
-bool_property_show(struct device *dev, struct device_attribute *attr, char *buf)
+static ssize_t peak_shift_enable_show(struct device *dev,
+				      struct device_attribute *attr, char *buf)
 {
 	struct wilco_ec_device *ec = dev_get_platdata(dev);
-	struct wilco_property_attribute *wpa;
-	u8 val;
+	bool enabled;
 	int ret;
 
-	wpa = container_of(attr, struct wilco_property_attribute, dev_attr);
-	ret = wilco_ec_get_byte_property(ec, wpa->property_id, &val);
+	ret = wilco_ec_get_peak_shift_enable(ec, &enabled);
 	if (ret < 0)
 		return ret;
 
-	return snprintf(buf, PAGE_SIZE, "%d\n", val);
+	return snprintf(buf, PAGE_SIZE, "%d\n", enabled);
 }
 
-static ssize_t
-bool_property_store(struct device *dev, struct device_attribute *attr,
-		    const char *buf, size_t count)
+static ssize_t peak_shift_enable_store(struct device *dev,
+				       struct device_attribute *attr,
+				       const char *buf, size_t count)
 {
 	struct wilco_ec_device *ec = dev_get_platdata(dev);
-	struct wilco_property_attribute *wpa;
-	bool val;
+	bool enable;
 	int ret;
 
-	if (strtobool(buf, &val) < 0)
+	if (strtobool(buf, &enable) < 0)
 		return -EINVAL;
 
-	wpa = container_of(attr, struct wilco_property_attribute, dev_attr);
-	ret = wilco_ec_set_byte_property(ec, wpa->property_id, (u8) val);
+	ret = wilco_ec_set_peak_shift_enable(ec, enable);
 	if (ret < 0)
 		return ret;
 
 	return count;
 }
 
-#define BOOL_PROPERTY_ATTR(_name, _property_id)				\
-	struct wilco_property_attribute _name =	{			\
-		__ATTR(_name, 0644, bool_property_show, bool_property_store), \
-		_property_id						\
-	}
+struct device_attribute dev_attr_peak_shift_enable =
+		__ATTR(enable, 0644,
+		       peak_shift_enable_show, peak_shift_enable_store);
 
-BOOL_PROPERTY_ATTR(peak_shift_enable, PID_PEAK_SHIFT);
-BOOL_PROPERTY_ATTR(advanced_charging_enable, PID_ADV_CHARGING);
+static ssize_t advanced_charging_enable_show(struct device *dev,
+					     struct device_attribute *attr,
+					     char *buf)
+{
+	struct wilco_ec_device *ec = dev_get_platdata(dev);
+	bool enabled;
+	int ret;
+
+	ret = wilco_ec_get_adv_charging_enable(ec, &enabled);
+	if (ret < 0)
+		return ret;
+
+	return snprintf(buf, PAGE_SIZE, "%d\n", enabled);
+}
+static ssize_t advanced_charging_enable_store(struct device *dev,
+					      struct device_attribute *attr,
+					      const char *buf, size_t count)
+{
+	struct wilco_ec_device *ec = dev_get_platdata(dev);
+	bool enable;
+	int ret;
+
+	if (strtobool(buf, &enable) < 0)
+		return -EINVAL;
+
+	ret = wilco_ec_set_adv_charging_enable(ec, enable);
+	if (ret < 0)
+		return ret;
+
+	return count;
+}
+
+struct device_attribute dev_attr_advanced_charging_enable =
+		__ATTR(enable, 0644,
+		       advanced_charging_enable_show,
+		       advanced_charging_enable_store);
 
 static ssize_t
 peak_shift_battery_threshold_show(struct device *dev,
 				  struct device_attribute *attr, char *buf)
 {
 	struct wilco_ec_device *ec = dev_get_platdata(dev);
-	u8 val;
-	int ret;
+	int ret, val;
 
-	ret = wilco_ec_get_byte_property(ec, PID_PEAK_SHIFT_BATTERY_THRESHOLD,
-					 &val);
+	ret = wilco_ec_get_peak_shift_battery_threshold(ec, &val);
 	if (ret < 0)
 		return ret;
 
@@ -90,24 +111,22 @@ peak_shift_battery_threshold_store(struct device *dev,
 				   const char *buf, size_t count)
 {
 	struct wilco_ec_device *ec = dev_get_platdata(dev);
-	u8 val;
-	int ret;
+	int ret, val;
 
-	if (kstrtou8(buf, 10, &val) < 0)
-		return -EINVAL;
-	if (val < PEAK_SHIFT_BATTERY_THRESHOLD_MIN ||
-	    val > PEAK_SHIFT_BATTERY_THRESHOLD_MAX)
+	if (kstrtoint(buf, 10, &val) < 0)
 		return -EINVAL;
 
-	ret = wilco_ec_set_byte_property(ec, PID_PEAK_SHIFT_BATTERY_THRESHOLD,
-					 val);
+	ret = wilco_ec_set_peak_shift_battery_threshold(ec, val);
 	if (ret < 0)
 		return ret;
 
 	return count;
 }
 
-DEVICE_ATTR_RW(peak_shift_battery_threshold);
+struct device_attribute dev_attr_peak_shift_battery_threshold =
+		__ATTR(battery_threshold, 0644,
+		       peak_shift_battery_threshold_show,
+		       peak_shift_battery_threshold_store);
 
 struct wilco_schedule_attribute {
 	struct device_attribute dev_attr;
@@ -117,9 +136,9 @@ struct wilco_schedule_attribute {
 #define to_wilco_schedule_attr(_dev_attr) \
 	container_of(_dev_attr, struct wilco_schedule_attribute, dev_attr)
 
-static ssize_t
-advanced_charging_show(struct device *dev, struct device_attribute *attr,
-		       char *buf)
+static ssize_t advanced_charging_schedule_show(struct device *dev,
+					       struct device_attribute *attr,
+					       char *buf)
 {
 	struct wilco_ec_device *ec = dev_get_platdata(dev);
 	struct wilco_schedule_attribute *wsa;
@@ -133,13 +152,13 @@ advanced_charging_show(struct device *dev, struct device_attribute *attr,
 		return ret;
 
 	return snprintf(buf, PAGE_SIZE, "%02d:%02d %02d:%02d\n",
-			sched.start_hours, sched.start_minutes,
-			sched.duration_hours, sched.duration_minutes);
+			sched.start_hour, sched.start_minute,
+			sched.duration_hour, sched.duration_minute);
 }
 
-static ssize_t
-advanced_charging_store(struct device *dev, struct device_attribute *attr,
-			const char *buf, size_t count)
+static ssize_t advanced_charging_schedule_store(struct device *dev,
+						struct device_attribute *attr,
+						const char *buf, size_t count)
 {
 	struct wilco_ec_device *ec = dev_get_platdata(dev);
 	struct wilco_schedule_attribute *wsa;
@@ -147,8 +166,8 @@ advanced_charging_store(struct device *dev, struct device_attribute *attr,
 	int ret;
 
 	ret = sscanf(buf, "%d:%d %d:%d",
-		     &sched.start_hours, &sched.start_minutes,
-		     &sched.duration_hours, &sched.duration_minutes);
+		     &sched.start_hour, &sched.start_minute,
+		     &sched.duration_hour, &sched.duration_minute);
 	if (ret != 4)
 		return -EINVAL;
 
@@ -161,23 +180,42 @@ advanced_charging_store(struct device *dev, struct device_attribute *attr,
 	return count;
 }
 
-#define ADVANCED_CHARGING_ATTR(_name, _day_of_week)			\
-	struct wilco_property_attribute adv_charging_attr_##_name = {	\
-		__ATTR(advanced_charging_##_name, 0644,			\
-		       advanced_charging_show, advanced_charging_store),\
-		_day_of_week						\
+#define ADVANCED_CHARGING_SCHED_ATTR(_name, _day_of_week)		\
+	struct wilco_schedule_attribute adv_charging_sched_attr_##_name = { \
+		.dev_attr = __ATTR(_name, 0644,				\
+				   advanced_charging_schedule_show,	\
+				   advanced_charging_schedule_store),	\
+		.day_of_week = _day_of_week				\
 	}
 
-ADVANCED_CHARGING_ATTR(sunday, 0);
-ADVANCED_CHARGING_ATTR(monday, 1);
-ADVANCED_CHARGING_ATTR(tuesday, 2);
-ADVANCED_CHARGING_ATTR(wednesday, 3);
-ADVANCED_CHARGING_ATTR(thursday, 4);
-ADVANCED_CHARGING_ATTR(friday, 5);
-ADVANCED_CHARGING_ATTR(saturday, 6);
+ADVANCED_CHARGING_SCHED_ATTR(sunday, 0);
+ADVANCED_CHARGING_SCHED_ATTR(monday, 1);
+ADVANCED_CHARGING_SCHED_ATTR(tuesday, 2);
+ADVANCED_CHARGING_SCHED_ATTR(wednesday, 3);
+ADVANCED_CHARGING_SCHED_ATTR(thursday, 4);
+ADVANCED_CHARGING_SCHED_ATTR(friday, 5);
+ADVANCED_CHARGING_SCHED_ATTR(saturday, 6);
 
-static ssize_t
-peak_shift_show(struct device *dev, struct device_attribute *attr, char *buf)
+static struct attribute *wilco_advanced_charging_attrs[] = {
+	&dev_attr_advanced_charging_enable.attr,
+	&adv_charging_sched_attr_sunday.dev_attr.attr,
+	&adv_charging_sched_attr_monday.dev_attr.attr,
+	&adv_charging_sched_attr_tuesday.dev_attr.attr,
+	&adv_charging_sched_attr_wednesday.dev_attr.attr,
+	&adv_charging_sched_attr_thursday.dev_attr.attr,
+	&adv_charging_sched_attr_friday.dev_attr.attr,
+	&adv_charging_sched_attr_saturday.dev_attr.attr,
+	NULL,
+};
+
+static struct attribute_group wilco_advanced_charging_attr_group = {
+	.name = "advanced_charging",
+	.attrs = wilco_advanced_charging_attrs,
+};
+
+static ssize_t peak_shift_schedule_show(struct device *dev,
+					struct device_attribute *attr,
+					char *buf)
 {
 	struct wilco_ec_device *ec = dev_get_platdata(dev);
 	struct wilco_schedule_attribute *wsa;
@@ -191,14 +229,14 @@ peak_shift_show(struct device *dev, struct device_attribute *attr, char *buf)
 		return ret;
 
 	return snprintf(buf, PAGE_SIZE, "%02d:%02d %02d:%02d %02d:%02d\n",
-			sched.start_hours, sched.start_minutes,
-			sched.end_hours, sched.end_minutes,
-			sched.charge_start_hours, sched.charge_start_minutes);
+			sched.start_hour, sched.start_minute,
+			sched.end_hour, sched.end_minute,
+			sched.charge_start_hour, sched.charge_start_minute);
 }
 
-static ssize_t
-peak_shift_store(struct device *dev, struct device_attribute *attr,
-		 const char *buf, size_t count)
+static ssize_t peak_shift_schedule_store(struct device *dev,
+					 struct device_attribute *attr,
+					 const char *buf, size_t count)
 {
 	struct wilco_ec_device *ec = dev_get_platdata(dev);
 	struct wilco_schedule_attribute *wsa;
@@ -206,9 +244,9 @@ peak_shift_store(struct device *dev, struct device_attribute *attr,
 	int ret;
 
 	ret = sscanf(buf, "%d:%d %d:%d %d:%d",
-		     &sched.start_hours, &sched.start_minutes,
-		     &sched.end_hours, &sched.end_minutes,
-		     &sched.charge_start_hours, &sched.charge_start_minutes);
+		     &sched.start_hour, &sched.start_minute,
+		     &sched.end_hour, &sched.end_minute,
+		     &sched.charge_start_hour, &sched.charge_start_minute);
 	if (ret != 6)
 		return -EINVAL;
 
@@ -221,51 +259,50 @@ peak_shift_store(struct device *dev, struct device_attribute *attr,
 	return count;
 }
 
-#define PEAK_SHIFT_ATTR(_name, _day_of_week)				\
-	struct wilco_property_attribute peak_shift_attr_##_name = {	\
-		__ATTR(peak_shift_##_name, 0644,			\
-		       peak_shift_show, peak_shift_store),		\
-		_day_of_week						\
+#define PEAK_SHIFT_SCHED_ATTR(_name, _day_of_week)			\
+	struct wilco_schedule_attribute peak_shift_sched_attr_##_name = { \
+		.dev_attr = __ATTR(_name, 0644,				\
+				   peak_shift_schedule_show,		\
+				   peak_shift_schedule_store),		\
+		.day_of_week = _day_of_week				\
 	}
 
-PEAK_SHIFT_ATTR(sunday, 0);
-PEAK_SHIFT_ATTR(monday, 1);
-PEAK_SHIFT_ATTR(tuesday, 2);
-PEAK_SHIFT_ATTR(wednesday, 3);
-PEAK_SHIFT_ATTR(thursday, 4);
-PEAK_SHIFT_ATTR(friday, 5);
-PEAK_SHIFT_ATTR(saturday, 6);
+PEAK_SHIFT_SCHED_ATTR(sunday, 0);
+PEAK_SHIFT_SCHED_ATTR(monday, 1);
+PEAK_SHIFT_SCHED_ATTR(tuesday, 2);
+PEAK_SHIFT_SCHED_ATTR(wednesday, 3);
+PEAK_SHIFT_SCHED_ATTR(thursday, 4);
+PEAK_SHIFT_SCHED_ATTR(friday, 5);
+PEAK_SHIFT_SCHED_ATTR(saturday, 6);
 
-static struct attribute *wilco_charge_schedule_attrs[] = {
-	&advanced_charging_enable.dev_attr.attr,
-	&adv_charging_attr_sunday.dev_attr.attr,
-	&adv_charging_attr_monday.dev_attr.attr,
-	&adv_charging_attr_tuesday.dev_attr.attr,
-	&adv_charging_attr_wednesday.dev_attr.attr,
-	&adv_charging_attr_thursday.dev_attr.attr,
-	&adv_charging_attr_friday.dev_attr.attr,
-	&adv_charging_attr_saturday.dev_attr.attr,
-
-	&peak_shift_enable.dev_attr.attr,
+static struct attribute *wilco_peak_shift_attrs[] = {
+	&dev_attr_peak_shift_enable.attr,
 	&dev_attr_peak_shift_battery_threshold.attr,
-	&peak_shift_attr_sunday.dev_attr.attr,
-	&peak_shift_attr_monday.dev_attr.attr,
-	&peak_shift_attr_tuesday.dev_attr.attr,
-	&peak_shift_attr_wednesday.dev_attr.attr,
-	&peak_shift_attr_thursday.dev_attr.attr,
-	&peak_shift_attr_friday.dev_attr.attr,
-	&peak_shift_attr_saturday.dev_attr.attr,
+	&peak_shift_sched_attr_sunday.dev_attr.attr,
+	&peak_shift_sched_attr_monday.dev_attr.attr,
+	&peak_shift_sched_attr_tuesday.dev_attr.attr,
+	&peak_shift_sched_attr_wednesday.dev_attr.attr,
+	&peak_shift_sched_attr_thursday.dev_attr.attr,
+	&peak_shift_sched_attr_friday.dev_attr.attr,
+	&peak_shift_sched_attr_saturday.dev_attr.attr,
 	NULL,
 };
 
-static struct attribute_group wilco_charge_schedule_attr_group = {
-	.attrs = wilco_charge_schedule_attrs,
+static struct attribute_group wilco_peak_shift_attr_group = {
+	.name = "peak_shift",
+	.attrs = wilco_peak_shift_attrs,
+};
+
+static const struct attribute_group *wilco_charge_schedule_attr_groups[] = {
+	&wilco_advanced_charging_attr_group,
+	&wilco_peak_shift_attr_group,
+	NULL
 };
 
 static int wilco_charge_schedule_probe(struct platform_device *pdev)
 {
-	return devm_device_add_group(&pdev->dev,
-				     &wilco_charge_schedule_attr_group);
+	return devm_device_add_groups(&pdev->dev,
+				      wilco_charge_schedule_attr_groups);
 }
 
 static struct platform_driver wilco_charge_schedule_driver = {
