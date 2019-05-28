2a03de2e808fb3d9128af026c300df7714c0a4d8
Nick Crews <ncrews@chromium.org>
2019-05-28 16:57:09 -0600

    FIXUP: CHROMIUM: platform/chrome: wilco_ec: Add charge scheduling backend
    There were several mistakes in the initial implementation:
    
    The EC property IDs were all messed up in the initial commit.
    This corrects them to follow the specification at
    https://docs.google.com/document/d/1lNgipPdQJySZ8VIPtM0UO0SGeupBR7Vbdu-obFmZ8mI
    
    The EC interface specification was incorrect in the format for the
    Advanced Charging arguments: http://issuetracker.google.com/133749406
    This fixes the format.
    
    Also, this has a couple of improvements:
    - Refactor ALL of the getters and setters into charge_schedule.h,
      so that charge_schedule_sysfs doesn't need to know about property IDs
    - Split up the Advanced Charging and Peak Shift attributes into
      separate directories with the attribute_group struct.
    - Naming tweaks and some other cleanup.
    
    BUG=b:132781159, b:133763518, b:133749406
    TEST=Using a USB-C power meter, playing with the
    policies has an effect on the AC power draw
    
    Change-Id: Idc2b85d246a7646e1795ecd1cb6bf802f78cbcc1
    Signed-off-by: Nick Crews <ncrews@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/1633253
    Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>
    Reviewed-by: Simon Glass <sjg@chromium.org>

--- drivers/platform/chrome/wilco_ec/charge_schedule.c
+++ drivers/platform/chrome/wilco_ec/charge_schedule.c
@@ -10,19 +10,36 @@
 #include <linux/platform_data/wilco-ec.h>
 #include "charge_schedule.h"
 
+/* Property IDs and related EC constants */
+#define PID_PEAK_SHIFT				0x04EA
+#define PID_PEAK_SHIFT_BATTERY_THRESHOLD	0x04EB
+#define PID_PEAK_SHIFT_SUNDAY			0x04EE
+#define PID_ADV_CHARGING			0x04ED
+#define PID_ADV_CHARGING_SUNDAY			0x04F5
+
 /*
  * Date and hour information is passed to/from the EC using packed bytes,
  * where each byte represents an hour and a minute that some event occurs.
  * The minute field supports quarter-hour intervals, so either
  * 0, 15, 30, or 45. This allows this info to be packed within 2 bits.
  * Along with the 5 bits of hour info [0-23], this gives us 7 used bits
- * within each packed byte.
+ * within each packed byte:
+ * +---------------+
+ * |7|6|5|4|3|2|1|0|
+ * +---------------+
+ * |X|  hour   |min|
+ * +---------------+
  */
 
+#define MINUTE_POSITION	0	/* bits[0:1] */
+#define MINUTE_MASK	0x03	/* 0b00000011 */
+#define HOUR_POSITION	2	/* bits[2:6] */
+#define HOUR_MASK	0x7c	/* 0b01111100 */
+
 struct adv_charging_payload {
-	u16 RESERVED;
-	u8 duration_time;
 	u8 start_time;
+	u8 duration_time;
+	u16 RESERVED;
 } __packed;
 
 struct peak_shift_payload {
@@ -32,48 +49,27 @@ struct peak_shift_payload {
 	u8 RESERVED;
 } __packed;
 
-/*
- * Peak Shift and Advanced Charging properties each pack the hour and
- * minute info into the bytes differently, hence this struct. For instance,
- * with Peak Shift, the minute info is bits 6 and 7 and the hour info is
- * bits 1 through 5, but with Advanced Charging the minute info is bits 0 and 1
- * and the hour info is bits 2 through 6.
- */
-struct time_bcd_format {
-	u8 hour_position;
-	u8 minute_position;
-};
-
-const struct time_bcd_format ADV_CHARGING_BCD_FORMAT = {
-	.minute_position = 0,	// bits[0:1]
-	.hour_position = 2	// bits[2:6]
-				// bit[7] is unused
-};
-
-const struct time_bcd_format PEAK_SHIFT_BCD_FORMAT = {
-				// bit[0] is unused
-	.hour_position = 1,	// bits[1:5]
-	.minute_position = 6	// bits[6:7]
-};
-
 /* Pack hour and minute info into a byte. */
-static u8 pack_field(struct time_bcd_format fmt, int hours, int quarter_hour)
+static u8 pack_field(int hour, int minute)
 {
 	int result = 0;
+	int quarter_hour;
 
-	quarter_hour /= 15;
-	result |= hours << fmt.hour_position;
-	result |= quarter_hour << fmt.minute_position;
+	quarter_hour = minute / 15;
+	result |= hour << HOUR_POSITION;
+	result |= quarter_hour << MINUTE_POSITION;
+
 	return (u8)result;
 }
 
 /* Extract hour and minute info from a byte. */
-static void unpack_field(struct time_bcd_format fmt, u8 field, int *hour,
-			 int *quarter_hour)
+static void unpack_field(int *hour, int *minute, u8 field)
 {
-	*hour =		(field >> fmt.hour_position)   & 0x1f; // 00011111
-	*quarter_hour = (field >> fmt.minute_position) & 0x03; // 00000011
-	*quarter_hour *= 15;
+	int quarter_hour;
+
+	*hour =		(field & HOUR_MASK)	>> HOUR_POSITION;
+	quarter_hour =	(field & MINUTE_MASK)	>> MINUTE_POSITION;
+	*minute = quarter_hour * 15;
 }
 
 #define hour_valid(h)   (h >= 0 && h < 24)
@@ -82,21 +78,21 @@ static void unpack_field(struct time_bcd_format fmt, u8 field, int *hour,
 static bool
 is_adv_charging_sched_valid(const struct adv_charge_schedule *sched)
 {
-	return (hour_valid(sched->start_hours) &&
-		hour_valid(sched->duration_hours) &&
-		minute_valid(sched->start_minutes) &&
-		minute_valid(sched->duration_minutes));
+	return (hour_valid(sched->start_hour) &&
+		hour_valid(sched->duration_hour) &&
+		minute_valid(sched->start_minute) &&
+		minute_valid(sched->duration_minute));
 }
 
 static bool
 is_peak_shift_schedule_valid(const struct peak_shift_schedule *sched)
 {
-	return (hour_valid(sched->start_hours) &&
-		hour_valid(sched->end_hours) &&
-		hour_valid(sched->charge_start_hours) &&
-		minute_valid(sched->start_minutes) &&
-		minute_valid(sched->end_minutes) &&
-		minute_valid(sched->charge_start_minutes));
+	return (hour_valid(sched->start_hour) &&
+		hour_valid(sched->end_hour) &&
+		hour_valid(sched->charge_start_hour) &&
+		minute_valid(sched->start_minute) &&
+		minute_valid(sched->end_minute) &&
+		minute_valid(sched->charge_start_minute));
 }
 
 int wilco_ec_get_adv_charge_schedule(struct wilco_ec_device *ec,
@@ -112,10 +108,10 @@ int wilco_ec_get_adv_charge_schedule(struct wilco_ec_device *ec,
 		return ret;
 
 	payload = (struct adv_charging_payload *) msg.data;
-	unpack_field(ADV_CHARGING_BCD_FORMAT, payload->start_time,
-		     &sched->start_hours, &sched->start_minutes);
-	unpack_field(ADV_CHARGING_BCD_FORMAT, payload->duration_time,
-		     &sched->duration_hours, &sched->duration_minutes);
+	unpack_field(&sched->start_hour, &sched->start_minute,
+		     payload->start_time);
+	unpack_field(&sched->duration_hour, &sched->duration_minute,
+		     payload->duration_time);
 
 	return 0;
 }
@@ -131,12 +127,10 @@ int wilco_ec_set_adv_charge_schedule(struct wilco_ec_device *ec,
 
 	payload = (struct adv_charging_payload *)msg.data;
 	memset(payload, 0, sizeof(*payload));
-	payload->start_time = pack_field(ADV_CHARGING_BCD_FORMAT,
-					 sched->start_hours,
-					 sched->start_minutes);
-	payload->duration_time = pack_field(ADV_CHARGING_BCD_FORMAT,
-					    sched->duration_hours,
-					    sched->duration_minutes);
+	payload->start_time = pack_field(sched->start_hour,
+					 sched->start_minute);
+	payload->duration_time = pack_field(sched->duration_hour,
+					    sched->duration_minute);
 	msg.length = sizeof(*payload);
 	msg.property_id = PID_ADV_CHARGING_SUNDAY + sched->day_of_week;
 
@@ -156,12 +150,11 @@ int wilco_ec_get_peak_shift_schedule(struct wilco_ec_device *ec,
 		return ret;
 
 	payload = (struct peak_shift_payload *) msg.data;
-	unpack_field(PEAK_SHIFT_BCD_FORMAT, payload->start_time,
-		     &sched->start_hours, &sched->start_minutes);
-	unpack_field(PEAK_SHIFT_BCD_FORMAT, payload->end_time,
-		     &sched->end_hours, &sched->end_minutes);
-	unpack_field(PEAK_SHIFT_BCD_FORMAT, payload->charge_start_time,
-		     &sched->charge_start_hours, &sched->charge_start_minutes);
+	unpack_field(&sched->start_hour, &sched->start_minute,
+		     payload->start_time);
+	unpack_field(&sched->end_hour, &sched->end_minute, payload->end_time);
+	unpack_field(&sched->charge_start_hour, &sched->charge_start_minute,
+		     payload->charge_start_time);
 
 	return 0;
 }
@@ -177,17 +170,76 @@ int wilco_ec_set_peak_shift_schedule(struct wilco_ec_device *ec,
 
 	payload = (struct peak_shift_payload *)msg.data;
 	memset(payload, 0, sizeof(*payload));
-	payload->start_time = pack_field(PEAK_SHIFT_BCD_FORMAT,
-					 sched->start_hours,
-					 sched->start_minutes);
-	payload->end_time = pack_field(PEAK_SHIFT_BCD_FORMAT,
-				       sched->end_hours,
-				       sched->end_minutes);
-	payload->charge_start_time = pack_field(PEAK_SHIFT_BCD_FORMAT,
-						sched->charge_start_hours,
-						sched->charge_start_minutes);
+	payload->start_time = pack_field(sched->start_hour,
+					 sched->start_minute);
+	payload->end_time = pack_field(sched->end_hour, sched->end_minute);
+	payload->charge_start_time = pack_field(sched->charge_start_hour,
+						sched->charge_start_minute);
 	msg.length = sizeof(*payload);
 	msg.property_id = PID_PEAK_SHIFT_SUNDAY + sched->day_of_week;
 
 	return wilco_ec_set_property(ec, &msg);
+}
+
+int wilco_ec_get_peak_shift_enable(struct wilco_ec_device *ec, bool *enable)
+{
+	u8 result;
+	int ret;
+
+	ret = wilco_ec_get_byte_property(ec, PID_PEAK_SHIFT, &result);
+	if (ret < 0)
+		return ret;
+
+	*enable = result;
+
+	return 0;
+}
+
+int wilco_ec_set_peak_shift_enable(struct wilco_ec_device *ec, bool enable)
+{
+	return wilco_ec_set_byte_property(ec, PID_PEAK_SHIFT, (u8)enable);
+}
+
+int wilco_ec_get_adv_charging_enable(struct wilco_ec_device *ec, bool *enable)
+{
+	u8 result;
+	int ret;
+
+	ret = wilco_ec_get_byte_property(ec, PID_ADV_CHARGING, &result);
+	if (ret < 0)
+		return ret;
+
+	*enable = result;
+
+	return 0;
+}
+
+int wilco_ec_set_adv_charging_enable(struct wilco_ec_device *ec, bool enable)
+{
+	return wilco_ec_set_byte_property(ec, PID_ADV_CHARGING, (u8)enable);
+}
+
+int wilco_ec_get_peak_shift_battery_threshold(struct wilco_ec_device *ec,
+					      int *percent)
+{
+	u8 result;
+	int ret;
+
+	ret = wilco_ec_get_byte_property(ec, PID_PEAK_SHIFT_BATTERY_THRESHOLD,
+					 &result);
+	if (ret < 0)
+		return ret;
+
+	*percent = result;
+
+	return 0;
+}
+int wilco_ec_set_peak_shift_battery_threshold(struct wilco_ec_device *ec,
+					      int percent)
+{
+	if (percent < WILCO_EC_PEAK_SHIFT_BATTERY_THRESHOLD_MIN ||
+	    percent > WILCO_EC_PEAK_SHIFT_BATTERY_THRESHOLD_MAX)
+		return -EINVAL;
+	return wilco_ec_set_byte_property(ec, PID_PEAK_SHIFT_BATTERY_THRESHOLD,
+					  (u8) percent);
 }
--- drivers/platform/chrome/wilco_ec/charge_schedule.h
+++ drivers/platform/chrome/wilco_ec/charge_schedule.h
@@ -11,32 +11,25 @@
 
 #include <linux/platform_data/wilco-ec.h>
 
-/* Property IDs and related EC constants */
-#define PID_ADV_CHARGING_SUNDAY			0x04EE
-#define PID_ADV_CHARGING			0x04ED
-#define PID_PEAK_SHIFT				0x0412
-#define PID_PEAK_SHIFT_SUNDAY			0x04F5
-#define PID_PEAK_SHIFT_BATTERY_THRESHOLD	0x04EB
-
-#define PEAK_SHIFT_BATTERY_THRESHOLD_MIN	15
-#define PEAK_SHIFT_BATTERY_THRESHOLD_MAX	100
+#define WILCO_EC_PEAK_SHIFT_BATTERY_THRESHOLD_MIN	15
+#define WILCO_EC_PEAK_SHIFT_BATTERY_THRESHOLD_MAX	100
 
 struct peak_shift_schedule {
 	int day_of_week;		/* 0==Sunday, 1==Monday, ... */
-	int start_hours;		/* 0..23 */
-	int start_minutes;		/* One of {0, 15, 30, 45} */
-	int end_hours;			/* 0..23 */
-	int end_minutes;		/* One of {0, 15, 30, 45} */
-	int charge_start_hours;		/* 0..23 */
-	int charge_start_minutes;	/* One of {0, 15, 30, 45} */
+	int start_hour;			/* 0..23 */
+	int start_minute;		/* One of {0, 15, 30, 45} */
+	int end_hour;			/* 0..23 */
+	int end_minute;			/* One of {0, 15, 30, 45} */
+	int charge_start_hour;		/* 0..23 */
+	int charge_start_minute;	/* One of {0, 15, 30, 45} */
 };
 
 struct adv_charge_schedule {
 	int day_of_week;	/* 0==Sunday, 1==Monday, ... */
-	int start_hours;	/* 0..23 */
-	int start_minutes;	/* One of {0, 15, 30, 45} */
-	int duration_hours;	/* 0..23 */
-	int duration_minutes;	/* One of {0, 15, 30, 45} */
+	int start_hour;		/* 0..23 */
+	int start_minute;	/* One of {0, 15, 30, 45} */
+	int duration_hour;	/* 0..23 */
+	int duration_minute;	/* One of {0, 15, 30, 45} */
 };
 
 /*
@@ -52,5 +45,21 @@ int wilco_ec_get_peak_shift_schedule(struct wilco_ec_device *ec,
 				     struct peak_shift_schedule *sched);
 int wilco_ec_set_peak_shift_schedule(struct wilco_ec_device *ec,
 				     const struct peak_shift_schedule *sched);
+
+/* Return 0 on success, negative error code on failure. */
+int wilco_ec_get_peak_shift_enable(struct wilco_ec_device *ec, bool *enable);
+int wilco_ec_set_peak_shift_enable(struct wilco_ec_device *ec, bool enable);
+int wilco_ec_get_adv_charging_enable(struct wilco_ec_device *ec, bool *enable);
+int wilco_ec_set_adv_charging_enable(struct wilco_ec_device *ec, bool enable);
+
+/*
+ * Return 0 on success, negative error code on failure.
+ * Valid range for setting is from |WILCO_EC_PEAK_SHIFT_BATTERY_THRESHOLD_MIN|
+ * to |WILCO_EC_PEAK_SHIFT_BATTERY_THRESHOLD_MAX|, inclusive.
+ */
+int wilco_ec_get_peak_shift_battery_threshold(struct wilco_ec_device *ec,
+					      int *percent);
+int wilco_ec_set_peak_shift_battery_threshold(struct wilco_ec_device *ec,
+					      int percent);
 
 #endif /* WILCO_EC_CHARGE_SCHEDULE_H */
