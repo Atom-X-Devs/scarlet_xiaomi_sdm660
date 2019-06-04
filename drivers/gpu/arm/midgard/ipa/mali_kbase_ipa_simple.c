abb47ccba91ff211b3fef3c58ba8d8c6c1e5eadb
Douglas Anderson <dianders@chromium.org>
2019-06-04 13:29:07 -0700

    CHROMIUM: MALI: Freeze temperature polling while suspended
    We don't need to run temperature polling while the system is
    suspending / resuming.  We'll mark ourselves freezable.  Without this
    suspend/resume freezes on rk3288-veyron devices.
    
    BUG=chromium:970685
    TEST=suspend / resume
    
    Change-Id: Iea9591c06182243120ce6e00fdb5b480c43e5dbb
    Signed-off-by: Douglas Anderson <dianders@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/1644069
    Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>
    Reviewed-by: Matthias Kaehlcke <mka@chromium.org>
    Reviewed-by: Dominik Behr <dbehr@chromium.org>

--- drivers/gpu/arm/midgard/ipa/mali_kbase_ipa_simple.c
+++ drivers/gpu/arm/midgard/ipa/mali_kbase_ipa_simple.c
@@ -27,6 +27,7 @@
 #endif
 #include <linux/of.h>
 #include <linux/delay.h>
+#include <linux/freezer.h>
 #include <linux/kthread.h>
 
 #include "mali_kbase.h"
@@ -154,6 +155,8 @@ static int poll_temperature(void *data)
 	int temp;
 #endif
 
+	set_freezable();
+
 	while (!kthread_should_stop()) {
 		struct thermal_zone_device *tz = READ_ONCE(model_data->gpu_tz);
 
@@ -173,6 +176,7 @@ static int poll_temperature(void *data)
 		WRITE_ONCE(model_data->current_temperature, temp);
 
 		msleep_interruptible(READ_ONCE(model_data->temperature_poll_interval_ms));
+		try_to_freeze();
 	}
 
 	return 0;
