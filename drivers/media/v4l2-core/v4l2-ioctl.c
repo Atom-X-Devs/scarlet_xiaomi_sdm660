a5c89626fb4aad7db1962df2fb0a2c0bb0450451
Boris Brezillon <boris.brezillon@collabora.com>
2019-06-05 12:46:25 -0400

    FROMGIT: media: v4l2: Fix the _MPLANE format check in v4l_enum_fmt()
    CAP_M2M_MPLANE means the device supports _MPLANE formats for both
    capture and output. Adjust the check to avoid EINVAL errors on
    such devices.
    
    BUG=b:79450911
    TEST=Checked that VEA unittest was still passing on Cheza.
    
    Reported-by: Maxime Jourdan <mjourdan@baylibre.com>
    Signed-off-by: Boris Brezillon <boris.brezillon@collabora.com>
    Reviewed-by: Tomasz Figa <tfiga@chromium.org>
    Signed-off-by: Hans Verkuil <hverkuil-cisco@xs4all.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+samsung@kernel.org>
    (cherry picked from commit f0d2b7a8915a00917207dd23aaf8554d7bf777b0
     git://linuxtv.org/media_tree.git)
    Signed-off-by: Alexandre Courbot <acourbot@chromium.org>
    
    Change-Id: I47fab8e419167bc923534f004bd1c39cf26d018d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/1683865
    Reviewed-by: Tomasz Figa <tfiga@chromium.org>
    Reviewed-by: Sean Paul <seanpaul@chromium.org>
    Commit-Queue: Alexandre Courbot <acourbot@chromium.org>
    Tested-by: Alexandre Courbot <acourbot@chromium.org>

--- drivers/media/v4l2-core/v4l2-ioctl.c
+++ drivers/media/v4l2-core/v4l2-ioctl.c
@@ -1371,6 +1371,7 @@ static int v4l_enum_fmt(const struct v4l2_ioctl_ops *ops,
 	struct video_device *vdev = video_devdata(file);
 	struct v4l2_fmtdesc *p = arg;
 	int ret = check_fmt(file, p->type);
+	u32 cap_mask;
 
 	if (ret)
 		return ret;
@@ -1379,7 +1380,9 @@ static int v4l_enum_fmt(const struct v4l2_ioctl_ops *ops,
 	switch (p->type) {
 	case V4L2_BUF_TYPE_VIDEO_CAPTURE:
 	case V4L2_BUF_TYPE_VIDEO_CAPTURE_MPLANE:
-		if (!!(vdev->device_caps & V4L2_CAP_VIDEO_CAPTURE_MPLANE) !=
+		cap_mask = V4L2_CAP_VIDEO_CAPTURE_MPLANE |
+			   V4L2_CAP_VIDEO_M2M_MPLANE;
+		if (!!(vdev->device_caps & cap_mask) !=
 		    (p->type == V4L2_BUF_TYPE_VIDEO_CAPTURE_MPLANE))
 			break;
 
@@ -1394,7 +1397,9 @@ static int v4l_enum_fmt(const struct v4l2_ioctl_ops *ops,
 		break;
 	case V4L2_BUF_TYPE_VIDEO_OUTPUT:
 	case V4L2_BUF_TYPE_VIDEO_OUTPUT_MPLANE:
-		if (!!(vdev->device_caps & V4L2_CAP_VIDEO_OUTPUT_MPLANE) !=
+		cap_mask = V4L2_CAP_VIDEO_OUTPUT_MPLANE |
+			   V4L2_CAP_VIDEO_M2M_MPLANE;
+		if (!!(vdev->device_caps & cap_mask) !=
 		    (p->type == V4L2_BUF_TYPE_VIDEO_OUTPUT_MPLANE))
 			break;
 
