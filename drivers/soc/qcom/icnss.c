10a979b5ea972b9454a0fd4cd4c320c3e665ac20
Vincent Palomares <paillon@google.com>
2019-06-14 16:16:38 -0700

    soc: qcom: icnss: Async suspend/resume callbacks
    Bug: 135007854
    Signed-off-by: Vincent Palomares <paillon@google.com>
    [ Tashar02: Apply at drivers/soc/qcom/icnss.c ]
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/soc/qcom/icnss.c
+++ drivers/soc/qcom/icnss.c
@@ -3846,6 +3846,8 @@ static int icnss_probe(struct platform_device *pdev)
 	if (ret)
 		goto out;
 
+	device_enable_async_suspend(dev);
+
 	spin_lock_init(&priv->event_lock);
 	spin_lock_init(&priv->on_off_lock);
 	mutex_init(&priv->dev_lock);
