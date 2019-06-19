7d19cb46c3a1d8ea2aa2ba16bdff7cf2e512f42e
Boris Brezillon <boris.brezillon@collabora.com>
2019-06-19 14:15:33 +0200

    FROMLIST: media: hantro: Move copy_metadata() before doing a decode operation
    Some decoders use intra slice/frame references. The capture buffer
    pointed by these references might be new and thus have invalid
    timestamp which prevents the decoder logic from retrieving the
    vb2_buffer object based on the output buf timestamp.
    Copy all metadata (including the timestamp) before starting the decode
    operation.
    
    Suggested-by: Jonas Karlman <jonas@kwiboo.se>
    Signed-off-by: Boris Brezillon <boris.brezillon@collabora.com>
    (am from https://patchwork.kernel.org/patch/11004015/)
    
    BUG=chromium:965375
    TEST=emerge-veyron_minnie-kernelnext chromeos-kernel-4_19
    
    Change-Id: I50cba8317deb50b68a31ee491286e664eea945b9
    Signed-off-by: Tomasz Figa <tfiga@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/1712732
    Reviewed-by: Sean Paul <seanpaul@chromium.org>
    Reviewed-by: Alexandre Courbot <acourbot@chromium.org>

--- drivers/staging/media/hantro/hantro_drv.c
+++ drivers/staging/media/hantro/hantro_drv.c
@@ -111,8 +111,6 @@ static void hantro_job_finish(struct hantro_dev *vpu,
 	src->sequence = ctx->sequence_out++;
 	dst->sequence = ctx->sequence_cap++;
 
-	v4l2_m2m_buf_copy_metadata(src, dst, true);
-
 	ret = ctx->buf_finish(ctx, &dst->vb2_buf, bytesused);
 	if (ret)
 		result = VB2_BUF_STATE_ERROR;
@@ -156,7 +154,11 @@ void hantro_watchdog(struct work_struct *work)
 static void device_run(void *priv)
 {
 	struct hantro_ctx *ctx = priv;
+	struct vb2_v4l2_buffer *src, *dst;
 	int ret;
+
+	src = v4l2_m2m_next_src_buf(ctx->fh.m2m_ctx);
+	dst = v4l2_m2m_next_dst_buf(ctx->fh.m2m_ctx);
 
 	ret = clk_bulk_enable(ctx->dev->variant->num_clocks, ctx->dev->clocks);
 	if (ret)
@@ -164,6 +166,8 @@ static void device_run(void *priv)
 	ret = pm_runtime_get_sync(ctx->dev->dev);
 	if (ret < 0)
 		goto err_cancel_job;
+
+	v4l2_m2m_buf_copy_metadata(src, dst, true);
 
 	ctx->codec_ops->run(ctx);
 	return;
