15781cbd53c369c2b11c4947d597993ca0ea6bc7
Guenter Roeck <groeck@chromium.org>
2019-06-19 14:29:05 -0700

    CHROMIUM: drm/i915: Add missing access checks in i915_gem_execbuffer2_ioctl
    CVE-2018-20669:
    
    An issue where a provided address with access_ok() is not checked was
    discovered in i915_gem_execbuffer2_ioctl in
    drivers/gpu/drm/i915/i915_gem_execbuffer.c in the Linux kernel through
    4.19.13. A local attacker can craft a malicious IOCTL function call to
    overwrite arbitrary kernel memory, resulting in a Denial of Service
    or privilege escalation.
    
    The problem is fixed upstream with commit 594cc251fdd0 ("make
    'user_access_begin()' do 'access_ok()'"), which merges access_ok into
    user_access_begin() system-wide. Since this would be difficult to
    backport, add the missing calls to access_ok() locally.
    
    BUG=chromium:976652
    TEST=Run i915 graphics tests
    
    Change-Id: I6868f0f2f59c907d0c2191621fd714cc3c8d7d5a
    Signed-off-by: Guenter Roeck <groeck@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/1666244
    Reviewed-by: Stéphane Marchesin <marcheu@chromium.org>

--- drivers/gpu/drm/i915/i915_gem_execbuffer.c
+++ drivers/gpu/drm/i915/i915_gem_execbuffer.c
@@ -1623,6 +1623,8 @@ end_user:
 		 * happened we would make the mistake of assuming that the
 		 * relocations were valid.
 		 */
+		if (!access_ok(VERIFY_WRITE, urelocs, size))
+			goto end_user;
 		user_access_begin();
 		for (copied = 0; copied < nreloc; copied++)
 			unsafe_put_user(-1,
@@ -2605,6 +2607,17 @@ i915_gem_execbuffer2_ioctl(struct drm_device *dev, void *data,
 		unsigned int i;
 
 		/* Copy the new buffer offsets back to the user's exec list. */
+		/*
+		 * Note: count * sizeof(*user_exec_list) does not overflow,
+		 * because we checked 'count' in check_buffer_count().
+		 *
+		 * And this range already got effectively checked earlier
+		 * when we did the "copy_from_user()" above.
+		 */
+		if (!access_ok(VERIFY_WRITE,
+			       user_exec_list, count * sizeof(*user_exec_list)))
+			goto end_user;
+
 		user_access_begin();
 		for (i = 0; i < args->buffer_count; i++) {
 			if (!(exec2_list[i].offset & UPDATE))
