e2d9f09b0811bbd2daa3118945a4b59bf71178df
Wen Yang <wen.yang99@zte.com.cn>
2019-07-10 15:25:09 +0800

    BACKPORT: ASoC: audio-graph-card: fix an use-after-free in graph_get_dai_id()
    After calling of_node_put() on the node variable, it is still being
    used, which may result in use-after-free.
    Fix this issue by calling of_node_put() after the last usage.
    
    Fixes: a0c426fe1433 ("ASoC: simple-card-utils: check "reg" property on asoc_simple_card_get_dai_id()")
    Link: https://lore.kernel.org/r/1562743509-30496-5-git-send-email-wen.yang99@zte.com.cn
    Signed-off-by: Wen Yang <wen.yang99@zte.com.cn>
    Acked-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>
    Signed-off-by: Guenter Roeck <groeck@chromium.org>
    (cherry picked from commit c152f8491a8d9a4b25afd65a86eb5e55e2a8c380)
    
    Conflicts:
       sound/soc/generic/audio-graph-card.c
       In the upstream kernel, the functionality was moved from
       asoc_simple_card_get_dai_id() to graph_get_dai_id().
    
    BUG=chromium:998168,b:123738217
    TEST=Test Audio use cases for CML
    
    Change-Id: Ia9c06955ce314297996749a327ba962c0e4fb6c1
    Signed-off-by: Guenter Roeck <groeck@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/1779146
    Reviewed-by: Curtis Malainey <cujomalainey@chromium.org>
    Reviewed-by: Dylan Reid <dgreid@chromium.org>

--- sound/soc/generic/simple-card-utils.c
+++ sound/soc/generic/simple-card-utils.c
@@ -272,6 +272,7 @@ static int asoc_simple_card_get_dai_id(struct device_node *ep)
 	struct device_node *node;
 	struct device_node *endpoint;
 	struct of_endpoint info;
+	const u32 *reg;
 	int i, id;
 	int ret;
 
@@ -293,8 +294,9 @@ static int asoc_simple_card_get_dai_id(struct device_node *ep)
 			return info.id;
 
 		node = of_get_parent(ep);
+		reg = of_get_property(node, "reg", NULL);
 		of_node_put(node);
-		if (of_get_property(node, "reg", NULL))
+		if (reg)
 			return info.port;
 	}
 	node = of_graph_get_port_parent(ep);
