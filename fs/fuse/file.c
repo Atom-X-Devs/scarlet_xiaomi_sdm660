a65d26e173255b60c5fdc428da184d1afc4c4f19
Vasily Averin <vvs@virtuozzo.com>
2019-08-19 08:48:26 +0300

    BACKPORT: fuse: redundant get_fuse_inode() calls in fuse_writepages_fill()
    Currently fuse_writepages_fill() calls get_fuse_inode() few times with
    the same argument.
    
    Signed-off-by: Vasily Averin <vvs@virtuozzo.com>
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>
    Change-Id: I03ab9dcd51cbbc6f1f6dc9a329c6202965ee001a
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- fs/fuse/file.c
+++ fs/fuse/file.c
@@ -2016,7 +2016,7 @@ static int fuse_writepages_fill(struct page *page,
 
 	if (!data->ff) {
 		err = -EIO;
-		data->ff = fuse_write_file_get(fc, get_fuse_inode(inode));
+		data->ff = fuse_write_file_get(fc, fi);
 		if (!data->ff)
 			goto out_unlock;
 	}
@@ -2061,8 +2061,6 @@ static int fuse_writepages_fill(struct page *page,
 	 * under writeback, so we can release the page lock.
 	 */
 	if (data->wpa == NULL) {
-		struct fuse_inode *fi = get_fuse_inode(inode);
-
 		err = -ENOMEM;
 		wpa = fuse_writepage_args_alloc();
 		if (!wpa) {
