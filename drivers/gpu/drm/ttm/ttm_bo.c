3173e7f841f0db70b47184fd9b11d199a1621d73
Lepton Wu <ytht.net@gmail.com>
2019-09-04 13:08:27 -0700

    BACKPORT: FROMLIST: drm/ttm: let ttm_bo_wait timeout be configurable
    When running dEQP against virgl driver, it turns out the default
    15 seconds timeout for ttm_bo_wait is not big enough for
    GLES31.functional.ssbo.layout.random.nested_structs_arrays_instance_arrays.22
    Change it to a configurable value so we can tune it before virgl
    performance gets improved.
    
    Signed-off-by: Lepton Wu <ytht.net@gmail.com>
    
    BUG=b:138755978
    TEST=Compile kernel
    (am from https://patchwork.kernel.org/patch/11132281/)
    
    Change-Id: I4fb34cdd0848c7f0c20434ad932b9e0151911321
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/1788167
    Reviewed-by: Gurchetan Singh <gurchetansingh@chromium.org>
    Tested-by: Lepton Wu <lepton@chromium.org>
    Commit-Queue: Lepton Wu <lepton@chromium.org>

--- drivers/gpu/drm/ttm/ttm_bo.c
+++ drivers/gpu/drm/ttm/ttm_bo.c
@@ -1714,7 +1714,7 @@ EXPORT_SYMBOL(ttm_bo_unmap_virtual);
 int ttm_bo_wait(struct ttm_buffer_object *bo,
 		bool interruptible, bool no_wait)
 {
-	long timeout = 15 * HZ;
+	long timeout = CONFIG_DRM_TTM_BO_WAIT_TIMEOUT * HZ;
 
 	if (no_wait) {
 		if (reservation_object_test_signaled_rcu(bo->resv, true))
