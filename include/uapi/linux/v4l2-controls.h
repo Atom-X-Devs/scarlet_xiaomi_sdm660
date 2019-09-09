224a9820a3f92a4010da216eff2f2bd5a5ef390d
Jerry-ch Chen <jerry-ch.chen@mediatek.com>
2019-09-09 16:12:25 +0800

    BACKPORT: FROMLIST: platform: mtk-isp: Add Mediatek FD driver
    This patch adds the driver of Face Detection (FD) unit in
    Mediatek camera system, providing face detection function.
    
    The mtk-isp directory will contain drivers for multiple IP
    blocks found in Mediatek ISP system. It will include ISP Pass 1
    driver (CAM), sensor interface driver, DIP driver and face
    detection driver.
    
    (am from https://patchwork.kernel.org/patch/11134913/)
    
    Set offset of MTK FD control ID base to 0x10c0 in
    include/uapi/linux/v4l2-controls.h
    
    Sovle conflicts in Kconfig and Makefile
    
    BUG=b:140012409
    TEST=build pass
    
    Change-Id: I1d1ba58cbdcdcc161b140398fc26b24ec2134cdb
    Signed-off-by: Jerry-ch Chen <jerry-ch.chen@mediatek.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/1792408
    Reviewed-by: Nicolas Boichat <drinkcat@chromium.org>
    Reviewed-by: Tomasz Figa <tfiga@chromium.org>
    Tested-by: Jerry-ch Chen <jerry-ch.chen@mediatek.corp-partner.google.com>
    Tested-by: Nicolas Boichat <drinkcat@chromium.org>
    Commit-Queue: Nicolas Boichat <drinkcat@chromium.org>

--- include/uapi/linux/v4l2-controls.h
+++ include/uapi/linux/v4l2-controls.h
@@ -192,6 +192,10 @@ enum v4l2_colorfx {
  * We reserve 16 controls for this driver. */
 #define V4L2_CID_USER_IMX_BASE			(V4L2_CID_USER_BASE + 0x10b0)
 
+/* The base for the mediatek FD driver controls */
+/* We reserve 16 controls for this driver. */
+#define V4L2_CID_USER_MTK_FD_BASE		(V4L2_CID_USER_BASE + 0x10c0)
+
 /* MPEG-class control IDs */
 /* The MPEG controls are applicable to all codec controls
  * and the 'MPEG' part of the define is historical */
