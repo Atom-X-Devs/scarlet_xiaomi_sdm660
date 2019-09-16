fd8c067ebb6d6ff783864b87971411896ec25c38
Guenter Roeck <groeck@chromium.org>
2019-09-16 10:14:35 -0700

    CHROMIUM: Revert "drm/i915: Restore sane defaults for KMS on GEM error load"
    This reverts commit b185bde520d5feb8fc750107d4cda32262fd92bf.
    
    Needed for merge of v4.19.73 to avoid substantial conflicts.
    The patch needs to be pulled into chromeos-4.19 manually if needed.
    
    BUG=chromium:1004310
    TEST=Merge with less conflicts
    
    Signed-off-by: Guenter Roeck <groeck@chromium.org>

--- drivers/gpu/drm/i915/i915_gem.c
+++ drivers/gpu/drm/i915/i915_gem.c
@@ -5595,8 +5595,6 @@ err_uc_misc:
 		i915_gem_cleanup_userptr(dev_priv);
 
 	if (ret == -EIO) {
-		mutex_lock(&dev_priv->drm.struct_mutex);
-
 		/*
 		 * Allow engine initialisation to fail by marking the GPU as
 		 * wedged. But we only want to do this where the GPU is angry,
@@ -5607,14 +5605,7 @@ err_uc_misc:
 					"Failed to initialize GPU, declaring it wedged!\n");
 			i915_gem_set_wedged(dev_priv);
 		}
-
-		/* Minimal basic recovery for KMS */
-		ret = i915_ggtt_enable_hw(dev_priv);
-		i915_gem_restore_gtt_mappings(dev_priv);
-		i915_gem_restore_fences(dev_priv);
-		intel_init_clock_gating(dev_priv);
-
-		mutex_unlock(&dev_priv->drm.struct_mutex);
+		ret = 0;
 	}
 
 	i915_gem_drain_freed_objects(dev_priv);
