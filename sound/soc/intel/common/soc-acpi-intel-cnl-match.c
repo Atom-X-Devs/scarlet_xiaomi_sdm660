011a98181d19b190cd9a005fcc5962e48cc25838
Naveen Manohar <naveen.m@intel.com>
2019-09-25 22:27:04 +0530

    BACKPORT: FROMLIST: ASoC: Intel: Add acpi match for rt1011 based m/c driver
    Add match for CML m/c with RT1011 and RT5682
    
    Signed-off-by: Naveen Manohar <naveen.m@intel.com>
    Signed-off-by: Sathya Prakash M R <sathya.prakash.m.r@intel.com>
    (am from https://patchwork.kernel.org/patch/11161149/)
    (also found at https://lkml.kernel.org/r/1569430624-30505-3-git-send-email-sathya.prakash.m.r@intel.com)
    
    Conflicts:
       sound/soc/intel/common/soc-acpi-intel-cnl-match.c - context change
    
    BUG=b:138826775
    TEST=Test Audio use cases for CML with full SOF patch series applied.
    
    Change-Id: Ia6163d02ffc89af04508c3cfab64ef1ef4c2c274
    Signed-off-by: Ap, Kamal <kamal.ap@intel.corp-partner.google.com>
    Signed-off-by: Ben Zhang <benzh@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/1821883

--- sound/soc/intel/common/soc-acpi-intel-cnl-match.c
+++ sound/soc/intel/common/soc-acpi-intel-cnl-match.c
@@ -35,6 +35,13 @@ struct snd_soc_acpi_mach snd_soc_acpi_intel_cnl_machines[] = {
 		.pdata = &cnl_pdata,
 	},
 	{
+		.id = "10EC1011",
+		.drv_name = "cml_rt1011_rt5682",
+		.quirk_data = &cml_codecs,
+		.sof_fw_filename = "sof-cnl.ri",
+		.sof_tplg_filename = "sof-cml-rt1011-rt5682.tplg",
+	},
+	{
 		.id = "MX98357A",
 		.drv_name = "sof_rt5682",
 		.quirk_data = &cml_codecs,
