abefac05a67cf8d699c5a3bdd434d60ada59ea2b
Wen Gong <wgong@codeaurora.org>
2019-11-11 14:42:41 +0800

    Revert "FROMLIST: ath10k: enable alt data of TX path for sdio"
    This reverts commit d4f71e9464c198a6bf402d89d756a50e56c94993.
    
    Reason for revert: refresh this patch to latest FROMLIST version.
    
    Original change's description:
    > FROMLIST: ath10k: enable alt data of TX path for sdio
    >
    > The default credit size is 1792 bytes, but the IP mtu is 1500 bytes,
    > then it has about 290 bytes's waste for each data packet on sdio
    > transfer path for TX bundle, it will reduce the transmission
    > utilization
    > ratio for data packet.
    >
    > This patch enable the small credit size in firmware, firmware will use
    > the new credit size 1556 bytes, it will increase the transmission
    > utilization ratio for data packet on TX patch. It results in
    > significant
    > performance improvement on TX path.
    >
    > This patch only effect sdio chip, it will not effect PCI, SNOC etc.
    >
    > Tested with QCA6174 SDIO with firmware
    > WLAN.RMH.4.4.1-00017-QCARMSWP-1.
    >
    > Signed-off-by: Wen Gong <wgong@codeaurora.org>
    > (am from https://patchwork.kernel.org/patch/11197613/)
    > (also found at
    > https://lkml.kernel.org/r/20191018072750.10372-4-wgong@codeaurora.org)
    >
    > BUG=b:136146847
    > BUG=b:141834462
    > TEST=Test throughout success
    >
    > Change-Id: I0c1eba6abaab83302e6d8153ea1d85c749dba03a
    > Signed-off-by: Wen Gong <wgong@codeaurora.org>
    > Signed-off-by: Claire Chang <tientzu@chromium.org>
    > Reviewed-on:
    > https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/1720420
    > Reviewed-by: Sean Paul <seanpaul@chromium.org>
    > Commit-Queue: Nicolas Boichat <drinkcat@chromium.org>
    
    BUG=b:141834462
    TEST=Test basic connection success
    
    Change-Id: I09d8b501cf5ba801040c5816a6717fb613102fd5
    Signed-off-by: Wen Gong <wgong@codeaurora.org>
    Signed-off-by: Claire Chang <tientzu@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/1909862
    Reviewed-by: Nicolas Boichat <drinkcat@chromium.org>

--- drivers/net/wireless/ath/ath10k/htc.c
+++ drivers/net/wireless/ath/ath10k/htc.c
@@ -649,16 +649,12 @@ int ath10k_htc_wait_target(struct ath10k_htc *htc)
 	 */
 	if (htc->control_resp_len >=
 	    sizeof(msg->hdr) + sizeof(msg->ready_ext)) {
-		htc->alt_data_credit_size =
-			__le16_to_cpu(msg->ready_ext.reserved) &
-			ATH10K_HTC_MSG_READY_EXT_ALT_DATA_MASK;
 		htc->max_msgs_per_htc_bundle =
 			min_t(u8, msg->ready_ext.max_msgs_per_htc_bundle,
 			      HTC_HOST_MAX_MSG_PER_RX_BUNDLE);
 		ath10k_dbg(ar, ATH10K_DBG_HTC,
-			   "Extended ready message. RX bundle size: %d, alt size:%d\n",
-			   htc->max_msgs_per_htc_bundle,
-			   htc->alt_data_credit_size);
+			   "Extended ready message. RX bundle size: %d\n",
+			   htc->max_msgs_per_htc_bundle);
 	}
 
 	return 0;
@@ -805,10 +801,6 @@ setup:
 	ep->max_tx_queue_depth = conn_req->max_send_queue_depth;
 	ep->max_ep_message_len = __le16_to_cpu(resp_msg->max_msg_size);
 	ep->tx_credits = tx_alloc;
-
-	if (conn_req->service_id == ATH10K_HTC_SVC_ID_HTT_DATA_MSG &&
-	    htc->alt_data_credit_size != 0)
-		ep->tx_credit_size = htc->alt_data_credit_size;
 
 	/* copy all the callbacks */
 	ep->ep_ops = conn_req->ep_ops;
