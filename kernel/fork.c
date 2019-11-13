187835806406690fe7e3c9f7609a9c59d1de4059
Yu Zhao <yuzhao@google.com>
2019-11-12 22:13:47 -0700

    CHROMIUM: mm: add mm_struct list walk to kstaled
    Walk mm_struct list to clear the referenced bit on pmd/pte entries.
    It's simple and works for all CPU architectures; therefore, it's
    more upstreamable. There are potential overhead from going through
    pgd/p4d/pud as well as mmap_sem contention. However, I wasn't able
    to produce any of the problems on cros or prod.
    
    BUG=b:123039911
    TEST=Ran platform.MemoryPressure and platform.MemoryStress
    
    Change-Id: I7dfa06e1b1a1bc9df6fe87815de49492353ed8ab
    Signed-off-by: Yu Zhao <yuzhao@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/1897316
    Reviewed-by: Salman Qazi <sqazi@google.com>
    Commit-Queue: Yu Zhao <yuzhao@chromium.org>
    Tested-by: Yu Zhao <yuzhao@chromium.org>
    (cherry picked from commit f3acfc455464191a8d17d469cb466a9d67e1ea9c)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/1934357

--- kernel/fork.c
+++ kernel/fork.c
@@ -91,6 +91,7 @@
 #include <linux/kcov.h>
 #include <linux/livepatch.h>
 #include <linux/thread_info.h>
+#include <linux/kstaled.h>
 
 #include <asm/pgtable.h>
 #include <asm/pgalloc.h>
@@ -615,7 +616,23 @@ static void check_mm(struct mm_struct *mm)
 }
 
 #define allocate_mm()	(kmem_cache_alloc(mm_cachep, GFP_KERNEL))
+#ifdef CONFIG_KSTALED
+static void rcu_free_mm(struct rcu_head *rcu)
+{
+	struct mm_struct *mm = container_of(rcu, struct mm_struct, rcu_head);
+
+	kmem_cache_free(mm_cachep, mm);
+}
+
+static void free_mm(struct mm_struct *mm)
+{
+	kstaled_del_mm(mm);
+
+	call_rcu(&mm->rcu_head, rcu_free_mm);
+}
+#else
 #define free_mm(mm)	(kmem_cache_free(mm_cachep, (mm)))
+#endif
 
 /*
  * Called when the last reference to the mm
@@ -975,6 +992,7 @@ static struct mm_struct *mm_init(struct mm_struct *mm, struct task_struct *p,
 		goto fail_nocontext;
 
 	mm->user_ns = get_user_ns(user_ns);
+	kstaled_add_mm(mm);
 	return mm;
 
 fail_nocontext:
