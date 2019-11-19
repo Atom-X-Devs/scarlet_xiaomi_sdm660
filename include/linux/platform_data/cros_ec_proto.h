47b315bd0026adf8488e2f8e965e93347e5ddc93
Enrico Granata <egranata@chromium.org>
2019-11-19 13:45:46 +0100

    BACKPORT: UPSTREAM: platform/chrome: cros_ec: handle MKBP more events flag
    The ChromeOS EC has support for signaling to the host that a single IRQ
    can serve multiple MKBP (Matrix KeyBoard Protocol) events.
    
    Doing this serves an optimization purpose, as it minimizes the number of
    round-trips into the interrupt handling machinery, and it proves
    beneficial to sensor timestamping as it keeps the desired synchronization
    of event times between the two processors.
    
    This patch adds kernel support for this EC feature, allowing the ec_irq
    to loop until all events have been served.
    
    Conflict: charger already present in cros_ec_proto.h
    
    BUG=chromium:946178
    TEST=tast run <ip> hardware.SensorRing with sensor ring enabled.
         iio-info with sensor ring disabled.
    
    Signed-off-by: Enrico Granata <egranata@chromium.org>
    Reviewed-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>
    Acked-by: Lee Jones <lee.jones@linaro.org>
    Signed-off-by: Enric Balletbo i Serra <enric.balletbo@collabora.com>
    (cherry picked from commit 3300fdd630d4d3d96e3ba9af63a740d3a4e8fc61)
    Signed-off-by: Gwendal Grignou <gwendal@chromium.org>
    Change-Id: I33df9fe80169cf6031c344efbbe568f4052aa688
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2228699

--- include/linux/platform_data/cros_ec_proto.h
+++ include/linux/platform_data/cros_ec_proto.h
@@ -116,7 +116,10 @@ struct cros_ec_command {
  *            code.
  * @pkt_xfer: Send packet to EC and get response.
  * @lock: One transaction at a time.
- * @mkbp_event_supported: True if this EC supports the MKBP event protocol.
+ * @charger: Charger connected to the EC, if any.
+ * @mkbp_event_supported: 0 if MKBP not supported. Otherwise its value is
+ *                        the maximum supported version of the MKBP host event
+ *                        command + 1.
  * @host_sleep_v1: True if this EC supports the sleep v1 command.
  * @event_notifier: Interrupt event notifier for transport devices.
  * @event_data: Raw payload transferred with the MKBP event.
@@ -157,7 +160,7 @@ struct cros_ec_device {
 			struct cros_ec_command *msg);
 	struct power_supply *charger;
 	struct mutex lock;
-	bool mkbp_event_supported;
+	u8 mkbp_event_supported;
 	bool host_sleep_v1;
 	struct blocking_notifier_head event_notifier;
 
@@ -214,13 +217,17 @@ int cros_ec_unregister(struct cros_ec_device *ec_dev);
 
 int cros_ec_query_all(struct cros_ec_device *ec_dev);
 
-int cros_ec_get_next_event(struct cros_ec_device *ec_dev, bool *wake_event);
+int cros_ec_get_next_event(struct cros_ec_device *ec_dev,
+			   bool *wake_event,
+			   bool *has_more_events);
 
 u32 cros_ec_get_host_event(struct cros_ec_device *ec_dev);
 
 int cros_ec_check_features(struct cros_ec_dev *ec, int feature);
 
 int cros_ec_get_sensor_count(struct cros_ec_dev *ec);
+
+bool cros_ec_handle_event(struct cros_ec_device *ec_dev);
 
 /**
  * cros_ec_get_time_ns() - Return time in ns.
