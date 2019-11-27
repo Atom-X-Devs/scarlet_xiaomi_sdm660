bf20ce36eb73e1525e4ce67efcc01c3efc510d47
Sonny Sasaka <sonnysasaka@chromium.org>
2019-11-27 13:56:28 -0800

    Revert "CHROMIUM: Bluetooth: Move sysfs init to beginning of af_bluetooth"
    This reverts commit 0417eaa05b0fde937b4bb6079fa6bfecbaf0b525.
    
    BUG=b:145163508
    TEST=Builds
    
    Change-Id: I81258b3fe005096ddbdbcf28ea12c37dee577e10
    Signed-off-by: Sonny Sasaka <sonnysasaka@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/1940759
    Reviewed-by: Yu Liu <yudiliu@google.com>

--- net/bluetooth/af_bluetooth.c
+++ net/bluetooth/af_bluetooth.c
@@ -32,7 +32,6 @@
 #include <asm/ioctls.h>
 
 #include <net/bluetooth/bluetooth.h>
-#include <net/bluetooth/hci_le_splitter.h>
 #include <linux/proc_fs.h>
 
 #include "leds.h"
@@ -780,10 +779,6 @@ static int __init bt_init(void)
 		goto cleanup_sysfs;
 
 	BT_INFO("HCI device and connection manager initialized");
-
-	err = hci_le_splitter_sysfs_init();
-	if (err < 0)
-		goto unregister_socket;
 
 	err = hci_sock_init();
 	if (err)
