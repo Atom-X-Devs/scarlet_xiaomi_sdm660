24ff99a0f713b183bc14cc5a22532a4f6bb3d576
Hsin-Yi Wang <hsinyi@chromium.org>
2020-01-09 14:28:57 +0800

    FIXUP: FROMGIT: drm/mediatek: Add ctm property support
    There might be some comp that doesn't have funcs, eg. hdmi-connector.
    Check for comp->funcs otherwise there will be NULL pointer dereference
    crash.
    
    [https://lore.kernel.org/lkml/1578637185.29400.3.camel@mtksdaap41/]
    
    BUG=b:144740108
    TEST=boot kernel
    
    Fixes: bd3de8cd782b ("drm/mediatek: Add gamma property according to hardware capability")
    Fixes: 7395eab077f9 ("drm/mediatek: Add ctm property support")
    Change-Id: Ic553a3ff30337d63b07c2eb4ff71f13ea2c0163e
    Signed-off-by: Hsin-Yi Wang <hsinyi@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/1994866
    Reviewed-by: Nicolas Boichat <drinkcat@chromium.org>
    Reviewed-by: Eizan Miyamoto <eizan@chromium.org>
    Commit-Queue: Nicolas Boichat <drinkcat@chromium.org>
    Tested-by: Nicolas Boichat <drinkcat@chromium.org>

--- drivers/gpu/drm/mediatek/mtk_drm_crtc.c
+++ drivers/gpu/drm/mediatek/mtk_drm_crtc.c
@@ -808,11 +808,13 @@ int mtk_drm_crtc_create(struct drm_device *drm_dev,
 
 		mtk_crtc->ddp_comp[i] = comp;
 
-		if (comp->funcs->ctm_set)
-			has_ctm = true;
+		if (comp->funcs) {
+			if (comp->funcs->ctm_set)
+				has_ctm = true;
 
-		if (comp->funcs->gamma_set)
-			gamma_lut_size = MTK_LUT_SIZE;
+			if (comp->funcs->gamma_set)
+				gamma_lut_size = MTK_LUT_SIZE;
+		}
 	}
 
 	for (i = 0; i < mtk_crtc->ddp_comp_nr; i++)
