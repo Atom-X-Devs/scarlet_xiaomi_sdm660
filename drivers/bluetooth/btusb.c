eba41b954bdaf615d231f2b7fa5f0bb123172317
Amit K Bag <amit.k.bag@intel.com>
2020-03-10 13:10:26 +0530

    Revert "CHROMIUM: Intel BT 7265 - Fix the firmware download issue"
    This reverts commit 0365ba6bdcd1b97c806a3554a3412dbf595c8390.
    
    Reason for revert: aligning to the upstream kernel
    
    BUG=none
    TEST=none
    
    Signed-off-by: Amit K Bag <amit.k.bag@intel.com>
    Change-Id: Id403d37b70d99082b2358c8d819b8add211cd938
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2096278
    Reviewed-by: Alain Michaud <alainm@chromium.org>
    Commit-Queue: Alain Michaud <alainm@chromium.org>
    Tested-by: Alain Michaud <alainm@chromium.org>

--- drivers/bluetooth/btusb.c
+++ drivers/bluetooth/btusb.c
@@ -31,7 +31,6 @@
 #include <linux/suspend.h>
 #include <linux/gpio/consumer.h>
 #include <asm/unaligned.h>
-#include <linux/delay.h>
 
 #include <net/bluetooth/bluetooth.h>
 #include <net/bluetooth/hci_core.h>
@@ -1973,11 +1972,6 @@ static int btusb_setup_intel(struct hci_dev *hdev)
 						 &disable_patch);
 		if (ret < 0)
 			goto exit_mfg_deactivate;
-		/* For each memory write controller need at least 2 ms to
-		 * realize the write is complete before it receives one
-		 * more write.
-		 */
-		mdelay(2);
 	}
 
 	release_firmware(fw);
@@ -1987,10 +1981,7 @@ static int btusb_setup_intel(struct hci_dev *hdev)
 
 	/* Patching completed successfully and disable the manufacturer mode
 	 * with reset and activate the downloaded firmware patches.
-	 * 8ms delay - Once firmware patch download complete, controller needs
-	 * 8ms to validate the patches.
 	 */
-	mdelay(8);
 	err = btintel_exit_mfg(hdev, true, true);
 	if (err)
 		return err;
