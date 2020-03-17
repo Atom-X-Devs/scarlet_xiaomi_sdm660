437e46e65b746c7289743e05f3da00a0c5023b0a
Amit K Bag <amit.k.bag@intel.corp-partner.google.com>
2020-03-17 05:50:22 +0000

    Revert "Bluetooth: print fw build version in power-on boot"
    This reverts commit 8f04d51b22f4b80d370bd8f077a009fdf2a5f151.
    
    Reason for revert: aligning to the upstream kernel
    
    Original change's description:
    > Bluetooth: print fw build version in power-on boot
    > 
    > To determine the build version of Bluetooth firmware to ensure reported
    > issue related to a particular release. This is very helpful for every fw
    > downloaded to BT controller and issue reported from field test.
    > 
    > BUG=b:64035404
    > TEST=kernel-boot log
    > Signed-off-by: Sukumar Ghorai <sukumar.ghorai@intel.com>
    > 
    > Change-Id: I54c033d06cb0a437778555be1a50321ea31fc425
    > Reviewed-on: https://chromium-review.googlesource.com/842504
    > Commit-Ready: Miao-chen Chou <mcchou@chromium.org>
    > Tested-by: Miao-chen Chou <mcchou@chromium.org>
    > Reviewed-by: Miao-chen Chou <mcchou@chromium.org>
    > Reviewed-on: https://chromium-review.googlesource.com/1644679
    > Commit-Ready: Yoni Shavit <yshavit@chromium.org>
    > Tested-by: Yoni Shavit <yshavit@chromium.org>
    > Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>
    
    Bug: b:64035404
    Change-Id: If38cc09729af2880749d8e372e94ecd5b39e6076
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2107037
    Reviewed-by: Alain Michaud <alainm@chromium.org>
    Commit-Queue: Alain Michaud <alainm@chromium.org>
    Tested-by: Alain Michaud <alainm@chromium.org>

--- drivers/bluetooth/btusb.c
+++ drivers/bluetooth/btusb.c
@@ -1986,12 +1986,7 @@ static int btusb_setup_intel(struct hci_dev *hdev)
 	if (err)
 		return err;
 
-	/* Need build number for downloaded fw patches in every power-on boot */
-	err = btintel_read_version(hdev, &ver);
-	if (err)
-		return err;
-	BT_INFO("%s: Intel Bluetooth fw patch 0x%02x completed and activated",
-		hdev->name, ver.fw_patch_num);
+	bt_dev_info(hdev, "Intel firmware patch completed and activated");
 
 	goto complete;
 
