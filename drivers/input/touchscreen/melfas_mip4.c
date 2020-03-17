Atom-X-Devs/scarlet_xiaomi_sdm660

commit c5ca96db39da130c92969ce07652df5cd7322fe3

FROMLIST: input: touchscreen: add delay time to device power on
Delay time for MELFAS MIP4 controller is required at power on stage
regardless the existence of GPIO consumer lookup from devicetree or
ACPI device table.

There is an issue if GPIO ce is undefined in the ACPI results no delay
time for device power on, the controller ended up in an abnormal state.

TEST=echo i2c-MLFS0000:00 > /sys/bus/i2c/drivers/mip4_ts/unbind
     verify the touch function is off

     echo i2c-MLFS0000:00 > /sys/bus/i2c/drivers/mip4_ts/bind
     verify the touch function is on

Signed-off-by: Crag.Wang <crag.wang@dell.com>

(am from https://patchwork.kernel.org/patch/11037755/)

BUG=b:136560969
Change-Id: Iffc6fb72bff5c9551783e2d9724579c1d653a364
Signed-off-by: Crag.Wang <crag_wang@dell.corp-partner.google.com>
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/1692081
Reviewed-by: Sean Paul <seanpaul@chromium.org>
Reviewed-by: Yijung - <yjjung@melfas.corp-partner.google.com>
Reviewed-by: Nick Crews <ncrews@chromium.org>

drivers/input/touchscreen/melfas_mip4.c
@@ -374,12 +374,11 @@ static int mip4_query_device(struct mip4_ts *ts)
 
 static int mip4_power_on(struct mip4_ts *ts)
 {
-	if (ts->gpio_ce) {
+	if (ts->gpio_ce)
 		gpiod_set_value_cansleep(ts->gpio_ce, 1);
 
-		/* Booting delay : 200~300ms */
-		usleep_range(200 * 1000, 300 * 1000);
-	}
+	/* Booting delay : 200~300ms */
+	usleep_range(200 * 1000, 300 * 1000);
 
 	return 0;
 }

commit c5c6cdab78d8a35d784ab87dc200bd3bfa929bad

CHROMIUM: Input: melfas_mip4 - Apply Wilco specific delay time to sarien only
Commit c5ca96db39da ("FROMLIST: input: touchscreen: add delay time to
device power on") adds 300 ms delay in mip4_power_on(), which is called
by mip4_resume(), so devices (eg. hana) using melfas touchscreen
would gain 300 ms in resume time. Limit the delay to Sarien only.

BUG=b:136560969, b:151124595, b:142698105
TEST=No 300 ms delay in resume on hana
TEST=bind and unbind touch and verify touch function on sarien

Fixes: c5ca96db39da ("FROMLIST: input: touchscreen: add delay time to device power on")
Signed-off-by: Hsin-Yi Wang <hsinyi@chromium.org>
Change-Id: I26ad91a6775101bb8e04a6c3947c9789d860eb00
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2100953
Reviewed-by: Daniel Kurtz <djkurtz@chromium.org>

drivers/input/touchscreen/melfas_mip4.c
@@ -18,6 +18,7 @@
 
 #include <linux/acpi.h>
 #include <linux/delay.h>
+#include <linux/dmi.h>
 #include <linux/firmware.h>
 #include <linux/gpio/consumer.h>
 #include <linux/i2c.h>
@@ -181,6 +182,8 @@ struct mip4_ts {
 	unsigned short key_code[MIP4_MAX_KEYS];
 
 	bool wake_irq_enabled;
+
+	bool poweron_delay;
 
 	u8 buf[MIP4_BUF_SIZE];
 };
@@ -374,12 +377,15 @@ static int mip4_query_device(struct mip4_ts *ts)
 
 static int mip4_power_on(struct mip4_ts *ts)
 {
-	if (ts->gpio_ce)
+	if (ts->gpio_ce) {
 		gpiod_set_value_cansleep(ts->gpio_ce, 1);
 
-	/* Booting delay : 200~300ms */
-	usleep_range(200 * 1000, 300 * 1000);
-
+		/* Booting delay : 200~300ms */
+		usleep_range(200 * 1000, 300 * 1000);
+	} else if (ts->poweron_delay) {
+		/* Booting delay : 200~300ms */
+		usleep_range(200 * 1000, 300 * 1000);
+	}
 	return 0;
 }
 
@@ -1432,6 +1438,18 @@ static const struct attribute_group mip4_attr_group = {
 	.attrs = mip4_attrs,
 };
 
+/* Some firmware needs additional delay in poweron */
+static const struct dmi_system_id mip4_poweron_delay_dmi_table[] = {
+	{
+		.ident = "Google Sarien",
+		.matches = {
+			DMI_MATCH(DMI_BOARD_VENDOR, "Dell Inc."),
+			DMI_MATCH(DMI_BOARD_NAME, "Sarien"),
+		},
+	},
+	{ }
+};
+
 static int mip4_probe(struct i2c_client *client, const struct i2c_device_id *id)
 {
 	struct mip4_ts *ts;
@@ -1451,6 +1469,7 @@ static int mip4_probe(struct i2c_client *client, const struct i2c_device_id *id)
 	if (!input)
 		return -ENOMEM;
 
+	ts->poweron_delay = dmi_check_system(mip4_poweron_delay_dmi_table);
 	ts->client = client;
 	ts->input = input;
 