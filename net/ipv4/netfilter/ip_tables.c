79088e4cbeb3cfc6093b4bfcd3ba2ed0a7e223c7
Oktapra Amtono <oktapra.amtono@gmail.com>
2020-04-20 20:49:21 +0700

    net: netfilter: Disable nf_log_trace
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>
    [ Tashar02: Compile out tracing using CONFIG_DEBUG_KERNEL macro ]
    Co-authored-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- net/ipv4/netfilter/ip_tables.c
+++ net/ipv4/netfilter/ip_tables.c
@@ -146,6 +146,7 @@ static const char *const comments[] = {
 	[NF_IP_TRACE_COMMENT_POLICY]	= "policy",
 };
 
+#ifdef CONFIG_DEBUG_KERNEL
 static const struct nf_loginfo trace_loginfo = {
 	.type = NF_LOG_TYPE_LOG,
 	.u = {
@@ -155,6 +156,7 @@ static const struct nf_loginfo trace_loginfo = {
 		},
 	},
 };
+#endif
 
 /* Mildly perf critical (only if packet tracing is on) */
 static inline int
@@ -211,9 +213,11 @@ static void trace_packet(struct net *net,
 		    &chainname, &comment, &rulenum) != 0)
 			break;
 
+#ifdef CONFIG_DEBUG_KERNEL
 	nf_log_trace(net, AF_INET, hook, skb, in, out, &trace_loginfo,
 		     "TRACE: %s:%s:%s:%u ",
 		     tablename, chainname, comment, rulenum);
+#endif
 }
 #endif
 
