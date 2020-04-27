6435892968a7f338d1a9c41084e4e41af8160a39
Tim Chen <tim-chen@quanta.corp-partner.google.com>
2020-04-27 19:26:57 +0800

    HACK: CHROMIUM: nvme: add WD 256G SSD to support nvme quirk suspend
    Add WD SN530 256G SSD to support suspend.
    
    BUG=b:154891692
    TEST=None
    
    Change-Id: I1f1418f5241fd37a1d8b327ab53ffc1437e2aebc
    Signed-off-by: Tim Chen <tim-chen@quanta.corp-partner.google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2167677
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Commit-Queue: Sam McNally <sammc@chromium.org>

--- drivers/nvme/host/core.c
+++ drivers/nvme/host/core.c
@@ -2137,6 +2137,11 @@ static const struct nvme_core_quirk_entry core_quirks[] = {
 		.vid = 0x144d,
 		.mn = "SAMSUNG MZALQ128HBHQ-000L2",
 		.quirks = NVME_QUIRK_SIMPLE_SUSPEND,
+	},
+	{
+		.vid = 0x15b7,
+		.mn = "WDC PC SN530 SDBPNPZ-256G-1014",
+		.quirks = NVME_QUIRK_SIMPLE_SUSPEND,
 	}
 };
 
