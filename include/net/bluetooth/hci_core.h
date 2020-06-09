8f8276bf4ace1edd9fc84522bda7bb517c075a14
Manish Mandlik <mmandlik@google.com>
2020-06-09 09:20:44 -0700

    Revert "CHROMIUM: bluetooth: disable 5.x LE BlueZ features"
    This reverts commit 5732b250db1ddf17acd3377f251f057d4e9ba5d6.
    
    Bluetooth 5.x features were disabled earlier as they were not used in
    other Chromium kernels and some previous Chromium bug fixes were based
    on the legacy sequences and commands. Since those fixes are no longer
    required and have been reverted, it is now okay to re-enable 5.x
    features. Also, these features are required for development of new
    Chromium features.
    
    BUG=b:148226565, b:133183613
    TEST=Built
    
    Signed-off-by: Manish Mandlik <mmandlik@google.com>
    Change-Id: I37e7b9469dc05ee5646f1b10ac77e5c14c06389d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2236418
    Reviewed-by: Alain Michaud <alainm@chromium.org>
    Tested-by: Manish Mandlik <mmandlik@chromium.org>
    Commit-Queue: Manish Mandlik <mmandlik@chromium.org>

--- include/net/bluetooth/hci_core.h
+++ include/net/bluetooth/hci_core.h
@@ -1278,33 +1278,23 @@ void hci_conn_del_sysfs(struct hci_conn *conn);
 #define bredr_sc_enabled(dev)  (lmp_sc_capable(dev) && \
 				hci_dev_test_flag(dev, HCI_SC_ENABLED))
 
-/* Disable 5.0 features for unified behavior accross chromium BlueZ kernels */
-#define SPEC_5_x_LE_FEATURES_ENABLE (0)
+#define scan_1m(dev) (((dev)->le_tx_def_phys & HCI_LE_SET_PHY_1M) || \
+		      ((dev)->le_rx_def_phys & HCI_LE_SET_PHY_1M))
 
-#define scan_1m(dev) ((((dev)->le_tx_def_phys & HCI_LE_SET_PHY_1M) || \
-		      ((dev)->le_rx_def_phys & HCI_LE_SET_PHY_1M)) & \
-		      SPEC_5_x_LE_FEATURES_ENABLE)
+#define scan_2m(dev) (((dev)->le_tx_def_phys & HCI_LE_SET_PHY_2M) || \
+		      ((dev)->le_rx_def_phys & HCI_LE_SET_PHY_2M))
 
-#define scan_2m(dev) ((((dev)->le_tx_def_phys & HCI_LE_SET_PHY_2M) || \
-		      ((dev)->le_rx_def_phys & HCI_LE_SET_PHY_2M)) & \
-		      SPEC_5_x_LE_FEATURES_ENABLE)
-
-#define scan_coded(dev) ((((dev)->le_tx_def_phys & HCI_LE_SET_PHY_CODED) || \
-			 ((dev)->le_rx_def_phys & HCI_LE_SET_PHY_CODED)) & \
-			 SPEC_5_x_LE_FEATURES_ENABLE)
+#define scan_coded(dev) (((dev)->le_tx_def_phys & HCI_LE_SET_PHY_CODED) || \
+			 ((dev)->le_rx_def_phys & HCI_LE_SET_PHY_CODED))
 
 /* Use ext scanning if set ext scan param and ext scan enable is supported */
-#define use_ext_scan(dev) ((((dev)->commands[37] & 0x20) && \
-			   ((dev)->commands[37] & 0x40)) & \
-			   SPEC_5_x_LE_FEATURES_ENABLE)
-
+#define use_ext_scan(dev) (((dev)->commands[37] & 0x20) && \
+			   ((dev)->commands[37] & 0x40))
 /* Use ext create connection if command is supported */
-#define use_ext_conn(dev) (((dev)->commands[37] & 0x80) & \
-			   SPEC_5_x_LE_FEATURES_ENABLE)
+#define use_ext_conn(dev) ((dev)->commands[37] & 0x80)
 
 /* Extended advertising support */
-#define ext_adv_capable(dev) ((((dev)->le_features[1] & HCI_LE_EXT_ADV)) & \
-			   SPEC_5_x_LE_FEATURES_ENABLE)
+#define ext_adv_capable(dev) (((dev)->le_features[1] & HCI_LE_EXT_ADV))
 
 /* ----- HCI protocols ----- */
 #define HCI_PROTO_DEFER             0x01
