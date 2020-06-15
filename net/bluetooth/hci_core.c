60b52579b105afa56e212296d0e5dc5cadbc9a58
Alain Michaud <alainm@chromium.org>
2020-06-15 14:16:41 +0000

    FROMLIST: bluetooth: Adding a configurable autoconnect timeout
    This patch adds a configurable LE autoconnect timeout.
    
    (am from https://patchwork.kernel.org/patch/11605821/)
    
    BUG=b:152815733
    TEST=Validate that the configured LE autoconnect timer is applied.
    
    Signed-off-by: Alain Michaud <alainm@chromium.org>
    Change-Id: Ifc2e2ac2d93ce3662aa4a3a89165348efbe91761
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2245358
    Reviewed-by: Abhishek Pandit-Subedi <abhishekpandit@chromium.org>

--- net/bluetooth/hci_core.c
+++ net/bluetooth/hci_core.c
@@ -3343,6 +3343,7 @@ struct hci_dev *hci_alloc_dev(void)
 	hdev->le_tx_def_phys = HCI_LE_SET_PHY_1M;
 	hdev->le_rx_def_phys = HCI_LE_SET_PHY_1M;
 	hdev->def_multi_adv_rotation_duration = HCI_DEFAULT_ADV_DURATION;
+	hdev->def_le_autoconnect_timeout = HCI_LE_AUTOCONN_TIMEOUT;
 
 	hdev->rpa_timeout = HCI_DEFAULT_RPA_TIMEOUT;
 	hdev->discov_interleaved_timeout = DISCOV_INTERLEAVED_TIMEOUT;
