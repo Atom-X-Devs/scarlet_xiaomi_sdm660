ce53feb85da0d335885fe5d5aa0a0deaca233b54
Alain Michaud <alainm@chromium.org>
2020-06-17 14:35:59 +0000

    Revert "CHROMIUM: bluetooth: set advertising intervals"
    This reverts commit a109a3dd3840554975b5e65676021e40ba282a65.
    
    Reason for revert: Removing the SetAdvertisingInterval feature in favor
    of the MGMT_OP_SET_DEF_SYSTEM_CONFIG mechanism for setting the global
    default advertising intervals.
    
    BUG=b:152815733
    TEST=None
    
    Signed-off-by: Alain Michaud <alainm@chromium.org>
    Cq-Depend: chromium:2249062
    Change-Id: Ib10ff3fb0856e6c02de6ea6eb44f95cd0f63746b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2250020
    Reviewed-by: Daniel Winkler <danielwinkler@google.com>

--- include/net/bluetooth/mgmt.h
+++ include/net/bluetooth/mgmt.h
@@ -109,7 +109,6 @@ struct mgmt_rp_read_index_list {
 #define MGMT_SETTING_STATIC_ADDRESS	0x00008000
 #define MGMT_SETTING_PHY_CONFIGURATION 	0x00010000
 #define MGMT_SETTING_WIDEBAND_SPEECH	0x00020000
-#define MGMT_SETTING_ADVERTISING_INTERVALS	0x00020000
 
 #define MGMT_OP_READ_INFO		0x0004
 #define MGMT_READ_INFO_SIZE		0
@@ -703,12 +702,6 @@ struct mgmt_rp_read_security_info {
 /*
  * Begin chromium only op_codes
  */
-#define MGMT_OP_SET_ADVERTISING_INTERVALS	0x0060
-struct mgmt_cp_set_advertising_intervals {
-	__le16	min_interval;
-	__le16	max_interval;
-} __packed;
-#define MGMT_SET_ADVERTISING_INTERVALS_SIZE	4
 
 #define MGMT_OP_SET_KERNEL_DEBUG			0x0064
 #define MGMT_SET_KERNEL_DEBUG_SIZE			1
