Kaz205/renoir

CHROMIUM: platform/iio: cros_ec_sensor_ring: connect to cros_ec_sensorhub
Remove code that is now in cros_ec_sensorhub.
Keep the iio cros_ec_sensor_ring driver until conversion is done.
Bring current behavior where iio:deviceX for sensors are only for
triggered samples.

BUG=chromium:510242
TEST=tast test with new sensor ABI keeps working.

Change-Id: I4c3847613856a0887793be5992b0dc1a32665371
Signed-off-by: Gwendal Grignou <gwendal@chromium.org>

drivers/platform/chrome/cros_ec_sensorhub_ring.c
@@ -33,6 +33,31 @@
 /* To measure by how much the filter is overshooting, if it happens. */
 #define FUTURE_TS_ANALYTICS_COUNT_MAX 100
 
+#if IS_ENABLED(CONFIG_IIO_CROS_EC_SENSORS_RING)
+/*
+ * To be compliant with existing code, device buffer is only for
+ * triggered samples.
+ */
+static inline int cros_sensorhub_send_sample(
+		struct cros_ec_sensorhub *sensorhub,
+		struct cros_ec_sensors_ring_sample *sample)
+{
+	int id = sample->sensor_id;
+	struct iio_dev *indio_dev;
+	cros_ec_sensorhub_push_samples_cb_t cb =
+		(cros_ec_sensorhub_push_samples_cb_t)
+		sensorhub->push_data[CROS_EC_SENSOR_BROADCAST].push_data_cb;
+
+	if (id > CROS_EC_SENSOR_MAX)
+		return -EINVAL;
+
+	indio_dev = sensorhub->push_data[CROS_EC_SENSOR_BROADCAST].indio_dev;
+	if (!indio_dev)
+		return 0;
+
+	return cb(indio_dev, sample);
+}
+#else
 static inline int cros_sensorhub_send_sample(
 		struct cros_ec_sensorhub *sensorhub,
 		struct cros_ec_sensors_ring_sample *sample)
@@ -40,9 +65,6 @@ static inline int cros_sensorhub_send_sample(
 	int id = sample->sensor_id;
 	cros_ec_sensorhub_push_data_cb_t cb;
 	struct iio_dev *indio_dev;
-
-	if (id > CROS_EC_SENSOR_MAX)
-		return -EINVAL;
 
 	cb = sensorhub->push_data[id].push_data_cb;
 	if (!cb)
@@ -55,6 +77,7 @@ static inline int cros_sensorhub_send_sample(
 
 	return cb(indio_dev, sample->vector, sample->timestamp);
 }
+#endif
 
 /**
  * cros_ec_sensorhub_register_push_data() - register the callback to the hub.
@@ -93,6 +116,29 @@ void cros_ec_sensorhub_unregister_push_data(struct cros_ec_sensorhub *sensorhub,
 	sensorhub->push_data[sensor_num].push_data_cb = NULL;
 }
 EXPORT_SYMBOL_GPL(cros_ec_sensorhub_unregister_push_data);
+
+#if IS_ENABLED(CONFIG_IIO_CROS_EC_SENSORS_RING)
+int cros_ec_sensorhub_register_push_sample(
+		struct cros_ec_sensorhub *sensor_hub,
+		struct iio_dev *indio_dev,
+		cros_ec_sensorhub_push_samples_cb_t cb)
+{
+	return cros_ec_sensorhub_register_push_data(
+			sensor_hub,
+			CROS_EC_SENSOR_BROADCAST,
+			indio_dev,
+			(cros_ec_sensorhub_push_data_cb_t)cb);
+}
+EXPORT_SYMBOL_GPL(cros_ec_sensorhub_register_push_sample);
+
+void cros_ec_sensorhub_unregister_push_sample(
+		struct cros_ec_sensorhub *sensor_hub)
+{
+	cros_ec_sensorhub_unregister_push_data(sensor_hub,
+			CROS_EC_SENSOR_BROADCAST);
+}
+EXPORT_SYMBOL_GPL(cros_ec_sensorhub_unregister_push_sample);
+#endif
 
 /**
  * cros_ec_sensorhub_ring_fifo_enable() - Enable or disable interrupt generation

Atom-X-Devs/scarlet_xiaomi_sdm660

BUG=chromium:510242,chromium:946178
(cherry picked from commit 1c45ac26d7e2e1b6f819bd8c6596451011bc0cbc)
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/1834452

drivers/platform/chrome/cros_ec_sensorhub_ring.c
@@ -32,16 +32,38 @@
 /* To measure by how much the filter is overshooting, if it happens. */
 #define FUTURE_TS_ANALYTICS_COUNT_MAX 100
 
+#if IS_ENABLED(CONFIG_IIO_CROS_EC_SENSORS_RING)
+/*
+ * To be compliant with existing code, device buffer is only for
+ * triggered samples.
+ */
 static inline int
 cros_sensorhub_send_sample(struct cros_ec_sensorhub *sensorhub,
 			   struct cros_ec_sensors_ring_sample *sample)
 {
-	cros_ec_sensorhub_push_data_cb_t cb;
+	cros_ec_sensorhub_push_samples_cb_t cb =
+		(cros_ec_sensorhub_push_samples_cb_t)
+		sensorhub->push_data[sensorhub->sensor_num].push_data_cb;
 	int id = sample->sensor_id;
 	struct iio_dev *indio_dev;
 
 	if (id > sensorhub->sensor_num)
 		return -EINVAL;
+
+	indio_dev = sensorhub->push_data[sensorhub->sensor_num].indio_dev;
+	if (!indio_dev)
+		return 0;
+
+	return cb(indio_dev, sample);
+}
+#else
+static inline int
+cros_sensorhub_send_sample(struct cros_ec_sensorhub *sensorhub,
+			   struct cros_ec_sensors_ring_sample *sample)
+{
+	int id = sample->sensor_id;
+	cros_ec_sensorhub_push_data_cb_t cb;
+	struct iio_dev *indio_dev;
 
 	cb = sensorhub->push_data[id].push_data_cb;
 	if (!cb)
@@ -54,6 +76,7 @@ cros_sensorhub_send_sample(struct cros_ec_sensorhub *sensorhub,
 
 	return cb(indio_dev, sample->vector, sample->timestamp);
 }
+#endif
 
 /**
  * cros_ec_sensorhub_register_push_data() - register the callback to the hub.
@@ -74,7 +97,7 @@ int cros_ec_sensorhub_register_push_data(struct cros_ec_sensorhub *sensorhub,
 					 struct iio_dev *indio_dev,
 					 cros_ec_sensorhub_push_data_cb_t cb)
 {
-	if (sensor_num >= sensorhub->sensor_num)
+	if (sensor_num >= sensorhub->sensor_num + 1)
 		return -EINVAL;
 	if (sensorhub->push_data[sensor_num].indio_dev)
 		return -EINVAL;
@@ -93,6 +116,29 @@ void cros_ec_sensorhub_unregister_push_data(struct cros_ec_sensorhub *sensorhub,
 	sensorhub->push_data[sensor_num].push_data_cb = NULL;
 }
 EXPORT_SYMBOL_GPL(cros_ec_sensorhub_unregister_push_data);
+
+#if IS_ENABLED(CONFIG_IIO_CROS_EC_SENSORS_RING)
+int cros_ec_sensorhub_register_push_sample(
+		struct cros_ec_sensorhub *sensorhub,
+		struct iio_dev *indio_dev,
+		cros_ec_sensorhub_push_samples_cb_t cb)
+{
+	return cros_ec_sensorhub_register_push_data(
+			sensorhub,
+			sensorhub->sensor_num,
+			indio_dev,
+			(cros_ec_sensorhub_push_data_cb_t)cb);
+}
+EXPORT_SYMBOL_GPL(cros_ec_sensorhub_register_push_sample);
+
+void cros_ec_sensorhub_unregister_push_sample(
+		struct cros_ec_sensorhub *sensorhub)
+{
+	cros_ec_sensorhub_unregister_push_data(sensorhub,
+			sensorhub->sensor_num);
+}
+EXPORT_SYMBOL_GPL(cros_ec_sensorhub_unregister_push_sample);
+#endif
 
 /**
  * cros_ec_sensorhub_ring_fifo_enable() - Enable or disable interrupt generation
@@ -1000,9 +1046,10 @@ int cros_ec_sensorhub_ring_add(struct cros_ec_sensorhub *sensorhub)
 
 	/*
 	 * Allocate the callback area based on the number of sensors.
+	 * Add one for the sensor ring.
 	 */
 	sensorhub->push_data = devm_kcalloc(
-			sensorhub->dev, sensorhub->sensor_num,
+			sensorhub->dev, sensorhub->sensor_num + 1,
 			sizeof(*sensorhub->push_data),
 			GFP_KERNEL);
 	if (!sensorhub->push_data)