Atom-X-Devs/scarlet_xiaomi_sdm660

commit 3bfd7663b760d3bf67c4e079b2027580583cb825

CHROMIUM: Bluetooth: btmrvl: Detect hangs and force a replug of the SDIO card
When scanning for BLE devices for a longer period (e.g. because a
BLE device is paired, but not connected) the Marvell 8997 often
ends up in a borked state, which manifests through failures on
certain SDIO transactions.

When such a SDIO failure is detected force a replug of the SDIO
card to initialize it from scratch. Since the SDIO bus is shared
with the WiFi part of the chip this also involves a reset of WiFi.

We probably want to upstream this patch once "mmc: core: Add
sdio_trigger_replug() API" (https://lore.kernel.org/patchwork/patch/1103196/)
or similar lands in the maintainer tree.

BUG=chromium:1000492, chromium:990991
TEST=on jerry with a BLE device paired, but not connected:
  suspend/resume 100x
    => Bluetooth still works
  leave the device running for 12h
    => Bluetooth still works

Change-Id: I40cc9b3d5de04f0631c931d94757fb0f462b24bd
Signed-off-by: Matthias Kaehlcke <mka@chromium.org>
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/1803515
Reviewed-by: Abhishek Pandit-Subedi <abhishekpandit@chromium.org>
Reviewed-by: Douglas Anderson <dianders@chromium.org>

drivers/bluetooth/btmrvl_sdio.c
@@ -59,6 +59,23 @@ static const struct of_device_id btmrvl_sdio_of_match_table[] = {
 	{ }
 };
 
+static void btmrvl_sdio_card_reset_work(struct work_struct *work)
+{
+	struct btmrvl_sdio_card *card =
+		container_of(work, struct btmrvl_sdio_card, reset_work);
+	struct sdio_func *func = card->func;
+
+	sdio_claim_host(func);
+	sdio_trigger_replug(func);
+	sdio_release_host(func);
+}
+
+static void btmrvl_sdio_card_reset(struct btmrvl_sdio_card *card)
+{
+	BT_ERR("resetting SDIO card!");
+	schedule_work(&card->reset_work);
+}
+
 static irqreturn_t btmrvl_wake_irq_bt(int irq, void *priv)
 {
 	struct btmrvl_sdio_card *card = priv;
@@ -703,6 +720,7 @@ static int btmrvl_sdio_card_to_host(struct btmrvl_private *priv)
 	ret = btmrvl_sdio_read_rx_len(card, &buf_len);
 	if (ret < 0) {
 		BT_ERR("read rx_len failed");
+		btmrvl_sdio_card_reset(card);
 		ret = -EIO;
 		goto exit;
 	}
@@ -738,6 +756,7 @@ static int btmrvl_sdio_card_to_host(struct btmrvl_private *priv)
 			  num_blocks * blksz);
 	if (ret < 0) {
 		BT_ERR("readsb failed: %d", ret);
+		btmrvl_sdio_card_reset(card);
 		ret = -EIO;
 		goto exit;
 	}
@@ -842,6 +861,7 @@ static int btmrvl_sdio_read_to_clear(struct btmrvl_sdio_card *card, u8 *ireg)
 	ret = sdio_readsb(card->func, adapter->hw_regs, 0, SDIO_BLOCK_SIZE);
 	if (ret) {
 		BT_ERR("sdio_readsb: read int hw_regs failed: %d", ret);
+		btmrvl_sdio_card_reset(card);
 		return ret;
 	}
 
@@ -1519,6 +1539,8 @@ static int btmrvl_sdio_probe(struct sdio_func *func,
 		card->support_pscan_win_report = data->support_pscan_win_report;
 		card->supports_fw_dump = data->supports_fw_dump;
 	}
+
+	INIT_WORK(&card->reset_work, btmrvl_sdio_card_reset_work);
 
 	if (btmrvl_sdio_register_dev(card) < 0) {
 		BT_ERR("Failed to register BT device!");

drivers/bluetooth/btmrvl_sdio.h
@@ -103,6 +103,7 @@ struct btmrvl_sdio_card {
 	struct btmrvl_private *priv;
 	struct device_node *plt_of_node;
 	struct btmrvl_plt_wake_cfg *plt_wake_cfg;
+	struct work_struct reset_work;
 };
 
 struct btmrvl_sdio_device {

commit 2fe05e1562cd6624dc4bf0036abc3ea19b4f42a0

CHROMIUM: Bluetooth: btmrvl: Fix use-after-free when forcing a replug of the SDIO card
Commit 3bfd7663b760 ("CHROMIUM: Bluetooth: btmrvl: Detect hangs and
force a replug of the SDIO card") introduced workqueue task that forces
a replug of the SDIO card when an error is detected. When the card is
removed, it is possible that a reset work task is pending. If that task
runs after the removal has completed, the access of the btmrvl_sdio_card
will result in a use-after-free since the card has been deallocated.

BUG=b:157434249
TEST=On elm-kernelnext, run
     'cts-tradefed run commandAndExit cts -m CtsBluetoothTestCases'
     100 times => system does not kernel panic and reboot

Change-Id: I005aa389cf0505490cad448f99a3a05f9989569b
Signed-off-by: Anand K Mistry <amistry@google.com>
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2265535
Reviewed-by: Daniel Kurtz <djkurtz@chromium.org>
Reviewed-by: Abhishek Pandit-Subedi <abhishekpandit@chromium.org>
Tested-by: Anand K Mistry <amistry@chromium.org>
Commit-Queue: Anand K Mistry <amistry@chromium.org>

drivers/bluetooth/btmrvl_sdio.c
@@ -1613,6 +1613,18 @@ static void btmrvl_sdio_remove(struct sdio_func *func)
 			card->priv->surprise_removed = true;
 			btmrvl_sdio_unregister_dev(card);
 			btmrvl_remove_card(card->priv);
+
+			/* Cancel any pending reset work. If a work item is
+			 * pending and runs after removal has finished, the work
+			 * item would cause a use-after-free on the
+			 * btmrvl_sdio_card which has been deallocated.
+			 *
+			 * btmrvl_sdio_card_reset() is called from either the
+			 * main thread or the interrupt handler. At this point,
+			 * neither should occur and cause more reset work, so
+			 * it's safe to cancel any pending reset work.
+			 */
+			cancel_work_sync(&card->reset_work);
 		}
 	}
 }