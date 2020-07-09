9ab2b772edbbf7ae886fb43b85a2692334d538c3
Tzung-Bi Shih <tzungbi@google.com>
2020-07-09 20:24:43 +0800

    BACKPORT: FROMGIT: ASoC: mediatek: mt8183-da7219: extract codec and DAI names
    Extracts codec and DAI names of DA7219.
    
    Signed-off-by: Tzung-Bi Shih <tzungbi@google.com>
    Link: https://lore.kernel.org/r/20200709122445.1584497-4-tzungbi@google.com
    Signed-off-by: Mark Brown <broonie@kernel.org>
    
    (cherry picked from commit e5d4bdffc8c89ce253e5663fecd09c893ece7eec
     http://git.kernel.org/pub/scm/linux/kernel/git/broonie/sound.git for-next)
    
    Conflicts:
       sound/soc/mediatek/mt8183/mt8183-da7219-max98357.c
    [Resolved for 4.19 doesn't have for_each_rtd_codec_dais.]
    
    BUG=b:158996684
    TEST=AP boot to shell on Damu && aplay -l
    
    Signed-off-by: Tzung-Bi Shih <tzungbi@chromium.org>
    Change-Id: Ic9889cd58ae9d8a253736fc59086f9a6169e6238
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2289477
    Reviewed-by: Nicolas Boichat <drinkcat@chromium.org>

--- sound/soc/mediatek/mt8183/mt8183-da7219-max98357.c
+++ sound/soc/mediatek/mt8183/mt8183-da7219-max98357.c
@@ -17,6 +17,9 @@
 #include "../../codecs/da7219.h"
 #include "mt8183-afe-common.h"
 
+#define DA7219_CODEC_DAI "da7219-hifi"
+#define DA7219_DEV_NAME "da7219.5-001a"
+
 struct mt8183_da7219_max98357_priv {
 	struct snd_soc_jack headset_jack;
 };
@@ -55,7 +58,7 @@ static int mt8183_da7219_i2s_hw_params(struct snd_pcm_substream *substream,
 	for (j = 0; j < rtd->num_codecs; j++) {
 		struct snd_soc_dai *codec_dai = rtd->codec_dais[j];
 
-		if (!strcmp(codec_dai->component->name, "da7219.5-001a")) {
+		if (!strcmp(codec_dai->component->name, DA7219_DEV_NAME)) {
 			ret = snd_soc_dai_set_sysclk(codec_dai,
 						     DA7219_CLKSRC_MCLK,
 						     mclk_fs,
@@ -83,12 +86,13 @@ static int mt8183_da7219_i2s_hw_params(struct snd_pcm_substream *substream,
 static int mt8183_da7219_hw_free(struct snd_pcm_substream *substream)
 {
 	struct snd_soc_pcm_runtime *rtd = substream->private_data;
+	struct snd_soc_dai *codec_dai;
 	int ret = 0, j;
 
 	for (j = 0; j < rtd->num_codecs; j++) {
-		struct snd_soc_dai *codec_dai = rtd->codec_dais[j];
+		codec_dai = rtd->codec_dais[j];
 
-		if (!strcmp(codec_dai->component->name, "da7219.5-001a")) {
+		if (!strcmp(codec_dai->component->name, DA7219_DEV_NAME)) {
 			ret = snd_soc_dai_set_pll(codec_dai,
 						  0, DA7219_SYSCLK_MCLK, 0, 0);
 			if (ret < 0) {
@@ -268,13 +272,13 @@ SND_SOC_DAILINK_DEFS(i2s1,
 
 SND_SOC_DAILINK_DEFS(i2s2,
 	DAILINK_COMP_ARRAY(COMP_CPU("I2S2")),
-	DAILINK_COMP_ARRAY(COMP_CODEC("da7219.5-001a", "da7219-hifi")),
+	DAILINK_COMP_ARRAY(COMP_CODEC(DA7219_DEV_NAME, DA7219_CODEC_DAI)),
 	DAILINK_COMP_ARRAY(COMP_EMPTY()));
 
 SND_SOC_DAILINK_DEFS(i2s3,
 	DAILINK_COMP_ARRAY(COMP_CPU("I2S3")),
 	DAILINK_COMP_ARRAY(COMP_CODEC("max98357a", "HiFi"),
-			 COMP_CODEC("da7219.5-001a", "da7219-hifi")),
+			   COMP_CODEC(DA7219_DEV_NAME, DA7219_CODEC_DAI)),
 	DAILINK_COMP_ARRAY(COMP_EMPTY()));
 
 SND_SOC_DAILINK_DEFS(i2s5,
