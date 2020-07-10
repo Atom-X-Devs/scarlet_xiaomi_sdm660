b378f2983abc006d169cd36cef1d93db871620b8
Abhishek Pandit-Subedi <abhishekpandit@chromium.org>
2020-07-10 16:31:57 -0700

    Revert "BACKPORT: FROMLIST: Bluetooth: Add mgmt op set_wake_capable"
    This reverts commit f969649a20cf3709bfce1fb8eedd21e8188056dc.
    
    Reason for revert: Replacing with upstream version.
    
    BUG=b:152903716
    TEST=Ran bluetooth_AdapterSRSanity on series
    
    Change-Id: Ib4230dbda6eda7c8922c8502849d0f5384a355bc
    Signed-off-by: Abhishek Pandit-Subedi <abhishekpandit@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2296220
    Reviewed-by: Alain Michaud <alainm@chromium.org>

--- include/net/bluetooth/mgmt.h
+++ include/net/bluetooth/mgmt.h
@@ -762,14 +762,6 @@ struct mgmt_rp_remove_adv_monitor {
 /*
  * Begin chromium only op_codes
  */
-
-#define MGMT_OP_SET_WAKE_CAPABLE			0x0065
-#define MGMT_SET_WAKE_CAPABLE_SIZE			8
-struct mgmt_cp_set_wake_capable {
-	struct mgmt_addr_info addr;
-	u8 wake_capable;
-} __packed;
-
 /*
  * End chromium only op_codes
  */
