c81e80433b10ab76fa250f41fa5d76e7fd74945a
Sean Paul <seanpaul@chromium.org>
2020-08-18 11:39:00 -0400

    BACKPORT: drm/i915: Factor out HDCP shim functions from dp for use by dp_mst
    These functions are all the same for dp and dp_mst, so move them into a
    dedicated file for both sst and mst to use.
    
    Reviewed-by: Ramalingam C <ramalingam.c@intel.com>
    Signed-off-by: Sean Paul <seanpaul@chromium.org>
    Link: https://patchwork.freedesktop.org/patch/msgid/20191203173638.94919-11-sean@poorly.run #v1
    Link: https://patchwork.freedesktop.org/patch/msgid/20191212190230.188505-12-sean@poorly.run #v2
    Link: https://patchwork.freedesktop.org/patch/msgid/20200117193103.156821-12-sean@poorly.run #v3
    Link: https://patchwork.freedesktop.org/patch/msgid/20200218220242.107265-12-sean@poorly.run #v4
    Link: https://patchwork.freedesktop.org/patch/msgid/20200305201236.152307-12-sean@poorly.run #v5
    Link: https://patchwork.freedesktop.org/patch/msgid/20200429195502.39919-12-sean@poorly.run #v6
    Link: https://patchwork.freedesktop.org/patch/msgid/20200623155907.22961-13-sean@poorly.run #v7
    
    Changes in v2:
    -None
    Changes in v3:
    -Created intel_dp_hdcp.c for the shared functions to live (Ville)
    Changes in v4:
    -Rebased on new drm logging change
    Changes in v5:
    -None
    Changes in v6:
    -None
    Changes in v7:
    -Rebased patch
    Changes in v8:
    -None
    
    Signed-off-by: Ramalingam C <ramalingam.c@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20200818153910.27894-13-sean@poorly.run
    
    (cherry picked from commit d079b7e4b6389e36c50730985100bb528f0d3883)
    
    Conflicts:
       drivers/gpu/drm/i915/Makefile
       drivers/gpu/drm/i915/display/intel_dp.c
       drivers/gpu/drm/i915/display/intel_dp.h
    
    BUG=b:156333982
    TEST=None
    
    Signed-off-by: Sam McNally <sammc@chromium.org>
    Change-Id: I80b21c9e7c6d49fc57468793f3b51a26ae82ed5d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2455070
    Reviewed-by: Edward O'Callaghan <quasisec@chromium.org>

--- drivers/gpu/drm/i915/intel_drv.h
+++ drivers/gpu/drm/i915/intel_drv.h
@@ -1860,6 +1860,10 @@ bool intel_digital_port_connected(struct intel_encoder *encoder);
 /* intel_dp_aux_backlight.c */
 int intel_dp_aux_init_backlight_funcs(struct intel_connector *intel_connector);
 
+/* intel_dp_hdcp.c */
+int intel_dp_init_hdcp(struct intel_digital_port *dig_port,
+		       struct intel_connector *intel_connector);
+
 /* intel_dp_mst.c */
 int intel_dp_mst_encoder_init(struct intel_digital_port *intel_dig_port, int conn_id);
 void intel_dp_mst_encoder_cleanup(struct intel_digital_port *intel_dig_port);
