1c2ebc7e8858513d2b97306046c5cfee1d7a9271
Joel Fernandes <joelaf@google.com>
2020-08-27 15:47:51 -0400

    FIXUP: FROMLIST: sched: Use sched-RCU in core-scheduling balancing logi
    This warning is a false-positive:
    [   26.661596] ============================
    [   26.665670] WARNING: suspicious RCU usage
    [   26.669735] 5.4.60 #1 Not tainted
    [   26.673118] -----------------------------
    [   26.677185] kernel/sched/core.c:5064 suspicious rcu_dereference_check() usage!
    [   26.684432]
    [   26.684432] other info that might help us debug this:
    [   26.684432]
    [   26.692456]
    [   26.692456] rcu_scheduler_active = 2, debug_locks = 1
    [   26.699000] 1 lock held by swapper/3/0:
    [   26.702860]  #0: ffffffff82c59548 (rcu_read_lock_sched){....}, at: rcu_lock_acquire+0x0/0x23
    [   26.711323]
    [   26.711323] stack backtrace:
    [   26.715690] CPU: 3 PID: 0 Comm: swapper/3 Not tainted 5.4.60 #1
    [   26.721606] Hardware name: Google Dalboz/Dalboz, BIOS Google_Dalboz.13432.0.2020_08_26_1406 08/26/2020
    [   26.730902] Call Trace:
    [   26.733358]  dump_stack+0xbd/0x11d
    [   26.736762]  sched_core_balance+0xf1/0x4b3
    [   26.740865]  ? do_raw_spin_lock+0x96/0x98
    [   26.744879]  __balance_callback+0x50/0xa1
    [   26.748893]  __schedule+0x85e/0x11fd
    [   26.752472]  schedule_idle+0x21/0x31
    [   26.756050]  do_idle+0x201/0x224
    [   26.759282]  cpu_startup_entry+0x1d/0x1f
    [   26.763208]  start_secondary+0x16e/0x1aa
    [   26.767130]  secondary_startup_64+0xa4/0xb0
    
    It is because rcu_read_lock_sched() API does not imply rcu_read_lock_held(). To
    fix it, just switch to using preempt_disable() to keep the
    rcu_read_lock_sched() sematics, but using rcu_read_lock() within to satisfy the
    lockdep warning.
    
    This will have no change in behavior.
    
    BUG=b:166637080
    Signed-off-by: Joel Fernandes <joelaf@google.com>
    Change-Id: I5ece23b0029a6cda3668707c72240a7df92c23d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2380396
    Reviewed-by: Sonny Rao <sonnyrao@chromium.org>
    Reviewed-by: Raul E Rangel <rrangel@chromium.org>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -4344,7 +4344,8 @@ static void sched_core_balance(struct rq *rq)
 	struct sched_domain *sd;
 	int cpu = cpu_of(rq);
 
-	rcu_read_lock_sched();
+	preempt_disable();
+	rcu_read_lock();
 	raw_spin_unlock_irq(rq_lockp(rq));
 	for_each_domain(cpu, sd) {
 		if (!(sd->flags & SD_LOAD_BALANCE))
@@ -4357,7 +4358,8 @@ static void sched_core_balance(struct rq *rq)
 			break;
 	}
 	raw_spin_lock_irq(rq_lockp(rq));
-	rcu_read_unlock_sched();
+	rcu_read_unlock();
+	preempt_enable();
 }
 
 static DEFINE_PER_CPU(struct callback_head, core_balance_head);
