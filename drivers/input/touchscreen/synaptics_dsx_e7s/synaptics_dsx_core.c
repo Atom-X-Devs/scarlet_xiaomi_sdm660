17c32b8e499306c9f7d92ac9759b1c00623654c4
Edwin Moquete <edwinmmoquete@gmail.com>
2020-12-23 22:53:17 -0400

    touchscreen: synaptics_dsx_e7s: Use sscanf in wake_gesture_store
    Using kstrtouint breaks DT2W
    
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>
    Signed-off-by: Anush02198 <Anush.4376@gmail.com>
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/input/touchscreen/synaptics_dsx_e7s/synaptics_dsx_core.c
+++ drivers/input/touchscreen/synaptics_dsx_e7s/synaptics_dsx_core.c
@@ -907,7 +907,7 @@ static ssize_t synaptics_rmi4_wake_gesture_store(struct device *dev,
 	unsigned int input;
 	struct synaptics_rmi4_data *rmi4_data = dev_get_drvdata(dev);
 
-	if (kstrtouint(buf, 10, &input) != 1)
+	if (sscanf(buf, "%u", &input) != 1)
 		return -EINVAL;
 
 	input = input > 0 ? 1 : 0;
