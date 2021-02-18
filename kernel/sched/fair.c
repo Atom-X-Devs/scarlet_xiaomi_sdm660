165d92bd2e1cfd55c5362a78fcadf850e8c2aa8d
Pavankumar Kondeti <quic_pkondeti@quicinc.com>
2021-02-18 11:09:28 +0530

    BACKPORT: ANDROID: sched: Exempt paused CPU from nohz idle balance
    A CPU can be paused while it is idle with it's tick stopped.
    nohz_balance_exit_idle() should be called from the local CPU,
    so it can't be called during pause which can happen remotely.
    This results in paused CPU participating in the nohz idle balance,
    which should be avoided. This can be done by calling
    
    Fix this issue by calling nohz_balance_exit_idle() from the paused
    CPU when it exits and enters idle again. This lazy approach avoids
    waking the CPU from idle during pause.
    
    Bug: 180530906
    Change-Id: Ia2dfd9c9cac9b0f37c55a9256b9d5f3141ca0421
    Signed-off-by: Pavankumar Kondeti <quic_pkondeti@quicinc.com>
    [ Tashar02: Backport to k4.19 ]
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -11974,9 +11974,20 @@ void nohz_balance_enter_idle(int cpu)
 
 	SCHED_WARN_ON(cpu != smp_processor_id());
 
-	/* If this CPU is going down, then nothing needs to be done: */
-	if (!cpu_active(cpu))
+	if (!cpu_active(cpu)) {
+		/*
+		 * A CPU can be paused while it is idle with it's tick
+		 * stopped. nohz_balance_exit_idle() should be called
+		 * from the local CPU, so it can't be called during
+		 * pause. This results in paused CPU participating in
+		 * the nohz idle balance, which should be avoided.
+		 *
+		 * When the paused CPU exits idle and enters again,
+		 * exempt the paused CPU from nohz_balance_exit_idle.
+		 */
+		nohz_balance_exit_idle(rq);
 		return;
+	}
 
 #ifdef CONFIG_SONY_SCHED
 	cpumask_clear_cpu(cpu, &cpu_wclaimed_mask);
