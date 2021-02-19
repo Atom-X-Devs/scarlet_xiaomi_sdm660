bfd375f4e3d001e13cc76b1e2ac2e2d2ad2f6c16
Oktapra Amtono <oktapra.amtono@gmail.com>
2021-02-19 08:23:05 +0700

    treewide: silence some logger
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>
    Signed-off-by: Anush02198 <Anush.4376@gmail.com>
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/video/fbdev/msm/mdss_dp.c
+++ drivers/video/fbdev/msm/mdss_dp.c
@@ -4471,7 +4471,7 @@ static int __maybe_unused mdss_dp_usbpd_setup(struct mdss_dp_drv_pdata *dp_drv)
 						    pd_phandle);
 
 	if (IS_ERR(dp_drv->pd)) {
-		pr_err("get_usbpd phandle failed (%ld)\n",
+		pr_debug("get_usbpd phandle failed (%ld)\n",
 				PTR_ERR(dp_drv->pd));
 		return PTR_ERR(dp_drv->pd);
 	}
@@ -4546,7 +4546,7 @@ static int mdss_dp_probe(struct platform_device *pdev)
 
 #if ENABLE_DP_SETUP
 	if (mdss_dp_usbpd_setup(dp_drv)) {
-		pr_err("Error usbpd setup!\n");
+		pr_debug("Error usbpd setup!\n");
 		dp_drv = NULL;
 		return -EPROBE_DEFER;
 	}
