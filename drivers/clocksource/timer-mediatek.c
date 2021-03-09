c22becf1dbc51bfc2ee102c2dfaac4896817caf3
Fengquan Chen <fengquan.chen@mediatek.corp-partner.google.com>
2021-03-09 11:56:43 +0800

    FROMLIST: clocksource/drivers/timer-mediatek: optimize systimer irq clear flow on Mediatek Socs
    1)ensure systimer is enabled before clear and disable interrupt, which
    only for systimer in Mediatek Socs.
    
    2)clear any pending timer-irq when shutdown to keep suspend flow clean,
    when use systimer as tick-broadcast timer
    
    (am from https://patchwork.kernel.org/project/linux-mediatek/patch/1614670085-26229-2-git-send-email-Fengquan.Chen@mediatek.com/)
    
    BUG=b:176946362
    TEST=boot asurada
    
    Signed-off-by: Fengquan Chen <fengquan.chen@mediatek.com>
    Change-Id: I97e16d766d52afddafc6b7ac24a9b5f152cda104
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2721679
    Reviewed-by: Sean Paul <seanpaul@chromium.org>
    Reviewed-by: Nicolas Boichat <drinkcat@chromium.org>
    Tested-by: Nicolas Boichat <drinkcat@chromium.org>
    Commit-Queue: Nicolas Boichat <drinkcat@chromium.org>

--- drivers/clocksource/timer-mediatek.c
+++ drivers/clocksource/timer-mediatek.c
@@ -84,6 +84,7 @@ static void __iomem *gpt_sched_reg __read_mostly;
 static void mtk_syst_ack_irq(struct timer_of *to)
 {
 	/* Clear and disable interrupt */
+	writel(SYST_CON_EN, SYST_CON_REG(to));
 	writel(SYST_CON_IRQ_CLR | SYST_CON_EN, SYST_CON_REG(to));
 }
 
@@ -120,6 +121,9 @@ static int mtk_syst_clkevt_next_event(unsigned long ticks,
 
 static int mtk_syst_clkevt_shutdown(struct clock_event_device *clkevt)
 {
+	/* Clear any irq */
+	mtk_syst_ack_irq(to_timer_of(clkevt));
+
 	/* Disable timer */
 	writel(0, SYST_CON_REG(to_timer_of(clkevt)));
 
