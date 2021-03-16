adb1ecc6a22063a67d23b90a7f871a0d5f638ab6
Alex Winkowski <dereference23@outlook.com>
2021-03-16 14:06:17 +0000

    mm: Set swappiness for CONFIG_INCREASE_MAXIMUM_SWAPPINESS=y case
    also set default swappiness to 100
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -178,7 +178,11 @@ int kswapd_threads_current = DEF_KSWAPD_THREADS_PER_NODE;
 /*
  * From 0 .. 100.  Higher means more swappy.
  */
-int vm_swappiness = 60;
+#ifndef CONFIG_INCREASE_MAXIMUM_SWAPPINESS
+int vm_swappiness = 100;
+#else
+int vm_swappiness = 190;
+#endif
 /*
  * The total number of pages which are beyond the high watermark within all
  * zones.
