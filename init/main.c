5313e663ddffe86154ba3171b02d7d3799fab4d7
Wei Wang <wvw@google.com>
2021-04-03 10:35:04 +0530

    init/main.c: add sync point between each level
    With PROBE_PREFER_ASYNCHRONOUS, drivers have ability to do asynchronous
    probing, however this might break promise of ordering. This patch is to
    add additional sync point between each init level.
    
    Test: Boot
    Bug: 63716230
    Bug: 77146523
    Bug: 115776306
    Bug: 116061339
    Change-Id: I2d2add41b29eaeecd6e68adcd09d9f5ec40fb97d
    Signed-off-by: Wei Wang <wvw@google.com>
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- init/main.c
+++ init/main.c
@@ -1021,6 +1021,8 @@ static void __init do_initcalls(void)
 		/* Parser modifies command_line, restore it each time */
 		strcpy(command_line, saved_command_line);
 		do_initcall_level(level, command_line);
+		/* finish all async calls before going into next level */
+		async_synchronize_full();
 	}
 
 	kfree(command_line);
