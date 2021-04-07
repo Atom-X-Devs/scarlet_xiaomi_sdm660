Atom-X-Devs/scarlet_xiaomi_sdm660

commit 098a98758e31584130da0cd3f3f9c2788c9b9fe0

CHROMIUM: iio: cros_ec_light: Fix regular and RGB callbacks
Calculation based on scan mask was incorrect.
Instead use the usual callback for single channel ALS, and
just save the first channel for RGB sensor.

Fix second RGB callback by preserving the first channel when necessary.

Fixes: 93069b13d ("CHROMIUM: iio: cros_ec_light: Add FIFO support for RGB sensor")

BUG=b:172790261
TEST=On nocturne with 4.19 (ALS sensor OPT3001), with USE flag
iioservice set, check we can retrieve samples:
   echo 1 > scan_elements/in_illuminance_en
   echo 1 > scan_elements/in_timestamp_en
   echo 1 > sampling_frequency
   echo 1 > buffer/enable
   od -x /dev/iio\:device4

Change-Id: Ie0f5247d21df0adb2c518d7dae3946c58c9ce950
Signed-off-by: Gwendal Grignou <gwendal@chromium.org>
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2568226
Reviewed-by: Cheng-Hao Yang <chenghaoyang@chromium.org>

drivers/iio/light/cros_ec_light_prox.c
@@ -300,26 +300,15 @@ static int cros_ec_light_push_data(
 		s64 timestamp)
 {
 	struct cros_ec_sensors_core_state *st = iio_priv(indio_dev);
-	unsigned long scan_mask;
 
 	if (!st || !indio_dev->active_scan_mask)
 		return 0;
 
-	scan_mask = *(indio_dev->active_scan_mask);
-	if (scan_mask & ((1 << indio_dev->num_channels) - 2)) {
-		/*
-		 * Only one channel at most is used.
-		 * Use regular push function.
-		 */
-		return cros_ec_sensors_push_data(indio_dev, data, timestamp);
-	}
-
-	if (test_bit(0, indio_dev->active_scan_mask)) {
-		/*
-		 * Save clear channel, will be used when RGB data arrives.
-		 */
+	/* Save clear channel, will be used when RGB data arrives. */
+	if (test_bit(0, indio_dev->active_scan_mask))
 		st->samples[0] = data[0];
-	}
+
+	/* Wait for RGB callback to send samples upstream. */
 	return 0;
 }
 
@@ -330,7 +319,8 @@ static int cros_ec_light_push_data_rgb(
 {
 	struct cros_ec_sensors_core_state *st = iio_priv(indio_dev);
 	s16 *out;
-	unsigned int i;
+	s64 delta;
+	unsigned int i = 1;
 
 	if (!st || !indio_dev->active_scan_mask)
 		return 0;
@@ -339,14 +329,23 @@ static int cros_ec_light_push_data_rgb(
 	 * Send all data needed.
 	 */
 	out = (s16 *)st->samples;
-	for_each_set_bit(i,
+	if (test_bit(0, indio_dev->active_scan_mask))
+		out++;
+
+	for_each_set_bit_from(i,
 			 indio_dev->active_scan_mask,
 			 indio_dev->masklength) {
-		if (i > 0)
-			*out = data[i - 1];
+		*out = data[i - 1];
 		out++;
 	}
-	iio_push_to_buffers_with_timestamp(indio_dev, st->samples, timestamp);
+
+	if (iio_device_get_clock(indio_dev) != CLOCK_BOOTTIME)
+		delta = iio_get_time_ns(indio_dev) - cros_ec_get_time_ns();
+	else
+		delta = 0;
+
+	iio_push_to_buffers_with_timestamp(indio_dev, st->samples,
+					   timestamp + delta);
 	return 0;
 }
 
@@ -420,7 +419,7 @@ static int cros_ec_light_prox_probe(struct platform_device *pdev)
 
 	ret = cros_ec_sensors_core_init(pdev, indio_dev, true,
 					cros_ec_light_capture,
-					cros_ec_light_push_data);
+					cros_ec_sensors_push_data);
 	if (ret)
 		return ret;
 
@@ -485,6 +484,10 @@ static int cros_ec_light_prox_probe(struct platform_device *pdev)
 			channel->channel2 = IIO_MOD_LIGHT_RED + i;
 			channel->type = IIO_LIGHT;
 		}
+		cros_ec_sensorhub_register_push_data(
+				sensor_hub, sensor_num,
+				indio_dev,
+				cros_ec_light_push_data);
 		cros_ec_sensorhub_register_push_data(
 				sensor_hub, sensor_num + 1,
 				indio_dev,

commit a063a76d14909cc77f9a0880e31fd44b4fba721c

CHROMIUM: iio: cros_ec_light: Fix RGB callbacks
Properly register RGB callack
Set sensor frequency to both clear and RGB sensors. The second call
does not use the frequency argument, but tell the EC to set the
oversampling ratio properly, allowing the RGB events to flow to the AP.

BUG=b:172343923
TEST=On volteer, check we get both clear and RGB events.

Cq-Depend: chromium:2570825
Fixes: 098a98758e315 ("CHROMIUM: iio: cros_ec_light: Fix regular and RGB callbacks")
Signed-off-by: Gwendal Grignou <gwendal@chromium.org>
Change-Id: I7523e146e9dfec821dc2f32dffa2402cb9a589b7
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2570048

drivers/iio/light/cros_ec_light_prox.c
@@ -283,6 +283,16 @@ static int cros_ec_light_prox_write(struct iio_dev *indio_dev,
 		if (ret == 0)
 			st->core.range_updated = true;
 		break;
+	case IIO_CHAN_INFO_SAMP_FREQ:
+		ret = cros_ec_sensors_core_write(&st->core, chan, val, val2,
+						 mask);
+		/* Repeat the same command to the RGB sensor. */
+		if (!ret && indio_dev->num_channels >
+			    CROS_EC_LIGHT_PROX_MIN_CHANNELS)
+			ret = cros_ec_light_extra_send_host_cmd(
+					&st->core, 1, 0);
+
+		break;
 	default:
 		ret = cros_ec_sensors_core_write(&st->core, chan, val, val2,
 						 mask);
@@ -484,6 +494,7 @@ static int cros_ec_light_prox_probe(struct platform_device *pdev)
 			channel->channel2 = IIO_MOD_LIGHT_RED + i;
 			channel->type = IIO_LIGHT;
 		}
+		cros_ec_sensorhub_unregister_push_data(sensor_hub, sensor_num);
 		cros_ec_sensorhub_register_push_data(
 				sensor_hub, sensor_num,
 				indio_dev,