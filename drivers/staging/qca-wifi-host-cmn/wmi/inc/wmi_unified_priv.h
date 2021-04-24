f491c2083b669032b7a9555df3b34ba88a527479
Park Ju Hyung <qkrwngud825@gmail.com>
2021-04-24 19:34:25 +0530

    qcacmn/qcacld-3.0: Disable debugging bloat as much as possible
    The default defconfig enables a lot of debugging features.
    Disable those by referencing other perf defconfigs.
    
    [Tashar02: Adapt for k4.19 and fix compilation]
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Co-authored-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/staging/qca-wifi-host-cmn/wmi/inc/wmi_unified_priv.h
+++ drivers/staging/qca-wifi-host-cmn/wmi/inc/wmi_unified_priv.h
@@ -253,6 +253,26 @@ struct wmi_debug_log_info {
 	struct dentry *wmi_log_debugfs_dir;
 };
 
+#else
+
+#define wmi_alert(params...) ((void)0)
+#define wmi_err(params...) ((void)0)
+#define wmi_warn(params...) ((void)0)
+#define wmi_info(params...) ((void)0)
+#define wmi_debug(params...) ((void)0)
+
+#define wmi_nofl_alert(params...) ((void)0)
+#define wmi_nofl_err(params...) ((void)0)
+#define wmi_nofl_warn(params...) ((void)0)
+#define wmi_nofl_info(params...) ((void)0)
+#define wmi_nofl_debug(params...) ((void)0)
+
+#define wmi_alert_rl(params...) ((void)0)
+#define wmi_err_rl(params...) ((void)0)
+#define wmi_warn_rl(params...) ((void)0)
+#define wmi_info_rl(params...) ((void)0)
+#define wmi_debug_rl(params...) ((void)0)
+
 #endif /*WMI_INTERFACE_EVENT_LOGGING */
 
 #ifdef WLAN_OPEN_SOURCE
