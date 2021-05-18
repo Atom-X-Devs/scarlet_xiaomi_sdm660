61d379099dd398ed3cd652b1a7a312a5426a6593
Kees Cook <keescook@chromium.org>
2021-05-17 20:16:57 -0700

    BACKPORT: string.h: Introduce memset_after() for wiping trailing members/padding
    A common idiom in kernel code is to wipe the contents of a structure
    after a given member. This is especially useful in places where there is
    trailing padding. These open-coded cases are usually difficult to read
    and very sensitive to struct layout changes. Introduce a new helper,
    memset_after() that takes the target struct instance, the byte to write,
    and the member name after which the zeroing should start.
    
    Cc: Steffen Klassert <steffen.klassert@secunet.com>
    Cc: Herbert Xu <herbert@gondor.apana.org.au>
    Cc: "David S. Miller" <davem@davemloft.net>
    Cc: Jakub Kicinski <kuba@kernel.org>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Cc: Francis Laniel <laniel_francis@privacyrequired.com>
    Cc: Vincenzo Frascino <vincenzo.frascino@arm.com>
    Cc: Daniel Axtens <dja@axtens.net>
    Cc: netdev@vger.kernel.org
    Signed-off-by: Kees Cook <keescook@chromium.org>
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>
    Change-Id: Ia91b715853e3d2e873658c0f4bdc6ce71fd2f5e8
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>
    [ Tashar02: Just import memset_after() ]
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- include/linux/string.h
+++ include/linux/string.h
@@ -306,4 +306,21 @@ static __always_inline size_t str_has_prefix(const char *str, const char *prefix
 	return strncmp(str, prefix, len) == 0 ? len : 0;
 }
 
+/**
+ * memset_after - Set a value after a struct member to the end of a struct
+ *
+ * @obj: Address of target struct instance
+ * @v: Byte value to repeatedly write
+ * @member: after which struct member to start writing bytes
+ *
+ * This is good for clearing padding following the given member.
+ */
+#define memset_after(obj, v, member)					\
+({									\
+	u8 *__ptr = (u8 *)(obj);					\
+	typeof(v) __val = (v);						\
+	memset(__ptr + offsetofend(typeof(*(obj)), member), __val,	\
+	       sizeof(*(obj)) - offsetofend(typeof(*(obj)), member));	\
+})
+
 #endif /* _LINUX_STRING_H_ */
