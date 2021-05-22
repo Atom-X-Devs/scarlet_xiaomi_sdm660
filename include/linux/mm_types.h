fae2735d79bc0c5f15a11b5d56465505ec8cfed4
Yu Zhao <yuzhao@google.com>
2021-05-22 17:37:05 -0600

    Revert "CHROMIUM: mm: remove kstaled pmd page list walk"
    This reverts commit 2da96489f151f361d3dbb210c918e203ab645321.
    
    BUG=b:123039911
    TEST=Built
    
    Signed-off-by: Yu Zhao <yuzhao@google.com>
    Change-Id: Iaabe61a952a0ab5e8b391edfcb0326472b19c2ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2914029
    Reviewed-by: Sonny Rao <sonnyrao@chromium.org>
    Tested-by: Yu Zhao <yuzhao@chromium.org>
    Commit-Queue: Yu Zhao <yuzhao@chromium.org>

--- include/linux/mm_types.h
+++ include/linux/mm_types.h
@@ -138,10 +138,15 @@ struct page {
 		struct {	/* Page table pages */
 			unsigned long _pt_pad_1;	/* compound_head */
 			pgtable_t pmd_huge_pte; /* protected by page->ptl */
-			unsigned long _pt_pad_2;	/* mapping */
 			union {
-				struct mm_struct *pt_mm; /* x86 pgds only */
-				atomic_t pt_frag_refcount; /* powerpc */
+				struct {
+					unsigned long _pt_pad_2;	/* mapping */
+					union {
+						struct mm_struct *pt_mm; /* x86 pgds only */
+						atomic_t pt_frag_refcount; /* powerpc */
+					};
+				};
+				struct list_head pmdp_list;	/* kstaled pmd page list */
 			};
 #if ALLOC_SPLIT_PTLOCKS
 			spinlock_t *ptl;
