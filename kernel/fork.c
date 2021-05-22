ce89654b0262999699b760125bb17588d593d2e6
Yu Zhao <yuzhao@google.com>
2021-05-22 17:35:21 -0600

    Revert "CHROMIUM: mm: add mm_struct list walk to kstaled"
    This reverts commit 187835806406690fe7e3c9f7609a9c59d1de4059.
    
    BUG=b:123039911
    TEST=Built
    
    Signed-off-by: Yu Zhao <yuzhao@google.com>
    Change-Id: Icf9a14143896dafb59c77375968479aaa876d85f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2914023
    Reviewed-by: Sonny Rao <sonnyrao@chromium.org>
    Tested-by: Yu Zhao <yuzhao@chromium.org>
    Commit-Queue: Yu Zhao <yuzhao@chromium.org>

--- kernel/fork.c
+++ kernel/fork.c
@@ -91,7 +91,6 @@
 #include <linux/kcov.h>
 #include <linux/livepatch.h>
 #include <linux/thread_info.h>
-#include <linux/kstaled.h>
 
 #include <asm/pgtable.h>
 #include <asm/pgalloc.h>
@@ -623,23 +622,7 @@ static void check_mm(struct mm_struct *mm)
 }
 
 #define allocate_mm()	(kmem_cache_alloc(mm_cachep, GFP_KERNEL))
-#ifdef CONFIG_KSTALED
-static void rcu_free_mm(struct rcu_head *rcu)
-{
-	struct mm_struct *mm = container_of(rcu, struct mm_struct, rcu_head);
-
-	kmem_cache_free(mm_cachep, mm);
-}
-
-static void free_mm(struct mm_struct *mm)
-{
-	kstaled_del_mm(mm);
-
-	call_rcu(&mm->rcu_head, rcu_free_mm);
-}
-#else
 #define free_mm(mm)	(kmem_cache_free(mm_cachep, (mm)))
-#endif
 
 /*
  * Called when the last reference to the mm
@@ -999,7 +982,6 @@ static struct mm_struct *mm_init(struct mm_struct *mm, struct task_struct *p,
 		goto fail_nocontext;
 
 	mm->user_ns = get_user_ns(user_ns);
-	kstaled_add_mm(mm);
 	return mm;
 
 fail_nocontext:
