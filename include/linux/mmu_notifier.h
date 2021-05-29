557151a690f6e1041d8e96fdcb37149d52653d05
Yu Zhao <yuzhao@google.com>
2021-05-29 17:06:38 -0600

    CHROMIUM: mm: multigenerational lru: scan kvm mmu pages
    For each kvm instance, we add a list of its mmu pages. And each kvm
    mmu page is reference counted and will not be freed before an rcu
    grace period has expired. This allows us to walk each kvm mmu page
    list under the rcu lock, and we can reschedule as long as we hold a
    reference to any mmu page on the list (and unlock the rcu).
    
    For data pages, we call get_page_unless_zero() and verify that it is
    the right page, using cmpxchg64 to make sure that the kvm pte hasn't
    changed.
    
    BUG=b:123039911
    TEST=Ran crostini.DiskIOPerf and crostini.VimCompile
    
    Signed-off-by: Yu Zhao <yuzhao@google.com>
    Change-Id: Ie047b349964063356fbbc604d0a935508ced5096
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2931334
    Reviewed-by: Yu Zhao <yuzhao@chromium.org>
    Reviewed-by: Sonny Rao <sonnyrao@chromium.org>
    Tested-by: Yu Zhao <yuzhao@chromium.org>
    Commit-Queue: Yu Zhao <yuzhao@chromium.org>

--- include/linux/mmu_notifier.h
+++ include/linux/mmu_notifier.h
@@ -14,6 +14,14 @@ struct mmu_notifier_ops;
 /* mmu_notifier_ops flags */
 #define MMU_INVALIDATE_DOES_NOT_BLOCK	(0x01)
 
+struct mmu_notifier_walk {
+	bool (*start_batch)(struct mm_struct *mm, void *priv);
+	bool (*end_batch)(void *priv, bool last);
+	struct page *(*get_page)(void *priv, unsigned long pfn, bool young);
+	void (*update_page)(void *priv, struct page *page);
+	void *private;
+};
+
 #ifdef CONFIG_MMU_NOTIFIER
 
 /*
@@ -88,6 +96,9 @@ struct mmu_notifier_ops {
 			   struct mm_struct *mm,
 			   unsigned long start,
 			   unsigned long end);
+
+	void (*clear_young_walk)(struct mmu_notifier *mn,
+				 struct mmu_notifier_walk *walk);
 
 	/*
 	 * test_young is called to check the young/accessed bitflag in
@@ -227,6 +238,8 @@ extern int __mmu_notifier_clear_flush_young(struct mm_struct *mm,
 extern int __mmu_notifier_clear_young(struct mm_struct *mm,
 				      unsigned long start,
 				      unsigned long end);
+extern void __mmu_notifier_clear_young_walk(struct mm_struct *mm,
+					    struct mmu_notifier_walk *walk);
 extern int __mmu_notifier_test_young(struct mm_struct *mm,
 				     unsigned long address);
 extern void __mmu_notifier_change_pte(struct mm_struct *mm,
@@ -263,6 +276,13 @@ static inline int mmu_notifier_clear_young(struct mm_struct *mm,
 	if (mm_has_notifiers(mm))
 		return __mmu_notifier_clear_young(mm, start, end);
 	return 0;
+}
+
+static inline void mmu_notifier_clear_young_walk(struct mm_struct *mm,
+						 struct mmu_notifier_walk *walk)
+{
+	if (mm_has_notifiers(mm))
+		__mmu_notifier_clear_young_walk(mm, walk);
 }
 
 static inline int mmu_notifier_test_young(struct mm_struct *mm,
@@ -454,6 +474,11 @@ static inline int mmu_notifier_clear_flush_young(struct mm_struct *mm,
 					  unsigned long end)
 {
 	return 0;
+}
+
+static inline void mmu_notifier_clear_young_walk(struct mm_struct *mm,
+						 struct mmu_notifier_walk *walk)
+{
 }
 
 static inline int mmu_notifier_test_young(struct mm_struct *mm,
