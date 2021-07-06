92fedd0a72c2f37b1c2fd01f75b1534d1d650215
Miguel de Dios <migueldedios@google.com>
2021-07-06 14:27:34 +0900

    drivers: clk: Specify sync probe for mmcc-660
    Bug: 115776306
    Bug: 77146523
    
    Signed-off-by: Miguel de Dios <migueldedios@google.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/clk/qcom/mmcc-sdm660.c
+++ drivers/clk/qcom/mmcc-sdm660.c
@@ -3119,6 +3119,7 @@ static struct platform_driver mmcc_660_driver = {
 	.driver		= {
 		.name	= "mmcc-sdm660",
 		.of_match_table = mmcc_660_match_table,
+		.probe_type = PROBE_FORCE_SYNCHRONOUS,
 	},
 };
 
