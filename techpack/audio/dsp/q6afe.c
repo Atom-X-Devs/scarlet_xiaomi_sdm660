cb82e69d658d0f234d7c508a78d98ab2acbe8d1e
Edwin Moquete <edwinmmoquete@gmail.com>
2021-07-10 10:48:36 -0400

    techpack: audio: Don't send unsupported commands to DSP
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- techpack/audio/dsp/q6afe.c
+++ techpack/audio/dsp/q6afe.c
@@ -5003,7 +5003,9 @@ static int q6afe_send_enc_config(u16 port_id,
 	struct asm_aptx_ad_speech_mode_cfg_t speech_codec_init_param;
 	struct param_hdr_v3 param_hdr;
 	int ret;
+#ifndef CONFIG_ARCH_SDM660
 	uint32_t frame_size_ctl_value_v2;
+#endif
 
 	pr_debug("%s:update DSP for enc format = %d\n", __func__, format);
 
@@ -5081,6 +5083,7 @@ static int q6afe_send_enc_config(u16 port_id,
 		goto exit;
 	}
 
+#ifndef CONFIG_ARCH_SDM660
 	if (format == ASM_MEDIA_FMT_AAC_V2) {
 		uint32_t frame_size_ctl_value = enc_blk_param.enc_blk_config.
 				aac_config.frame_ctl.ctl_value;
@@ -5184,6 +5187,7 @@ static int q6afe_send_enc_config(u16 port_id,
 			goto exit;
 		}
 	}
+#endif
 
 	pr_debug("%s:sending AFE_ENCODER_PARAM_ID_PACKETIZER to DSP\n",
 		 __func__);
@@ -5200,6 +5204,7 @@ static int q6afe_send_enc_config(u16 port_id,
 		goto exit;
 	}
 
+#ifndef CONFIG_ARCH_SDM660
 	if (format != ASM_MEDIA_FMT_APTX_AD_SPEECH) {
 		pr_debug("%s:sending AFE_ENCODER_PARAM_ID_ENABLE_SCRAMBLING mode= %d to DSP payload\n",
 			  __func__, scrambler_mode);
@@ -5216,6 +5221,7 @@ static int q6afe_send_enc_config(u16 port_id,
 			goto exit;
 		}
 	}
+#endif
 
 	if (format == ASM_MEDIA_FMT_APTX) {
 		pr_debug("%s:sending CAPI_V2_PARAM_ID_APTX_ENC_SWITCH_TO_MONO mode= %d to DSP payload\n",
