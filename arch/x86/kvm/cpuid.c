2cd3f94186757236461a048438b7ad3b61e3283b
Suleiman Souhlal <suleiman@google.com>
2021-07-28 17:47:43 +0900

    CHROMIUM: kvm,x86: Support heterogeneous RT VCPU configurations.
    When CONFIG_KVM_HETEROGENEOUS_RT is enabled, it is possible to use a
    RT VCPU without getting hundreds of milliseconds of latency spikes
    when the RT VCPU preempts another VCPU that's holding a lock.
    The guest needs to have preempt_count reporting enabled.
    
    We achieve this by preventing non-RT VCPUs from getting preempted if
    they are in a critical section, which we define as having non-zero
    preempt_count or interrupts disabled. This avoids the priority inversion
    of a RT VCPU preempting another VCPU that is holding a lock that it
    wants.
    
    On a machine with 2 CPU threads, creating a VM with 3 VCPUs and making
    VCPU 2 RT in the host (and preventing most tasks from running on it by
    using "isolcpus"), the max latency shown by
    "/data/local/tmp/cyclictest -l100000 -m -Sp90 -i1000 -q -n" in the guest
    for VCPU2 never exceeds 4ms (and is often < 1ms) in my tests, even when
    the host CPUs are overloaded by running something like
    "for i in `seq 8`; do yes > /dev/null & done".
    Without these changes, the max latency would often get in the hundreds
    of milliseconds.
    
    (Based on https://lore.kernel.org/kvm/20210728073700.120449-1-suleiman@google.com/T/#me73733e2b6ad50330331c5336b421d21ec202504 with some fixes
    addressing comments from upstream and review on
    https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3058781 )
    
    BUG=b:187794729
    TEST=Described above.
    
    Signed-off-by: Suleiman Souhlal <suleiman@google.com>
    Change-Id: I55a1ed70827d9e515754dc66386c456c6471af89
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3063871
    Tested-by: Suleiman Souhlal <suleiman@chromium.org>
    Auto-Submit: Suleiman Souhlal <suleiman@chromium.org>
    Reviewed-by: Sergey Senozhatsky <senozhatsky@chromium.org>
    Commit-Queue: Suleiman Souhlal <suleiman@chromium.org>

--- arch/x86/kvm/cpuid.c
+++ arch/x86/kvm/cpuid.c
@@ -636,6 +636,9 @@ static inline int __do_cpuid_ent(struct kvm_cpuid_entry2 *entry, u32 function,
 			     (1 << KVM_FEATURE_PV_UNHALT) |
 			     (1 << KVM_FEATURE_PV_TLB_FLUSH) |
 			     (1 << KVM_FEATURE_ASYNC_PF_VMEXIT) |
+#ifdef CONFIG_KVM_HETEROGENEOUS_RT
+			     (1 << KVM_FEATURE_PREEMPT_COUNT) |
+#endif
 			     (1 << KVM_FEATURE_PV_SEND_IPI);
 
 		if (sched_info_on())
