a7daba916c6fd34e82f3b87715253529c1228612
Swetha Chikkaboraiah <schikk@codeaurora.org>
2021-08-13 09:40:52 +0530

    Fix for compilation error
    Signed-off-by: Swetha Chikkaboraiah <schikk@codeaurora.org>
    Change-Id: If6b5caf655568b0d4ee12a71927df9476f3b4c55

--- drivers/video/fbdev/msm/mdss_hdmi_tx.c
+++ drivers/video/fbdev/msm/mdss_hdmi_tx.c
@@ -649,7 +649,7 @@ static ssize_t edid_store(struct device *dev,
 	memset(hdmi_ctrl->edid_buf, 0, hdmi_ctrl->edid_buf_size);
 
 	while (edid_size--) {
-		char t[char_to_nib + 1];
+		char t[3]; /* char_to_nib + 1; */
 		int d;
 
 		memcpy(t, buf_t, sizeof(char) * char_to_nib);
