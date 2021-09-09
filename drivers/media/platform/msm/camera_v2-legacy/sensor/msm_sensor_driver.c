011defebfdd95e4cc142961b9aa81827b6a42653
Divyanshu-Modi <divyan.m05@gmail.com>
2021-09-09 21:03:05 +0530

    camera_v2/legacy: Add support for new cam blobs
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>
    Co-authored-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/media/platform/msm/camera_v2-legacy/sensor/msm_sensor_driver.c
+++ drivers/media/platform/msm/camera_v2-legacy/sensor/msm_sensor_driver.c
@@ -1473,6 +1473,9 @@ int32_t msm_sensor_driver_probe(void *setting,
 		slave_info->vendor_id_info = slave_info32->vendor_id_info;
 		slave_info->vcm_id_info = slave_info32->vcm_id_info;
 #endif
+#ifdef CONFIG_XIAOMI_NEW_CAMERA_BLOBS
+		slave_info->lens_id_info = slave_info32->lens_id_info;
+#endif
 
 		slave_info->slave_addr = slave_info32->slave_addr;
 		slave_info->power_setting_array.size =
@@ -1736,6 +1739,9 @@ CSID_TG:
 #ifdef CONFIG_MACH_LONGCHEER
 	s_ctrl->sensordata->vendor_id_info = &(slave_info->vendor_id_info);
 	s_ctrl->sensordata->vcm_id_info = &(slave_info->vcm_id_info);
+#endif
+#ifdef CONFIG_XIAOMI_NEW_CAMERA_BLOBS
+	s_ctrl->sensordata->lens_id_info = &(slave_info->lens_id_info);
 #endif
 
 	/*
