1e397e331b1547da587669ffe206220cad61124a
Dakkshesh <dakkshesh5@gmail.com>
2021-09-30 12:41:30 +0530

    drivers: devfreq_boost: Skip boosting if Profile is set to Battery Mode
    Signed-off-by: Dakkshesh <dakkshesh5@gmail.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/devfreq/devfreq_boost.c
+++ drivers/devfreq/devfreq_boost.c
@@ -10,6 +10,7 @@
 #include <linux/input.h>
 #include <linux/kthread.h>
 #include <uapi/linux/sched/types.h>
+#include <linux/kprofiles.h>
 
 enum {
 	SCREEN_OFF,
@@ -55,7 +56,7 @@ static struct df_boost_drv df_boost_drv_g __read_mostly = {
 
 static void __devfreq_boost_kick(struct boost_dev *b)
 {
-	if (!READ_ONCE(b->df) || test_bit(SCREEN_OFF, &b->state))
+	if (!READ_ONCE(b->df) || test_bit(SCREEN_OFF, &b->state) || active_mode() == 1)
 		return;
 
 	set_bit(INPUT_BOOST, &b->state);
@@ -77,7 +78,7 @@ static void __devfreq_boost_kick_max(struct boost_dev *b,
 	unsigned long boost_jiffies = msecs_to_jiffies(duration_ms);
 	unsigned long curr_expires, new_expires;
 
-	if (!READ_ONCE(b->df) || test_bit(SCREEN_OFF, &b->state))
+	if (!READ_ONCE(b->df) || test_bit(SCREEN_OFF, &b->state) || active_mode() == 1)
 		return;
 
 	do {
