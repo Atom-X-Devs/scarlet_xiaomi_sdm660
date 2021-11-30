53a17798d37ebf5de4d8c8465bdf440dc42930bc
Archie Pusaka <apusaka@chromium.org>
2021-11-30 10:45:04 +0800

    Revert "FROMLIST: Bluetooth: Attempt to clear HCI_LE_ADV on adv set terminated error event"
    This reverts commit b70cacfe084990b826ff8fc9a7c6f379b4837494.
    
    Reason for revert: Original CL makes DUT not wakeable by peer.
    
    Original change's description:
    > FROMLIST: Bluetooth: Attempt to clear HCI_LE_ADV on adv set terminated error event
    >
    > We should clear the flag if the adv instance removed due to receiving
    > this error status is the last one we have.
    >
    > Signed-off-by: Archie Pusaka <apusaka@chromium.org>
    > Reviewed-by: Miao-chen Chou <mcchou@chromium.org>
    > (am from https://patchwork.kernel.org/patch/12614183/)
    > (also found at https://lore.kernel.org/r/20211111132045.v3.2.I4e34d9e5fdd7515aa15d0ee2ef94d57dcb48a927@changeid)
    >
    > BUG=b:196523329, b:196521730
    > TEST=AdvHealth pass on ezkinil and dedede
    >
    > Signed-off-by: Alain Michaud <alainm@chromium.org>
    > Change-Id: I4e34d9e5fdd7515aa15d0ee2ef94d57dcb48a927
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3298058
    > Reviewed-by: Sean Paul <seanpaul@chromium.org>
    > Reviewed-by: Michael Sun <michaelfsun@google.com>
    > Commit-Queue: Michael Sun <michaelfsun@google.com>
    
    BUG=b:196523329, b:196521730, b:208181614
    TEST=Use BT mouse to wake DUT
    
    Signed-off-by: Archie Pusaka <apusaka@chromium.org>
    Change-Id: I8045fa384908a8334cd09649e9e3071735d270cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3312699
    Reviewed-by: Yun-Hao Chung <howardchung@chromium.org>

--- net/bluetooth/hci_event.c
+++ net/bluetooth/hci_event.c
@@ -5330,7 +5330,7 @@ static void hci_le_ext_adv_term_evt(struct hci_dev *hdev, struct sk_buff *skb)
 {
 	struct hci_evt_le_ext_adv_set_term *ev = (void *) skb->data;
 	struct hci_conn *conn;
-	struct adv_info *adv, *n;
+	struct adv_info *adv;
 
 	BT_DBG("%s status 0x%2.2x", hdev->name, ev->status);
 
@@ -5356,13 +5356,6 @@ static void hci_le_ext_adv_term_evt(struct hci_dev *hdev, struct sk_buff *skb)
 		hci_remove_adv_instance(hdev, ev->handle);
 		mgmt_advertising_removed(NULL, hdev, ev->handle);
 
-		list_for_each_entry_safe(adv, n, &hdev->adv_instances, list) {
-			if (adv->enabled)
-				return;
-		}
-
-		/* We are no longer advertising, clear HCI_LE_ADV */
-		hci_dev_clear_flag(hdev, HCI_LE_ADV);
 		return;
 	}
 
