6a0125aca0b34a25666b69e861ec62d69793451e
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2021-12-30 04:50:03 +0600

    Revert "sched/isolation: Isolate workqueues when "nohz_full=" is set"
    We won't be using CONFIG_NO_HZ_FULL.
    
    This reverts commit 1bda3f8087fce9063da0b8aef87f17a3fe541aca.
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/sched/isolation.c
+++ kernel/sched/isolation.c
@@ -3,7 +3,6 @@
  *  any CPU: unbound workqueues, timers, kthreads and any offloadable work.
  *
  * Copyright (C) 2017 Red Hat, Inc., Frederic Weisbecker
- * Copyright (C) 2017-2018 SUSE, Frederic Weisbecker
  *
  */
 #include "sched.h"
@@ -117,7 +116,7 @@ static int __init housekeeping_nohz_full_setup(char *str)
 {
 	unsigned int flags;
 
-	flags = HK_FLAG_TICK | HK_FLAG_WQ | HK_FLAG_TIMER | HK_FLAG_RCU | HK_FLAG_MISC;
+	flags = HK_FLAG_TICK | HK_FLAG_TIMER | HK_FLAG_RCU | HK_FLAG_MISC;
 
 	return housekeeping_setup(str, flags);
 }
