dbf805340027d69863cfc7f34d12f01c951c7489
Łukasz Spintzyk <lukasz.spintzyk@synaptics.com>
2022-01-03 09:14:10 +0100

    CHROMIUM: drm/evdi: Fix bugcheck error on duplicate sysfs file create
    BUG=None
    TEST=sarien chromebook
    
    Change-Id: I2346f0a7f9ba4a231282f4f64a1d855f2033b848
    Signed-off-by: Łukasz Spintzyk <lukasz.spintzyk@synaptics.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3450243
    Reviewed-by: Dominik Behr <dbehr@chromium.org>

--- drivers/gpu/drm/evdi/evdi_drv.h
+++ drivers/gpu/drm/evdi/evdi_drv.h
@@ -155,9 +155,6 @@ struct drm_clip_rect evdi_framebuffer_sanitize_rect(
 			const struct evdi_framebuffer *fb,
 			const struct drm_clip_rect *rect);
 
-int evdi_driver_setup_early(struct drm_device *dev);
-void evdi_driver_setup_late(struct drm_device *dev);
-
 void evdi_painter_send_cursor_set(struct evdi_painter *painter,
 				  struct evdi_cursor *cursor);
 void evdi_painter_send_cursor_move(struct evdi_painter *painter,
@@ -170,4 +167,3 @@ bool evdi_painter_i2c_data_notify(struct evdi_device *evdi,
 
 int evdi_fb_get_bpp(uint32_t format);
 #endif
-
