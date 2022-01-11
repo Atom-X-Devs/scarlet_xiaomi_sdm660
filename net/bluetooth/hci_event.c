ec94965c78269085131e450051051e6c58d66a84
Dan Carpenter <dan.carpenter@oracle.com>
2022-01-11 10:23:19 +0300

    UPSTREAM: Bluetooth: hci_sync: unlock on error in hci_inquiry_result_with_rssi_evt()
    Add unlocks to two error paths in hci_inquiry_result_with_rssi_evt().
    
    Fixes: fee645033e2c ("Bluetooth: hci_event: Use skb_pull_data when processing inquiry results")
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
    (cherry picked from commit c07ba878ca199a6089cdb323bf526adbeeb4201f)
    
    BUG=none
    TEST=build
    
    Signed-off-by: Zhengping Jiang <jiangzp@google.com>

--- net/bluetooth/hci_event.c
+++ net/bluetooth/hci_event.c
@@ -4550,7 +4550,7 @@ static void hci_inquiry_result_with_rssi_evt(struct hci_dev *hdev, void *edata,
 			if (!info) {
 				bt_dev_err(hdev, "Malformed HCI Event: 0x%2.2x",
 					   HCI_EV_INQUIRY_RESULT_WITH_RSSI);
-				return;
+				goto unlock;
 			}
 
 			bacpy(&data.bdaddr, &info->bdaddr);
@@ -4580,7 +4580,7 @@ static void hci_inquiry_result_with_rssi_evt(struct hci_dev *hdev, void *edata,
 			if (!info) {
 				bt_dev_err(hdev, "Malformed HCI Event: 0x%2.2x",
 					   HCI_EV_INQUIRY_RESULT_WITH_RSSI);
-				return;
+				goto unlock;
 			}
 
 			bacpy(&data.bdaddr, &info->bdaddr);
@@ -4602,7 +4602,7 @@ static void hci_inquiry_result_with_rssi_evt(struct hci_dev *hdev, void *edata,
 		bt_dev_err(hdev, "Malformed HCI Event: 0x%2.2x",
 			   HCI_EV_INQUIRY_RESULT_WITH_RSSI);
 	}
-
+unlock:
 	hci_dev_unlock(hdev);
 }
 
