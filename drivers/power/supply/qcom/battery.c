739b38020068ea59d1c2505ed4f4da5b5a052df7
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2022-01-23 07:13:33 +0600

    power: supply: battery: Don't create debugfs when !CONFIG_DEBUG_FS
    Additionally, compile out unused codes while debug_fs is disabled.
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/power/supply/qcom/battery.c
+++ drivers/power/supply/qcom/battery.c
@@ -109,7 +109,9 @@ struct pl_data {
 	int			main_fcc_max;
 	enum power_supply_type	charger_type;
 	/* debugfs directory */
+#ifdef CONFIG_DEBUG_FS
 	struct dentry		*dfs_root;
+#endif
 	u32			float_voltage_uv;
 };
 
@@ -124,7 +126,9 @@ enum {
 	FORCE_INOV_DISABLE_BIT	= BIT(1),
 };
 
+#ifdef CONFIG_DEBUG_FS
 static int debug_mask;
+#endif
 
 #ifdef DEBUG
 #define pl_dbg(chip, reason, fmt, ...)			\
@@ -2042,6 +2046,7 @@ static void pl_config_init(struct pl_data *chip, int smb_version)
 	}
 }
 
+#ifdef CONFIG_DEBUG_FS
 static void qcom_batt_create_debugfs(struct pl_data *chip)
 {
 	struct dentry *entry;
@@ -2059,6 +2064,7 @@ static void qcom_batt_create_debugfs(struct pl_data *chip)
 		pr_err("Couldn't create force_dc_psy_update file rc=%ld\n",
 			(long)entry);
 }
+#endif
 
 #define DEFAULT_RESTRICTED_CURRENT_UA	1000000
 int qcom_batt_init(struct charger_param *chg_param)
@@ -2081,7 +2087,9 @@ int qcom_batt_init(struct charger_param *chg_param)
 	if (!chip)
 		return -ENOMEM;
 
+#ifdef CONFIG_DEBUG_FS
 	qcom_batt_create_debugfs(chip);
+#endif
 
 	chip->slave_pct = 50;
 	chip->chg_param = chg_param;
