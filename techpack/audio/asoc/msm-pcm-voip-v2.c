d211313707a7b97dd44cc4e3cbaed54ba7ef8a17
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2022-01-26 20:40:07 +0600

    techpack: msm-pcm-voip-v2: Demote DL data unavailability log to debug
    Spams the dmesg with the following logs:
    
    [25230.066665] DL data not available
    [25230.086652] DL data not available
    [25230.106634] DL data not available
    [25230.126641] DL data not available
    [25230.146694] DL data not available
    [25230.166636] DL data not available
    [25230.186734] DL data not available
    [25230.206620] DL data not available
    [25230.226662] DL data not available
    [25230.246614] DL data not available
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- techpack/audio/asoc/msm-pcm-voip-v2.c
+++ techpack/audio/asoc/msm-pcm-voip-v2.c
@@ -664,7 +664,7 @@ static void voip_process_dl_pkt(uint8_t *voc_pkt, void *private_data)
 	} else {
 		*((uint32_t *)voc_pkt) = 0;
 		spin_unlock_irqrestore(&prtd->dsp_lock, dsp_flags);
-		pr_err_ratelimited("DL data not available\n");
+		pr_debug("DL data not available\n");
 	}
 	wake_up(&prtd->in_wait);
 }
