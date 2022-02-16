886adba910057dd0e50a3800497b80d58ffaa482
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2022-02-17 02:07:58 +0600

    power: qpnp-fg-gen3: Hardcode learned_cc_uah for wayne
    As wayne (and jasmine_sprout) has a 3010 mAh battery, use 3000000
    at the ternary operator. The expression will evaluate to learned_cc_uah
    being greater than 3000000, otherwise 3000000 will be evaluated.
    
    Inspired from: https://github.com/Atom-X-Devs/android_kernel_xiaomi_scarlet/commit/9079cadeb9d3bf95e7658fdedad7b124032f4338
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/power/supply/qcom/qpnp-fg-gen3.c
+++ drivers/power/supply/qcom/qpnp-fg-gen3.c
@@ -1127,6 +1127,9 @@ static int fg_load_learned_cap_from_sram(struct fg_dev *fg)
 
 	chip->cl.learned_cc_uah = act_cap_mah * 1000;
 
+#ifdef CONFIG_MACH_XIAOMI_WAYNE
+	chip->cl.learned_cc_uah = (chip->cl.learned_cc_uah > 3000000) ? chip->cl.learned_cc_uah : 3000000;
+#endif
 	if (chip->cl.learned_cc_uah != chip->cl.nom_cap_uah) {
 		if (chip->cl.learned_cc_uah == 0)
 			chip->cl.learned_cc_uah = chip->cl.nom_cap_uah;
