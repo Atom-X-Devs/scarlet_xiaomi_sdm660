d563e1de58660eb4e2421cf2f0077a77f0f74b2b
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2022-02-27 16:06:07 +0600

    qseecom/shmbridge: Disable shared memory bridge for SDM660
    Disable shared memory bridge mechanism as it's not supported by
    our SoC.
    
    [    0.005700] shmbridge is not supported by this target
    
    Inspired from: https://github.com/sonyxperiadev/kernel/commit/3c4573059f8a58f0a8f9fe60732b612ba3c8974e
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/misc/qseecom.c
+++ drivers/misc/qseecom.c
@@ -9493,6 +9493,10 @@ static int qseecom_register_heap_shmbridge(uint32_t heapid, uint64_t *handle)
 	uint32_t ns_vmids[] = {VMID_HLOS};
 	uint32_t ns_vm_perms[] = {PERM_READ | PERM_WRITE};
 
+#ifdef CONFIG_ARCH_SDM660
+	goto out;
+#endif
+
 	ion_node = of_find_compatible_node(NULL, NULL, "qcom,msm-ion");
 	if (!ion_node) {
 		pr_err("Failed to get qcom,msm-ion node\n");
@@ -9520,6 +9524,9 @@ static int qseecom_register_heap_shmbridge(uint32_t heapid, uint64_t *handle)
 	}
 
 	pr_warn("Could not get heap %d info: No shmbridge created\n", heapid);
+#ifdef CONFIG_ARCH_SDM660
+out:
+#endif
 	return 0;
 }
 
