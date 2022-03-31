90396dcc90ab5534bff7be1f80e47fdbdbd7cad0
Ilan Peer <ilan.peer@intel.com>
2022-03-31 15:16:52 +0300

    CHROMIUM: iwl7000: Fix HE PHY capabilities setting
    The HE PHY capability channel width support for 40MHz in 2GHz
    was set for all bands, which is wrong.
    Fix the code so it would be set only for 2GHz band.
    
    Change-Id: Ic24394ecc7abaa097c60a7c1e567f9d12be6f04c
    Signed-off-by: Ilan Peer <ilan.peer@intel.com>
    iwl7000-tree: 1fab38ba01163415a35f0b4eeaf573f429bb35d7

--- drivers/net/wireless/iwl7000/iwlwifi/iwl-nvm-parse.c
+++ drivers/net/wireless/iwl7000/iwlwifi/iwl-nvm-parse.c
@@ -724,7 +724,6 @@ static const struct ieee80211_sband_iftype_data iwl_he_eht_capa[] = {
 				.mac_cap_info[3] =
 					IEEE80211_HE_MAC_CAP3_OMI_CONTROL,
 				.phy_cap_info[0] =
-					IEEE80211_HE_PHY_CAP0_CHANNEL_WIDTH_SET_40MHZ_IN_2G |
 					IEEE80211_HE_PHY_CAP0_CHANNEL_WIDTH_SET_40MHZ_80MHZ_IN_5G,
 				.phy_cap_info[1] =
 					IEEE80211_HE_PHY_CAP1_LDPC_CODING_IN_PAYLOAD,
