57476eeaa7212168d740eee38f0892f5da149597
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2022-05-27 23:26:28 +0600

    camera_v2-*: Silence logging debug info upon opening camera
    We are not gonna do anything with that information
    after opening camera. This is annoying and hurts
    readability in dmesg. Silence the debug logs with
    camera driver's own debug log silencing wrapper.
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/media/platform/msm/camera_v2-oss/sensor/csid/msm_csid.c
+++ drivers/media/platform/msm/camera_v2-oss/sensor/csid/msm_csid.c
@@ -317,7 +317,7 @@ static int msm_csid_reset(struct csid_device *csid_dev)
 		irq = msm_camera_vio_r(csid_dev->base,
 			csid_dev->ctrl_reg->csid_reg.csid_irq_status_addr,
 			csid_dev->pdev->id);
-		pr_err_ratelimited("%s CSID%d_IRQ_STATUS_ADDR = 0x%x\n",
+		CDBG("%s CSID%d_IRQ_STATUS_ADDR = 0x%x\n",
 			__func__, csid_dev->pdev->id, irq);
 		if (irq & (0x1 << irq_bitshift)) {
 			rc = 1;
@@ -662,7 +662,7 @@ static irqreturn_t msm_csid_irq(int irq_num, void *data)
 
 	irq = msm_camera_io_r(csid_dev->base +
 		csid_dev->ctrl_reg->csid_reg.csid_irq_status_addr);
-	pr_err_ratelimited("%s CSID%d_IRQ_STATUS_ADDR = 0x%x\n",
+	CDBG("%s CSID%d_IRQ_STATUS_ADDR = 0x%x\n",
 		 __func__, csid_dev->pdev->id, irq);
 	if (irq & (0x1 <<
 		csid_dev->ctrl_reg->csid_reg.csid_rst_done_irq_bitshift))
@@ -711,7 +711,7 @@ static int msm_csid_init(struct csid_device *csid_dev, uint32_t *csid_version)
 		return rc;
 	}
 
-	pr_info("%s: CSID_VERSION = 0x%x\n", __func__,
+	CDBG("%s: CSID_VERSION = 0x%x\n", __func__,
 		csid_dev->ctrl_reg->csid_reg.csid_version);
 	/* power up */
 	rc = msm_camera_config_vreg(&csid_dev->pdev->dev, csid_dev->csid_vreg,
