f5f28cc7c7789b759367bf0812794f7ec23914b7
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2022-05-27 23:26:28 +0600

    camera_v2-*: Silence logging debug info upon opening camera
    We are not gonna do anything with that information
    after opening camera. This is annoying and hurts
    readability in dmesg. Silence the debug logs with
    camera driver's own debug log silencing wrapper.
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/media/platform/msm/camera_v2-oss/ispif/msm_ispif.c
+++ drivers/media/platform/msm/camera_v2-oss/ispif/msm_ispif.c
@@ -1444,7 +1444,7 @@ static void ispif_process_irq(struct ispif_device *ispif,
 	if (out[vfe_id].ispifIrqStatus0 &
 			ISPIF_IRQ_STATUS_PIX_SOF_MASK) {
 		if (ispif->ispif_sof_debug < ISPIF_SOF_DEBUG_COUNT)
-			pr_err("%s: PIX0 frame id: %u\n", __func__,
+			CDBG("%s: PIX0 frame id: %u\n", __func__,
 				ispif->sof_count[vfe_id].sof_cnt[PIX0]);
 		ispif->sof_count[vfe_id].sof_cnt[PIX0]++;
 		ispif->ispif_sof_debug++;
@@ -1452,7 +1452,7 @@ static void ispif_process_irq(struct ispif_device *ispif,
 	if (out[vfe_id].ispifIrqStatus1 &
 			ISPIF_IRQ_STATUS_PIX_SOF_MASK) {
 		if (ispif->ispif_sof_debug < ISPIF_SOF_DEBUG_COUNT*2)
-			pr_err("%s: PIX1 frame id: %u\n", __func__,
+			CDBG("%s: PIX1 frame id: %u\n", __func__,
 				ispif->sof_count[vfe_id].sof_cnt[PIX1]);
 		ispif->sof_count[vfe_id].sof_cnt[PIX1]++;
 		ispif->ispif_sof_debug++;
@@ -1460,7 +1460,7 @@ static void ispif_process_irq(struct ispif_device *ispif,
 	if (out[vfe_id].ispifIrqStatus0 &
 			ISPIF_IRQ_STATUS_RDI0_SOF_MASK) {
 		if (ispif->ispif_rdi0_debug < ISPIF_SOF_DEBUG_COUNT)
-			pr_err("%s: RDI0 frame id: %u\n", __func__,
+			CDBG("%s: RDI0 frame id: %u\n", __func__,
 				ispif->sof_count[vfe_id].sof_cnt[RDI0]);
 		ispif->sof_count[vfe_id].sof_cnt[RDI0]++;
 		ispif->ispif_rdi0_debug++;
@@ -1468,7 +1468,7 @@ static void ispif_process_irq(struct ispif_device *ispif,
 	if (out[vfe_id].ispifIrqStatus1 &
 			ISPIF_IRQ_STATUS_RDI1_SOF_MASK) {
 		if (ispif->ispif_rdi1_debug < ISPIF_SOF_DEBUG_COUNT)
-			pr_err("%s: RDI1 frame id: %u\n", __func__,
+			CDBG("%s: RDI1 frame id: %u\n", __func__,
 				ispif->sof_count[vfe_id].sof_cnt[RDI1]);
 		ispif->sof_count[vfe_id].sof_cnt[RDI1]++;
 		ispif->ispif_rdi1_debug++;
@@ -1476,7 +1476,7 @@ static void ispif_process_irq(struct ispif_device *ispif,
 	if (out[vfe_id].ispifIrqStatus2 &
 			ISPIF_IRQ_STATUS_RDI2_SOF_MASK) {
 		if (ispif->ispif_rdi2_debug < ISPIF_SOF_DEBUG_COUNT)
-			pr_err("%s: RDI2 frame id: %u\n", __func__,
+			CDBG("%s: RDI2 frame id: %u\n", __func__,
 				ispif->sof_count[vfe_id].sof_cnt[RDI2]);
 		ispif->sof_count[vfe_id].sof_cnt[RDI2]++;
 		ispif->ispif_rdi2_debug++;
