c85b8d46d716444dab56cd4affca4529c2d2cc70
Johannes Berg <johannes.berg@intel.com>
2022-06-24 16:28:33 +0200

    CHROMIUM: iwl7000: mac80211: mlme: do IEEE80211_STA_RESET_SIGNAL_AVE per link
    Remove the IEEE80211_STA_RESET_SIGNAL_AVE flag and use
    a bool instead, but invert the polarity (now calling it
    tracking_signal_avg) so we don't have to initialize it,
    and put that into the link instead.
    
    Change-Id: I2398433851572c95290b613422cc08d5b5cc9625
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    tested: iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    iwl7000-tree: 57a2feefb49768aedbe46251e04bde1f82861eac

--- drivers/net/wireless/iwl7000/mac80211/ieee80211_i.h
+++ drivers/net/wireless/iwl7000/mac80211/ieee80211_i.h
@@ -358,7 +358,6 @@ enum ieee80211_sta_flags {
 	IEEE80211_STA_MFP_ENABLED	= BIT(6),
 	IEEE80211_STA_UAPSD_ENABLED	= BIT(7),
 	IEEE80211_STA_NULLFUNC_ACKED	= BIT(8),
-	IEEE80211_STA_RESET_SIGNAL_AVE	= BIT(9),
 	IEEE80211_STA_DISABLE_WMM	= BIT(14),
 	IEEE80211_STA_ENABLE_RRM	= BIT(15),
 };
@@ -875,6 +874,7 @@ struct ieee80211_link_data_managed {
 	s16 p2p_noa_index;
 
 	bool have_beacon;
+	bool tracking_signal_avg;
 
 	bool csa_waiting_bcn;
 	bool csa_ignored_same_chan;
