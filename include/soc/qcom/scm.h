13ef3e6facd8d9e02b6f85d85d4a8f0f4e86646c
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2022-06-29 13:31:23 +0600

    Revert "soc: qcom: Add support for extra arguments from shmbridge"
    Shmbridge has been disabled as of commit [1]. Therefore, revert this.
    
    [1]: https://github.com/Atom-X-Devs/android_kernel_xiaomi_scarlet/commit/e48c7758066d90039b92bc046209c4cf46ac8280
    
    This reverts commit ea75fbdf36fab1b8486685ea9c512b354a125f07.
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- include/soc/qcom/scm.h
+++ include/soc/qcom/scm.h
@@ -6,8 +6,6 @@
 #ifndef __MACH_SCM_H
 #define __MACH_SCM_H
 
-#include <soc/qcom/qtee_shmbridge.h>
-
 #define SCM_SVC_BOOT			0x1
 #define SCM_SVC_PIL			0x2
 #define SCM_SVC_UTIL			0x3
@@ -88,7 +86,7 @@ struct scm_desc {
 	u64 ret[MAX_SCM_RETS];
 
 	/* private */
-	struct qtee_shm shm;
+	void *extra_arg_buf;
 	u64 x5;
 };
 
