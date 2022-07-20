0c320cb06443e50283dd823438e035362deae48f
Aashish Sharma <shraash@google.com>
2022-07-20 11:12:25 +0530

    BACKPORT: FROMGIT: HID: nintendo: Fix unused-const-variable compiler warning
    The following compiler warning is produced when CONFIG_NINTENDO_FF
    is not set:
    
    >> drivers/hid/hid-nintendo.c:405:29: warning: 'JC_RUMBLE_ZERO_AMP_PKT_CNT'
       defined but not used [-Wunused-const-variable=]
    
    To fix this, move the variable 'JC_RUMBLE_ZERO_AMP_PKT_CNT' under the
    CONFIG_NINTENDO_FF's conditional compilation block.
    
    Signed-off-by: Aashish Sharma <shraash@google.com>
    Reviewed-by: Guenter Roeck <groeck@chromium.org>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>
    
    (cherry picked from commit 669abca7b767f0daa34f6b965a415f7a9045703e
     https://git.kernel.org/pub/scm/linux/kernel/git/hid/hid.git for-5.20/nintendo)
    
    Conflicts:
       drivers/hid/hid-nintendo.c
    
    BUG=b:227610040
    TEST=CQ
    
    Change-Id: I344bbdc42818550c2a23fd7c6f5867c2d2d788bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3779928
    Commit-Queue: Guenter Roeck <groeck@chromium.org>
    Commit-Queue: Aashish Sharma <shraash@google.com>
    Reviewed-by: Guenter Roeck <groeck@chromium.org>
    Auto-Submit: Aashish Sharma <shraash@google.com>
    Tested-by: Aashish Sharma <shraash@google.com>

--- drivers/hid/hid-nintendo.c
+++ drivers/hid/hid-nintendo.c
@@ -322,8 +322,6 @@ struct joycon_input_report {
 #define JC_RUMBLE_DATA_SIZE	8
 #define JC_RUMBLE_QUEUE_SIZE	8
 
-static const unsigned short JC_RUMBLE_ZERO_AMP_PKT_CNT = 5;
-
 /* Each physical controller is associated with a joycon_ctlr struct */
 struct joycon_ctlr {
 	struct hid_device *hdev;
@@ -990,6 +988,8 @@ static void joycon_clamp_rumble_freqs(struct joycon_ctlr *ctlr)
 				     JOYCON_MAX_RUMBLE_HIGH_FREQ);
 	spin_unlock_irqrestore(&ctlr->lock, flags);
 }
+
+static const unsigned short JC_RUMBLE_ZERO_AMP_PKT_CNT = 5;
 
 static int joycon_set_rumble(struct joycon_ctlr *ctlr, u16 amp_r, u16 amp_l,
 			     bool schedule_now)
