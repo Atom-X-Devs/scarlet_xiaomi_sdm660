187364686b5c5502ac7874f1aa82cd3996e951ab
Daniil Lunev <dlunev@google.com>
2022-08-04 08:39:51 +1000

    Revert "BACKPORT: FROMLIST: FUSE: Retire superblock on force unmount"
    To replace with FROMGIT
    
    This reverts commit 5e36fafc662ce9df90bf43c7510476989373797e.
    
    BUG=b:226570055
    TEST=CQ
    
    Change-Id: I3a78ce0d6ef790bf521c2f96fb11b828fb512355
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3807228
    Tested-by: Daniil Lunev <dlunev@chromium.org>
    Reviewed-by: Sarthak Kukreti <sarthakkukreti@chromium.org>
    Commit-Queue: Daniil Lunev <dlunev@chromium.org>

--- fs/fuse/inode.c
+++ fs/fuse/inode.c
@@ -377,10 +377,7 @@ void fuse_unlock_inode(struct inode *inode, bool locked)
 
 static void fuse_umount_begin(struct super_block *sb)
 {
-	struct fuse_conn *fc = get_fuse_conn_super(sb);
-
-	fuse_abort_conn(fc, false);
-	retire_super(sb);
+	fuse_abort_conn(get_fuse_conn_super(sb), false);
 }
 
 static void fuse_send_destroy(struct fuse_conn *fc)
