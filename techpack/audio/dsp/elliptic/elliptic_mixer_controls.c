8772d288b0c3712394cb8b336ebc01624883ed2f
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2022-08-15 17:03:40 +0600

    techpack: audio: elliptic: Fix conditional variable check
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- techpack/audio/dsp/elliptic/elliptic_mixer_controls.c
+++ techpack/audio/dsp/elliptic/elliptic_mixer_controls.c
@@ -1325,7 +1325,7 @@ unsigned int elliptic_add_component_controls(void *component)
 	const unsigned int num_controls =
 		ARRAY_SIZE(ultrasound_filter_mixer_controls);
 
-	if (platform != NULL) {
+	if (component != NULL) {
 		snd_soc_add_component_controls(
 			(struct snd_soc_component *)component,
 			ultrasound_filter_mixer_controls, num_controls);
