9b658945abf5274e731a90d147675cc957c14f60
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2022-08-15 13:10:56 +0600

    Revert "sched: Increase the time a task is considered cache-hot"
    This reverts commit 081af3bbf616011792822d9794c372d1a5182a22.
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -94,7 +94,7 @@ unsigned int sysctl_sched_child_runs_first __read_mostly;
 unsigned int sysctl_sched_wakeup_granularity			= 1000000UL;
 static unsigned int normalized_sysctl_sched_wakeup_granularity	= 1000000UL;
 
-const_debug unsigned int sysctl_sched_migration_cost	= 1000000UL;
+const_debug unsigned int sysctl_sched_migration_cost	= 500000UL;
 
 #ifdef CONFIG_SMP
 /*
