4d0669ac373f216b0b0d0e800f3d0fd35ff6444e
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2022-08-24 00:58:09 +0600

    fs/pstore: ram: Correct the ramoops memory address
    Use the resevered memory address from console ramoops that is
    available at the dts which is 0xA0000000.
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- fs/pstore/ram.c
+++ fs/pstore/ram.c
@@ -1013,7 +1013,7 @@ static int __init ramoops_memreserve(char *p)
 
 	size = memparse(p, &p) & PAGE_MASK;
 	ramoops_data.mem_size = size;
-	ramoops_data.mem_address = 0xB0000000;
+	ramoops_data.mem_address = 0xA0000000;
 	ramoops_data.console_size = size / 2;
 	ramoops_data.pmsg_size = size / 2;
 	ramoops_data.max_reason = KMSG_DUMP_OOPS;
