4dc859a09f4d09e689303f136890c82105766287
Guenter Roeck <linux@roeck-us.net>
2022-10-17 06:09:03 -0700

    BACKPORT: FROMLIST: thermal/core: Delete device under thermal device zone lock
    Thermal device attributes may still be opened after unregistering
    the thermal zone and deleting the thermal device.
    
    Currently there is no protection against accessing thermal device
    operations after unregistering a thermal zone. To enable adding
    such protection, protect the device delete operation with the
    thermal zone device mutex. This requires splitting the call to
    device_unregister() into its components, device_del() and put_device().
    Only the first call can be executed under mutex protection, since
    put_device() may result in releasing the thermal zone device memory.
    
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>
    (am from https://patchwork.kernel.org/patch/13008850/)
    (also found at https://lore.kernel.org/r/20221017130910.2307118-3-linux@roeck-us.net)
    
    UPSTREAM-TASK=b:257097168
    BUG=b:221250880
    TEST=CQ including Intel wireless tests
    
    Conflicts:
    	drivers/thermal/thermal_core.c (context)
    
    Change-Id: Ib575c6e96ec2be9fd81e4a6dd7f0ffff05ee2aad
    Disallow-Recycled-Builds: test-failures
    Signed-off-by: Guenter Roeck <groeck@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/4000590
    Reviewed-by: Alexandru Stan <amstan@chromium.org>

--- drivers/thermal/thermal_core.c
+++ drivers/thermal/thermal_core.c
@@ -1396,7 +1396,12 @@ void thermal_zone_device_unregister(struct thermal_zone_device *tz)
 	thermal_remove_hwmon_sysfs(tz);
 	ida_simple_remove(&thermal_tz_ida, tz->id);
 	ida_destroy(&tz->ida);
-	device_unregister(&tz->device);
+
+	mutex_lock(&tz->lock);
+	device_del(&tz->device);
+	mutex_unlock(&tz->lock);
+
+	put_device(&tz->device);
 }
 EXPORT_SYMBOL_GPL(thermal_zone_device_unregister);
 
