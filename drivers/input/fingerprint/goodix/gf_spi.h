eeb61b12cc8ef4ccdbc42f49d829ce8e81f4f91e
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2022-11-02 13:33:04 +0600

    fingerprint: goodix: Clean up the coding style
    - Remove unused variables.
    - Remove unnecessary braces.
    - Use "ifdef" instead of "if defined" for cosmetic change.
    - Fix macro guard's parentheses.
    - Remove GF_NETLINK_ENABLE macro usage.
    - Return 0 straightforwardly instead of using rc.
    - Reformat using clang-format.
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/input/fingerprint/goodix/gf_spi.h
+++ drivers/input/fingerprint/goodix/gf_spi.h
@@ -9,7 +9,7 @@
 #include <linux/types.h>
 #include <linux/notifier.h>
 
-enum FP_MODE{
+enum FP_MODE {
 	GF_IMAGE_MODE = 0,
 	GF_KEY_MODE,
 	GF_SLEEP_MODE,
@@ -19,7 +19,7 @@ enum FP_MODE{
 
 #define SUPPORT_NAV_EVENT
 
-#if defined(SUPPORT_NAV_EVENT)
+#ifdef SUPPORT_NAV_EVENT
 #define GF_NAV_INPUT_UP			KEY_UP
 #define GF_NAV_INPUT_DOWN		KEY_DOWN
 #define GF_NAV_INPUT_LEFT		KEY_LEFT
@@ -29,14 +29,13 @@ enum FP_MODE{
 #define GF_NAV_INPUT_LONG_PRESS		KEY_SEARCH
 #define GF_NAV_INPUT_HEAVY		KEY_CHAT
 #endif
-
 #define GF_KEY_INPUT_HOME		KEY_HOME
 #define GF_KEY_INPUT_MENU		KEY_MENU
 #define GF_KEY_INPUT_BACK		KEY_BACK
 #define GF_KEY_INPUT_POWER		KEY_POWER
 #define GF_KEY_INPUT_CAMERA		KEY_CAMERA
 
-#if defined(SUPPORT_NAV_EVENT)
+#ifdef SUPPORT_NAV_EVENT
 typedef enum gf_nav_event {
 	GF_NAV_NONE = 0,
 	GF_NAV_FINGER_UP,
@@ -63,7 +62,7 @@ typedef enum gf_key_event {
 
 struct gf_key {
 	enum gf_key_event key;
-	uint32_t value;   /* key down = 1, key up = 0 */
+	uint32_t value;	/* key down = 1, key up = 0 */
 };
 
 struct gf_key_map {
@@ -78,31 +77,25 @@ struct gf_ioc_chip_info {
 	unsigned char reserved[5];
 };
 
-#define GF_IOC_MAGIC    'g'     //define magic number
-#define GF_IOC_INIT             _IOR(GF_IOC_MAGIC, 0, uint8_t)
-#define GF_IOC_EXIT             _IO(GF_IOC_MAGIC, 1)
-#define GF_IOC_RESET            _IO(GF_IOC_MAGIC, 2)
-#define GF_IOC_ENABLE_IRQ       _IO(GF_IOC_MAGIC, 3)
-#define GF_IOC_DISABLE_IRQ      _IO(GF_IOC_MAGIC, 4)
-#define GF_IOC_ENABLE_SPI_CLK   _IOW(GF_IOC_MAGIC, 5, uint32_t)
-#define GF_IOC_DISABLE_SPI_CLK  _IO(GF_IOC_MAGIC, 6)
-#define GF_IOC_ENABLE_POWER     _IO(GF_IOC_MAGIC, 7)
-#define GF_IOC_DISABLE_POWER    _IO(GF_IOC_MAGIC, 8)
-#define GF_IOC_INPUT_KEY_EVENT  _IOW(GF_IOC_MAGIC, 9, struct gf_key)
-#define GF_IOC_ENTER_SLEEP_MODE _IO(GF_IOC_MAGIC, 10)
-#define GF_IOC_GET_FW_INFO      _IOR(GF_IOC_MAGIC, 11, uint8_t)
-#define GF_IOC_REMOVE           _IO(GF_IOC_MAGIC, 12)
-#define GF_IOC_CHIP_INFO        _IOW(GF_IOC_MAGIC, 13, struct gf_ioc_chip_info)
-
-#if defined(SUPPORT_NAV_EVENT)
+#define GF_IOC_MAGIC		'g'	//define magic number
+#define GF_IOC_INIT		_IOR(GF_IOC_MAGIC, 0, uint8_t)
+#define GF_IOC_EXIT		_IO(GF_IOC_MAGIC, 1)
+#define GF_IOC_RESET		_IO(GF_IOC_MAGIC, 2)
+#define GF_IOC_ENABLE_IRQ	_IO(GF_IOC_MAGIC, 3)
+#define GF_IOC_DISABLE_IRQ	_IO(GF_IOC_MAGIC, 4)
+#define GF_IOC_ENABLE_SPI_CLK	_IOW(GF_IOC_MAGIC, 5, uint32_t)
+#define GF_IOC_DISABLE_SPI_CLK	_IO(GF_IOC_MAGIC, 6)
+#define GF_IOC_ENABLE_POWER	_IO(GF_IOC_MAGIC, 7)
+#define GF_IOC_DISABLE_POWER	_IO(GF_IOC_MAGIC, 8)
+#define GF_IOC_INPUT_KEY_EVENT	_IOW(GF_IOC_MAGIC, 9, struct gf_key)
+#define GF_IOC_ENTER_SLEEP_MODE	_IO(GF_IOC_MAGIC, 10)
+#define GF_IOC_GET_FW_INFO	_IOR(GF_IOC_MAGIC, 11, uint8_t)
+#define GF_IOC_REMOVE		_IO(GF_IOC_MAGIC, 12)
+#define GF_IOC_CHIP_INFO	_IOW(GF_IOC_MAGIC, 13, struct gf_ioc_chip_info)
+#ifdef SUPPORT_NAV_EVENT
 #define GF_IOC_NAV_EVENT	_IOW(GF_IOC_MAGIC, 14, gf_nav_event_t)
-#define  GF_IOC_MAXNR    15  /* THIS MACRO IS NOT USED NOW... */
-#else
-#define  GF_IOC_MAXNR    14  /* THIS MACRO IS NOT USED NOW... */
 #endif
-
-#define USE_PLATFORM_BUS     1
-#define GF_NETLINK_ENABLE 1
+#define USE_PLATFORM_BUS 1
 #define GF_NET_EVENT_IRQ 1
 #define GF_NET_EVENT_FB_BLACK 2
 #define GF_NET_EVENT_FB_UNBLACK 3
@@ -131,7 +124,7 @@ struct gf_dev {
 	bool proximity_state; /* 0:far 1:near */
 };
 
-static inline int gf_parse_dts(struct gf_dev* gf_dev);
+static inline int gf_parse_dts(struct gf_dev *gf_dev);
 static inline void gf_cleanup(struct gf_dev *gf_dev)
 {
 	pr_info("[info] %s\n", __func__);
@@ -149,15 +142,12 @@ static inline void gf_cleanup(struct gf_dev *gf_dev)
 
 static inline int gf_power_on(struct gf_dev *gf_dev)
 {
-	int rc = 0;
-
-	return rc;
+	return 0;
 }
+
 static inline int gf_power_off(struct gf_dev *gf_dev)
 {
-	int rc = 0;
-
-	return rc;
+	return 0;
 }
 
 static inline int gf_hw_reset(struct gf_dev *gf_dev, unsigned int delay_ms);
