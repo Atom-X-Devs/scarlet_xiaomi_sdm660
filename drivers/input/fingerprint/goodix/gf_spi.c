7c5ab9a539d8640aae5f19bd778996f3d1ed8321
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2022-11-02 18:47:17 +0600

    fingerprint: goodix: Reduce wakelock hold time to 1000 ms
    Forcing system to stay awake for 2000 ms after a scan wastes
    power. Lower it down to a more reasonable amount.
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/input/fingerprint/goodix/gf_spi.c
+++ drivers/input/fingerprint/goodix/gf_spi.c
@@ -58,7 +58,7 @@
 #define VER_MINOR   2
 #define PATCH_LEVEL 10
 
-#define WAKELOCK_HOLD_TIME 2000 /* in ms */
+#define WAKELOCK_HOLD_TIME 1000 /* in ms */
 #define FP_UNLOCK_REJECTION_TIMEOUT (WAKELOCK_HOLD_TIME - 500)
 #define GF_SPIDEV_NAME     "goodix,fingerprint"
 #define GF_DEV_NAME            "goodix_fp"
