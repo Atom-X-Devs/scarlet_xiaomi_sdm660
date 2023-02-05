c39f414cbfc3e8b30cc541da1781bc880ba4b179
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2023-02-06 05:21:17 +0600

    kernel/fork: Simplify Kprofiles mode handling
    Use ternary operator instead of switch statement as it's simpler and
    doesn't bring the overhead of loading the entire jump table like switch
    statements.
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -2329,19 +2329,9 @@ long _do_fork(unsigned long clone_flags,
 	struct task_struct *p;
 	int trace = 0;
 	long nr;
-	unsigned int period = 30;
+	unsigned int period;
 
-	switch (kp_active_mode()) {
-	case 0: /* Use balance mode's boost period */
-	case 2:
-		/* Boost for 50 ms when balance mode is active */
-		period = 50;
-		break;
-	case 3:
-		/* Boost for 100 ms when performance mode is active */
-		period = 100;
-		break;
-	}
+	period = (kp_active_mode() == 2) ? 50 : (kp_active_mode() == 3) ? 100 : 30;
 
 	/* Boost DDR bus to the max when userspace launches an app */
 	if (task_is_zygote(current))
