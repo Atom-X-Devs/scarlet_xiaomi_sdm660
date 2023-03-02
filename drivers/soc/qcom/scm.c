d0f5d5b50944460d2d55ff7475beb24324aa583a
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2023-03-03 01:16:48 +0600

    Revert "soc: qcom: scm: Remove double mutex lock"
    This turned out to be incorrect. Revert this.
    
    This reverts commit 0a284e99792895141be01de3b701e3a10aa5bebd.
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/soc/qcom/scm.c
+++ drivers/soc/qcom/scm.c
@@ -364,6 +364,10 @@ static int __scm_call2(u32 fn_id, struct scm_desc *desc, bool retry)
 	trace_scm_call_start(x0, desc);
 	do {
 		mutex_lock(&scm_lock);
+
+		if (SCM_SVC_ID(fn_id) == SCM_SVC_LMH)
+			mutex_lock(&scm_lmh_lock);
+
 		desc->ret[0] = desc->ret[1] = desc->ret[2] = 0;
 
 		if (scm_version == SCM_ARMV8_64)
@@ -378,6 +382,10 @@ static int __scm_call2(u32 fn_id, struct scm_desc *desc, bool retry)
 						  desc->args[2], desc->x5,
 						  &desc->ret[0], &desc->ret[1],
 						  &desc->ret[2]);
+
+		if (SCM_SVC_ID(fn_id) == SCM_SVC_LMH)
+			mutex_unlock(&scm_lmh_lock);
+
 		mutex_unlock(&scm_lock);
 		if (!retry)
 			goto out;
