1eeeb9c5954ffd5f56d0016671f8df23382d4843
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2023-03-25 13:54:16 +0600

    Revert "cpufreq: Do not kill userspace boosting when battery mode is active"
    This reverts commit 2ce1b3b8bfa4a4fe53fa29bf6d14959147ee593f.
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -723,8 +723,7 @@ static ssize_t store_##file_name					\
 	int ret, temp;							\
 	struct cpufreq_policy new_policy;				\
 									\
-	if ((&policy->object == &policy->min) &&			\
-	    (kp_active_mode() != 1))					\
+	if (&policy->object == &policy->min)				\
 		return count;						\
 									\
 	memcpy(&new_policy, policy, sizeof(*policy));			\
