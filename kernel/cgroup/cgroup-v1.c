a54a17fbb39bdea4e6acbe9d156fd9be4cd13402
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2023-03-25 18:13:52 +0900

    Revert "kernel/cgroup: Simplify Kprofiles mode handling"
    This reverts commit 7d36bb05a757d34ebc0a6e21846c10894a83e9d0.
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/cgroup/cgroup-v1.c
+++ kernel/cgroup/cgroup-v1.c
@@ -516,7 +516,7 @@ static ssize_t __cgroup1_procs_write(struct kernfs_open_file *of,
 	struct task_struct *task;
 	const struct cred *cred, *tcred;
 	ssize_t ret;
-        unsigned int period;
+        unsigned int period = 200;
 
 	cgrp = cgroup_kn_lock_live(of->kn, false);
 	if (!cgrp)
@@ -543,7 +543,17 @@ static ssize_t __cgroup1_procs_write(struct kernfs_open_file *of,
 	if (ret)
 		goto out_finish;
 
-	period = (kp_active_mode() == 2) ? 300 : (kp_active_mode() == 3) ? 500 : 200;
+        switch (kp_active_mode()) {
+        case 0: /* Use balance mode's boost period */
+        case 2:
+                /* Boost for 50 ms when balance mode is active */
+                period = 300;
+                break;
+        case 3:
+                /* Boost for 100 ms when performance mode is active */
+                period = 500;
+                break;
+        }
 
 	ret = cgroup_attach_task(cgrp, task, threadgroup);
 
