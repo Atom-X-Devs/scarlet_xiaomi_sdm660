Atom-X-Devs/scarlet_xiaomi_sdm660

commit 61eb9ae316ff31836f1570977b6ccfc69fecfb47

usb: f_fs: Fix ffs_log usage before ffs is set
In order to avoid use-after-free, the ffs_data struct is only set after
spin lock since upstream commit cfe5f6fd335d ("usb: f_fs: Fix use-after-free
for epfile"). ffs_log tries to access ffs->ipc_log which now fails because
ffs is not set. Move the ffs_log usage behind the initialization of ffs to
fix this issue.

Change-Id: I471b7fa0116b4ddefd24e4413164923821dd7802
Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

drivers/usb/gadget/function/f_fs.c
@@ -2111,14 +2111,15 @@ static void ffs_func_eps_disable(struct ffs_function *func)
 	unsigned short count;
 	unsigned long flags;
 
-	ffs_log("enter: state %d setup_state %d flag %lu", func->ffs->state,
-		func->ffs->setup_state, func->ffs->flags);
-
 	spin_lock_irqsave(&func->ffs->eps_lock, flags);
 	ep = func->eps;
 	ffs = func->ffs;
 	epfile = func->ffs->epfiles;
 	count = func->ffs->eps_count;
+
+	ffs_log("enter: state %d setup_state %d flag %lu", func->ffs->state,
+		func->ffs->setup_state, func->ffs->flags);
+
 	while (count--) {
 		/* pending requests get nuked */
 		if (likely(ep->ep))
@@ -2144,14 +2145,15 @@ static int ffs_func_eps_enable(struct ffs_function *func)
 	unsigned long flags;
 	int ret = 0;
 
-	ffs_log("enter: state %d setup_state %d flag %lu", func->ffs->state,
-		func->ffs->setup_state, func->ffs->flags);
-
 	spin_lock_irqsave(&func->ffs->eps_lock, flags);
 	ffs = func->ffs;
 	ep = func->eps;
 	epfile = ffs->epfiles;
 	count = ffs->eps_count;
+
+	ffs_log("enter: state %d setup_state %d flag %lu", func->ffs->state,
+		func->ffs->setup_state, func->ffs->flags);
+
 	while(count--) {
 		ep->ep->driver_data = ep;
 

commit a3022c1a1b6f8aede4e56cabf7a7b73e072dbca5

usb: gadget: f_fs: Unlock mutex after dropping log
Move the ffs_log() usage inside the mutex lock and unlock it afterwards
to avoid unexpected behaviour.

Fixes: 61eb9ae316ff ("usb: f_fs: Fix ffs_log usage before ffs is set")
Change-Id: Ic296cda6c094c4e75e467b6b0f4b4341f0fe5810
Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

drivers/usb/gadget/function/f_fs.c
@@ -2038,9 +2038,9 @@ static void functionfs_unbind(struct ffs_data *ffs)
 		ffs->ep0req = NULL;
 		ffs->gadget = NULL;
 		clear_bit(FFS_FL_BOUND, &ffs->flags);
-		mutex_unlock(&ffs->mutex);
 		ffs_log("state %d setup_state %d flag %lu gadget %pK\n",
 			ffs->state, ffs->setup_state, ffs->flags, ffs->gadget);
+		mutex_unlock(&ffs->mutex);
 		ffs_data_put(ffs);
 	}
 }