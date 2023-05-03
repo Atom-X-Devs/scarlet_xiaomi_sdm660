10cd859bfae0530492a3e986e77e00eaf9a6f505
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2023-05-04 03:19:57 +0600

    fingerprint: fpc: Remove unused `irqf` variable
    irqf was previously used for bitwise OR operation: assigning the result to the left
    operand and passing that variable. Now that we are doing all of the operations
    directly without using a custom variable, irq has no use case anymore. It's just
    there with a pointless initialization. Remove the unneeded codes.
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/input/fingerprint/fpc/fpc1020_platform_tee.c
+++ drivers/input/fingerprint/fpc/fpc1020_platform_tee.c
@@ -668,7 +668,6 @@ static inline int fpc1020_probe(struct platform_device *pdev)
 	struct fpc1020_data *fpc1020 =
 		devm_kzalloc(dev, sizeof(*fpc1020), GFP_KERNEL);
 	int rc = 0;
-	int irqf = 0;
 	size_t i;
 
 	if (!fpc1020) {
@@ -745,7 +744,7 @@ static inline int fpc1020_probe(struct platform_device *pdev)
 	mutex_init(&fpc1020->lock);
 	rc = devm_request_threaded_irq(dev, gpio_to_irq(fpc1020->irq_gpio),
 		NULL, fpc1020_irq_handler,
-		irqf | IRQF_TRIGGER_RISING | IRQF_ONESHOT,
+		IRQF_TRIGGER_RISING | IRQF_ONESHOT,
 		dev_name(dev), fpc1020);
 	if (rc)
 		goto exit;
