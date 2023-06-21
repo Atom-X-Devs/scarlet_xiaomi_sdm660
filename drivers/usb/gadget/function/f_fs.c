8b367385d16dfa6b24daa57f4e997e82870a3e08
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2023-06-22 01:01:20 +0600

    Revert "usb: f_fs: Free descriptors in func_unbind"
    Revert to avoid conflicts with upstream commit 9a95fba9362a ("usb:
    gadget: f_fs: Add unbind event before functionfs_unbind").
    
    This reverts commit 67a1b84a3dcc98c0b551054a9cac2a93b68c5d67.
    
    Change-Id: Ia22d842b77699eba847fe8f0db1fb699712dc256
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/usb/gadget/function/f_fs.c
+++ drivers/usb/gadget/function/f_fs.c
@@ -3820,6 +3820,7 @@ static void ffs_func_unbind(struct usb_configuration *c,
 	if (!--opts->refcnt) {
 		ffs_event_add(ffs, FUNCTIONFS_UNBIND);
 		functionfs_unbind(ffs);
+		return;
 	}
 
 	/* cleanup after autoconfig */
@@ -3844,12 +3845,10 @@ static void ffs_func_unbind(struct usb_configuration *c,
 	func->function.ssp_descriptors = NULL;
 	func->interfaces_nums = NULL;
 
-	if (opts->refcnt) {
-		ffs_event_add(ffs, FUNCTIONFS_UNBIND);
+	ffs_event_add(ffs, FUNCTIONFS_UNBIND);
 
-		ffs_log("exit: state %d setup_state %d flag %lu", ffs->state,
-			ffs->setup_state, ffs->flags);
-	}
+	ffs_log("exit: state %d setup_state %d flag %lu", ffs->state,
+		ffs->setup_state, ffs->flags);
 }
 
 static struct usb_function *ffs_alloc(struct usb_function_instance *fi)
