8d529fd0453bef1f9b65a440eb3987a797d95713
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2023-07-27 06:13:51 +0600

    fbdev: mdss: Import minimal jasmine/wayne changes
    - Do not add unblanking.
    - Do not add the prerequisites for livedisplay.
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/video/fbdev/msm/mdss_dsi_panel.c
+++ drivers/video/fbdev/msm/mdss_dsi_panel.c
@@ -495,7 +495,9 @@ int mdss_dsi_panel_reset(struct mdss_panel_data *pdata, int enable)
 			gpio_set_value((ctrl_pdata->disp_en_gpio), 0);
 			gpio_free(ctrl_pdata->disp_en_gpio);
 		}
+#ifndef CONFIG_XIAOMI_SDM660
 		gpio_set_value((ctrl_pdata->rst_gpio), 0);
+#endif
 		gpio_free(ctrl_pdata->rst_gpio);
 		if (gpio_is_valid(ctrl_pdata->lcd_mode_sel_gpio)) {
 			gpio_set_value(ctrl_pdata->lcd_mode_sel_gpio, 0);
