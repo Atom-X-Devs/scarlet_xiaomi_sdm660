18f0b5eac32135394c6c907c90d12457a913dd0f
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2023-07-27 20:35:40 +0600

    Revert "mdss: fbdev: msm: Convert to wakeup sources"
    This reverts commit aad596b6a2beefb32d1196b9471703305699eb5d.

--- drivers/video/fbdev/msm/mdss_fb.c
+++ drivers/video/fbdev/msm/mdss_fb.c
@@ -118,13 +118,11 @@ static void mdss_fb_set_mdp_sync_pt_threshold(struct msm_fb_data_type *mfd,
 #ifdef CONFIG_MACH_LONGCHEER
 #define WAIT_RESUME_TIMEOUT 200
 static struct fb_info *prim_fbi;
+static struct delayed_work prim_panel_work;
 static atomic_t prim_panel_is_on;
+static struct wake_lock prim_panel_wakelock;
 static void prim_panel_off_delayed_work(struct work_struct *work)
 {
-	struct delayed_work *dw = to_delayed_work(work);
-	struct msm_fb_data_type *mfd = container_of(dw, struct msm_fb_data_type,
-		prim_panel_work);
-
 #ifdef CONFIG_FRAMEBUFFER_CONSOLE
 	console_lock();
 #endif
@@ -137,7 +135,7 @@ static void prim_panel_off_delayed_work(struct work_struct *work)
 	if (atomic_read(&prim_panel_is_on)) {
 		fb_blank(prim_fbi, FB_BLANK_POWERDOWN);
 		atomic_set(&prim_panel_is_on, false);
-		__pm_relax(mfd->prim_panel_wakelock);
+		wake_unlock(&prim_panel_wakelock);
 	}
 	unlock_fb_info(prim_fbi);
 #ifdef CONFIG_FRAMEBUFFER_CONSOLE
@@ -1498,8 +1496,8 @@ static int mdss_fb_remove(struct platform_device *pdev)
 #ifdef CONFIG_MACH_LONGCHEER
 	if (mfd->panel_info && mfd->panel_info->is_prim_panel) {
 		atomic_set(&prim_panel_is_on, false);
-		cancel_delayed_work_sync(&mfd->prim_panel_work);
-		wakeup_source_unregister(mfd->prim_panel_wakelock);
+		cancel_delayed_work_sync(&prim_panel_work);
+		wake_lock_destroy(&prim_panel_wakelock);
 	}
 #endif
 	mdss_fb_remove_sysfs(mfd);
@@ -2202,8 +2200,8 @@ static int mdss_fb_blank(int blank_mode, struct fb_info *info)
 	if ((info == prim_fbi) && (blank_mode == FB_BLANK_UNBLANK) &&
 		atomic_read(&prim_panel_is_on)) {
 		atomic_set(&prim_panel_is_on, false);
-		__pm_relax(mfd->prim_panel_wakelock);
-		cancel_delayed_work_sync(&mfd->prim_panel_work);
+		wake_unlock(&prim_panel_wakelock);
+		cancel_delayed_work_sync(&prim_panel_work);
 		return 0;
 	}
 #endif
@@ -2883,8 +2881,8 @@ static int mdss_fb_register(struct msm_fb_data_type *mfd)
 	if (panel_info->is_prim_panel) {
 		prim_fbi = fbi;
 		atomic_set(&prim_panel_is_on, false);
-		INIT_DELAYED_WORK(&mfd->prim_panel_work, prim_panel_off_delayed_work);
-		mfd->prim_panel_wakelock = wakeup_source_register(NULL, "prim_panel_wakelock");
+		INIT_DELAYED_WORK(&prim_panel_work, prim_panel_off_delayed_work);
+		wake_lock_init(&prim_panel_wakelock, WAKE_LOCK_SUSPEND, "prim_panel_wakelock");
 	}
 #endif
 
@@ -5482,16 +5480,16 @@ int mdss_prim_panel_fb_unblank(int timeout)
 #endif
 			return 0;
 		}
-		__pm_stay_awake(mfd->prim_panel_wakelock);
+		wake_lock(&prim_panel_wakelock);
 		ret = fb_blank(prim_fbi, FB_BLANK_UNBLANK);
 		if (!ret) {
 			atomic_set(&prim_panel_is_on, true);
 			if (timeout > 0)
-				schedule_delayed_work(&mfd->prim_panel_work, msecs_to_jiffies(timeout));
+				schedule_delayed_work(&prim_panel_work, msecs_to_jiffies(timeout));
 			else
-				__pm_relax(mfd->prim_panel_wakelock);
+				wake_unlock(&prim_panel_wakelock);
 		} else
-			__pm_relax(mfd->prim_panel_wakelock);
+			wake_unlock(&prim_panel_wakelock);
 		unlock_fb_info(prim_fbi);
 #ifdef CONFIG_FRAMEBUFFER_CONSOLE
 		console_unlock();
