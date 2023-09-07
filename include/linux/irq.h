4a041800bbcdcc31f6c1d339f235e3b7e2eb2aac
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2023-09-07 15:15:27 +0600

    treewide: Remove perf-critical API and its usage
    We will use a simple IRQ balancer which is comparatively much more efficient
    than the current perf-critical API since there are many other IRQs that aren't
    marked perf-critical and perf-critical only does static balancing.
    SBalance will balance the loads accross all CPUs (except the excluded ones)
    fairly under a specific polling interval.
    Therefore, to avoid collision of functionality, deprecate the perf-critical API
    and its usage completely.
    
    This reverts the following commits:
    
    4c88f558bf626 ("devfreq_boost: Run boost kthreads on big CPU cluster")
    aaaaa350b3464 ("workqueue: Affine unbound workqueues to little CPUs by default")
    28fad9026fd82 ("Revert "workqueue: respect isolated cpus when queueing an unbound work"")
    6a0125aca0b34 ("Revert "sched/isolation: Isolate workqueues when "nohz_full=" is set"")
    0262a4c3efdee ("kernel: Extend the perf-critical API to little CPUs")
    fb57651e333a1 ("kernel: Add bi-cluster API to affine IRQs and kthreads to fast CPUs")
    0bfc20abc7eda ("Revert "genirq: Provide IRQCHIP_AFFINITY_PRE_STARTUP"")
    b227ff5bff103 ("defconfigs: xiaomi: Populate cluster CPU bitmasks for sdm660")
    f774b8cc1e811 ("cpumask: Add cpumasks for big, LITTLE CPU clusters")
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- include/linux/irq.h
+++ include/linux/irq.h
@@ -212,7 +212,6 @@ struct irq_data {
  *				  required
  * IRQD_AFFINITY_ON_ACTIVATE	- Affinity is set on activation. Don't call
  *				  irq_chip::irq_set_affinity() when deactivated.
- * IRQD_PERF_CRITICAL		- IRQ is performance-critical
  */
 enum {
 	IRQD_TRIGGER_MASK		= 0xf,
@@ -236,7 +235,6 @@ enum {
 	IRQD_DEFAULT_TRIGGER_SET	= (1 << 25),
 	IRQD_CAN_RESERVE		= (1 << 26),
 	IRQD_MSI_NOMASK_QUIRK		= (1 << 27),
-	IRQD_PERF_CRITICAL		= (1 << 28),
 	IRQD_AFFINITY_ON_ACTIVATE	= (1 << 29),
 };
 
@@ -537,6 +535,7 @@ struct irq_chip {
  * IRQCHIP_ONESHOT_SAFE:	One shot does not require mask/unmask
  * IRQCHIP_EOI_THREADED:	Chip requires eoi() on unmask in threaded mode
  * IRQCHIP_SUPPORTS_LEVEL_MSI	Chip can provide two doorbells for Level MSIs
+ * IRQCHIP_AFFINITY_PRE_STARTUP:      Default affinity update before startup
  */
 enum {
 	IRQCHIP_SET_TYPE_MASKED		= (1 <<  0),
@@ -547,6 +546,7 @@ enum {
 	IRQCHIP_ONESHOT_SAFE		= (1 <<  5),
 	IRQCHIP_EOI_THREADED		= (1 <<  6),
 	IRQCHIP_SUPPORTS_LEVEL_MSI	= (1 <<  7),
+	IRQCHIP_AFFINITY_PRE_STARTUP	= (1 << 10),
 };
 
 #include <linux/irqdesc.h>
--- kernel/workqueue.c
+++ kernel/workqueue.c
@@ -47,6 +47,7 @@
 #include <linux/nodemask.h>
 #include <linux/moduleparam.h>
 #include <linux/uaccess.h>
+#include <linux/sched/isolation.h>
 #include <linux/nmi.h>
 #include <linux/bug.h>
 #include <linux/delay.h>
@@ -5209,6 +5210,10 @@ int workqueue_set_unbound_cpumask(cpumask_var_t cpumask)
 	int ret = -EINVAL;
 	cpumask_var_t saved_cpumask;
 
+	/*
+	 * Not excluding isolated cpus on purpose.
+	 * If the user wishes to include them, we allow that.
+	 */
 	cpumask_and(cpumask, cpumask, cpu_possible_mask);
 	if (!cpumask_empty(cpumask)) {
 		apply_wqattrs_lock();
@@ -5819,12 +5824,13 @@ static void __init wq_numa_init(void)
 int __init workqueue_init_early(void)
 {
 	int std_nice[NR_STD_WORKER_POOLS] = { 0, HIGHPRI_NICE_LEVEL };
+	int hk_flags = HK_FLAG_DOMAIN | HK_FLAG_WQ;
 	int i, cpu;
 
 	WARN_ON(__alignof__(struct pool_workqueue) < __alignof__(long long));
 
 	BUG_ON(!alloc_cpumask_var(&wq_unbound_cpumask, GFP_KERNEL));
-	cpumask_copy(wq_unbound_cpumask, cpu_lp_mask);
+	cpumask_copy(wq_unbound_cpumask, housekeeping_cpumask(hk_flags));
 
 	pwq_cache = KMEM_CACHE(pool_workqueue, SLAB_PANIC);
 
