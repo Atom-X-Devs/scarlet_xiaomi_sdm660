94df7583c460f6d6db2e7a524d261073b1bb57b5
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2023-09-26 03:23:45 +0600

    arch_topology: Use sysfs_emit() for cpu_capacity_show()
    Convert the scnprintf() call in sysfs device show function cpu_capacity_show()
    to sysfs_emit() for PAGE_SIZE buffer safety and to align with upstream commit
    3ce2cd63e8ee03 ("drivers core: Use sysfs_emit and sysfs_emit_at for
    show(device *...) functions").
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/base/arch_topology.c
+++ drivers/base/arch_topology.c
@@ -114,7 +114,7 @@ static ssize_t cpu_capacity_show(struct device *dev,
 	struct cpu *cpu = container_of(dev, struct cpu, dev);
 
 	if (is_sched_lib_based_app(current->pid))
-		return scnprintf(buf, PAGE_SIZE, "%lu\n", SCHED_CAPACITY_SCALE);
+		return sysfs_emit(buf, "%lu\n", SCHED_CAPACITY_SCALE);
 
 	return sysfs_emit(buf, "%lu\n", topology_get_cpu_scale(NULL, cpu->dev.id));
 }
