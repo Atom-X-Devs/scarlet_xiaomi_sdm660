ef33ccfe86e30f7349cb628d93e4fc4c70098e88
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2023-12-22 16:29:57 +0600

    zram: Hardcode zRAM size to 2 GB
    2 GB zRAM size is ample for a device that has 4 GB physical ram in its base
    variant. Any more of it will just be a waste. Therefore, hardcode the
    zRAM size to 2 GB.
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -1720,9 +1720,8 @@ static ssize_t disksize_store(struct device *dev,
 	struct zram *zram = dev_to_zram(dev);
 	int err;
 
-	disksize = memparse(buf, NULL);
-	if (!disksize)
-		return -EINVAL;
+	disksize = (u64)SZ_1G * 2;
+	pr_info("Setting zRAM size to %li GB", disksize / 1073741824);
 
 	down_write(&zram->init_lock);
 	if (init_done(zram)) {
